695ff475ab76c8ddbdef89619becb95dcbe226b8
Olivier Corby <olivier.corby@inria.fr>
2011-05-10 07:38:57 +0000

    rule engine
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@44 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/Loader.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/Loader.java
@@ -1,10 +1,14 @@
 package fr.inria.edelweiss.kgraph.api;
 
+import fr.inria.edelweiss.kgraph.rule.RuleEngine;
+
 public interface Loader {
 	
 	void load(String path);
 	
 	void load(String path, String source);
+	
+	RuleEngine getRuleEngine();
 
 
 }

70293d515095bae07c307ef2571a5ac380058df2
Olivier Corby <olivier.corby@inria.fr>
2011-05-30 07:04:58 +0000

    draft QueryEngine that load and run several queries
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@94 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/QueryEngine.java
@@ -0,0 +1,80 @@
+package fr.inria.edelweiss.kgraph.query;
+
+import java.util.ArrayList;
+
+import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.edelweiss.kgram.core.Mappings;
+import fr.inria.edelweiss.kgram.core.Query;
+import fr.inria.edelweiss.kgraph.core.Graph;
+
+/**
+ * Equivalent of RuleEngine for Query
+ * Run a set of query
+ * 
+ * @author Olivier Corby, Edelweiss, INRIA 2010
+ *
+ */
+public class QueryEngine {
+	
+	Graph graph;
+	QueryProcess exec;
+	ArrayList<Query> list;
+	
+	boolean isDebug = false;
+	
+	QueryEngine(Graph g){
+		graph = g;
+		exec = QueryProcess.create(g);
+		list = new ArrayList<Query>();
+	}
+	
+	public static QueryEngine create(Graph g){
+		return new QueryEngine(g);
+	}
+
+	public void setDebug(boolean b){
+		isDebug = b;
+	}
+	
+	public void addQuery(String q)  {
+		 try {
+			defQuery(q);
+		} catch (EngineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Query defQuery(String q) throws EngineException {
+		Query qq = exec.compile(q);
+		if (qq != null) {
+			list.add(qq);
+			return qq;
+		}
+		return null;
+	}
+	
+	
+	public void process(){
+		for (Query q : list){
+			if (isDebug){
+				q.setDebug(isDebug);
+			}
+			Mappings map = exec.query(q);
+			if (isDebug){
+				System.out.println(map);
+				System.out.println();
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

6ff644264adde55dfcb58cbf28a2c3beb116e255
Olivier Corby <olivier.corby@inria.fr>
2011-06-29 07:51:04 +0000

    frame title

--- kggui/src/main/java/fr/inria/acacia/corese/gui/core/MainFrame.java
+++ kggui/src/main/java/fr/inria/acacia/corese/gui/core/MainFrame.java
@@ -160,7 +160,7 @@ public class MainFrame extends JFrame implements ActionListener{
 	
 	public MainFrame(CaptureOutput aCapturer, String p_PropertyPath){
 		super();
-		this.setTitle("Corese/KGRAM GUI - 3.0.2 - 2011-05-30" ); //+ Corese.version + " - " + Corese.date );
+		this.setTitle("Corese/KGRAM GUI - 3.0 - 2011-07-01" ); //+ Corese.version + " - " + Corese.date );
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(900, 700);
 		this.setMinimumSize(this.getSize());

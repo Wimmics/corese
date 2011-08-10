36cd2809e9946082f9d2de2d9e0709f79addc3d5
Olivier Corby <olivier.corby@inria.fr>
2011-08-10 09:46:02 +0000

    Draft service provider
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@183 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/api/query/Provider.java
@@ -0,0 +1,17 @@
+package fr.inria.edelweiss.kgram.api.query;
+
+import fr.inria.edelweiss.kgram.api.core.Node;
+import fr.inria.edelweiss.kgram.core.Exp;
+import fr.inria.edelweiss.kgram.core.Mappings;
+
+/**
+ * Service Provider
+ * 
+ * @author Olivier Corby, Edelweiss, INRIA 2011
+ *
+ */
+public interface Provider {
+	
+	Mappings service(Node serv, Exp exp);
+
+}

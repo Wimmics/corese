c8d52642ca8f538cd495435f85571cc2e7ae3655
Olivier Corby <olivier.corby@inria.fr>
2011-08-18 08:00:11 +0000

    LoadException

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/Loader.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/Loader.java
@@ -1,12 +1,17 @@
 package fr.inria.edelweiss.kgraph.api;
 
 import fr.inria.edelweiss.kgraph.rule.RuleEngine;
+import fr.inria.edelweiss.kgtool.load.LoadException;
 
 public interface Loader {
 	
 	void load(String path);
 	
 	void load(String path, String source);
+	
+	void loadWE(String path) throws LoadException;
+	
+	void loadWE(String path, String source) throws LoadException;
 	
 	RuleEngine getRuleEngine();
 

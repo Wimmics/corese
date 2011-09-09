d4021bc50d2b698721216c82d54868a402481245
Alban Gaignar <alban.gaignard@i3s.unice.fr>
2011-09-09 08:17:12 +0000

    EdgeImpl -> EdgeCore

--- kgengine/src/test/java/test/kgraph/TestGraph.java
+++ kgengine/src/test/java/test/kgraph/TestGraph.java
@@ -4,7 +4,7 @@ import fr.inria.acacia.corese.exceptions.EngineException;
 import fr.inria.acacia.corese.triple.cst.RDFS;
 import fr.inria.edelweiss.kgram.api.core.Node;
 import fr.inria.edelweiss.kgram.core.Mappings;
-import fr.inria.edelweiss.kgraph.core.EdgeImpl;
+import fr.inria.edelweiss.kgraph.core.EdgeCore;
 import fr.inria.edelweiss.kgraph.core.Graph;
 import fr.inria.edelweiss.kgraph.logic.Entailment;
 import fr.inria.edelweiss.kgraph.query.QueryProcess;
@@ -46,35 +46,35 @@ public class TestGraph {
 		s = graph.addBlank();
 		p = graph.addProperty("name");
 		o = graph.addLiteral("John");
-		graph.addEdge(EdgeImpl.create(g, s, p, o, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o, o));
 		
 		p = graph.addProperty("member");
 		o = graph.addResource("cnrs");
-		graph.addEdge(EdgeImpl.create(g, s, p, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o));
 		
 		p = graph.addProperty("age");
 		o = graph.addLiteral(23);
-		graph.addEdge(EdgeImpl.create(g, s, p, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o));
 		
 		p = graph.addProperty(Entailment.RDFTYPE);
 		o = graph.addResource("Person");
-		graph.addEdge(EdgeImpl.create(g, s, p, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o));
 		
 		s = graph.addResource("Person");
 		p = graph.addProperty(Entailment.RDFSSUBCLASSOF);
 		o = graph.addResource("Animal");
-		graph.addEdge(EdgeImpl.create(g, s, p, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o));
 		
 		s = graph.addResource("name");
 		p = graph.addProperty(Entailment.RDFSSUBPROPERTYOF);
 		o = graph.addResource("property");
-		graph.addEdge(EdgeImpl.create(g, s, p, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o));
 		
 		
 		s = graph.addResource("cnrs");
 		p = graph.addProperty("name");
 		o = graph.addLiteral("CNRS");
-		graph.addEdge(EdgeImpl.create(g, s, p, o));
+		graph.addEdge(EdgeCore.create(g, s, p, o));
 
 		
 		return graph;

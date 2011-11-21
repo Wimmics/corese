36e0f04b0a5316122da8fccddaed4121a6cc2d42
Olivier Corby <olivier.corby@inria.fr>
2011-11-21 08:52:50 +0000

    kg:similarity kg:pstep kg:similarity kg:cstep
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@475 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/PragmaImpl.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/PragmaImpl.java
@@ -1,9 +1,11 @@
 package fr.inria.edelweiss.kgraph.query;
 
+import fr.inria.acacia.corese.api.IDatatype;
 import fr.inria.acacia.corese.triple.parser.Triple;
 import fr.inria.edelweiss.kgenv.parser.Pragma;
 import fr.inria.edelweiss.kgram.core.Query;
 import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.logic.Distance;
 import fr.inria.edelweiss.kgraph.logic.Entailment;
 
 /**
@@ -15,20 +17,27 @@ import fr.inria.edelweiss.kgraph.logic.Entailment;
  */
 public class PragmaImpl extends Pragma {
 	
-	static final String ENTAIL 	= KG + "entailment";
+	static final String ENTAIL 	 	= KG + "entailment";
+	static final String SIMILARITY 	= KG + "similarity";
+	static final String PSTEP 		= KG + "pstep";
+	static final String CSTEP 		= KG + "cstep";
+
 	
 	QueryProcess exec;
 	Graph graph;
 	Entailment entail ;
 
 
-	public PragmaImpl(QueryProcess ex, Query q){
+	PragmaImpl(QueryProcess ex, Query q){
 		super(q, ex.getAST(q));
 		exec = ex;
 		graph = exec.getGraph();
 		entail = graph.getEntailment();
 	}
 
+	public static PragmaImpl create(QueryProcess ex, Query q){
+		return new PragmaImpl(ex, q);
+	}
 
 	public void triple(Triple t){
 
@@ -36,7 +45,7 @@ public class PragmaImpl extends Pragma {
 		String property = t.getProperty().getLongName();
 		String object   = t.getObject().getLongName();
 		if (object == null) object = t.getObject().getName();
-		
+		IDatatype dt = t.getObject().getDatatypeValue();
 		
 		if (subject.equals(SELF)){
 			if (property.equals(ENTAIL)){
@@ -53,7 +62,18 @@ public class PragmaImpl extends Pragma {
 			// kg:entailment rdfs:range false
 			entail.set(property, value(object));
 		}
-		
+		else if (subject.equals(SIMILARITY)){
+			if (property.equals(PSTEP)){
+				// kg:similarity kg:pstep 0.5
+				graph.setPropertyDistance(null);
+				Distance.setPropertyStep(dt.getDoubleValue());
+			}
+			else if (property.equals(CSTEP)){
+				// kg:similarity kg:cstep 2
+				graph.setClassDistance(null);
+				Distance.setClassStep(dt.getDoubleValue());
+			}
+		}
 				
 	}
 

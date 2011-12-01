4963a92872dbb554161f6cceda9dacfead43f14a
Olivier Corby <olivier.corby@inria.fr>
2011-12-01 13:04:26 +0000

    do not generate pretty print
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@487 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- sparql/src/main/java/fr/inria/acacia/corese/triple/parser/ASTQuery.java
+++ sparql/src/main/java/fr/inria/acacia/corese/triple/parser/ASTQuery.java
@@ -1474,7 +1474,7 @@ public class ASTQuery  implements Keyword {
         	compileDescribe(parser);
         }
         
-        setQueryPrettyPrint(getSparqlPrettyPrint());
+      //  setQueryPrettyPrint(getSparqlPrettyPrint());
         
     }
     

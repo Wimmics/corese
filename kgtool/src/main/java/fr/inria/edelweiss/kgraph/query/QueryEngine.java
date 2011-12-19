f35ec43ba36a7ac1a4e1790b43e4efa2ce223c6f
Olivier Corby <olivier.corby@inria.fr>
2011-12-19 14:04:15 +0000

    construct where queries

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/QueryEngine.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/QueryEngine.java
@@ -1,10 +1,17 @@
 package fr.inria.edelweiss.kgraph.query;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.edelweiss.kgram.api.core.Edge;
+import fr.inria.edelweiss.kgram.api.core.Entity;
+import fr.inria.edelweiss.kgram.api.core.Node;
+import fr.inria.edelweiss.kgram.api.core.Regex;
+import fr.inria.edelweiss.kgram.core.Exp;
+import fr.inria.edelweiss.kgram.core.Mapping;
 import fr.inria.edelweiss.kgram.core.Mappings;
 import fr.inria.edelweiss.kgram.core.Query;
 import fr.inria.edelweiss.kgraph.core.Graph;
@@ -57,6 +64,10 @@ public class QueryEngine {
 		return null;
 	}
 	
+	public List<Query> getQueries(){
+		return list;
+	}
+	
 	
 	public void process(){
 		for (Query q : list){
@@ -72,9 +83,50 @@ public class QueryEngine {
 	
 	
 	
-	
-	
-	
+	public Mappings process(Query q, Mapping m){
+		try {
+			Mappings map = exec.query(q, m, null, null);
+			return map;
+		} catch (EngineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Mappings.create(q);
+	}
+
+	/**
+	 * pname is property name
+	 * queries are construct where
+	 * find a query with construct {?x pname ?y}
+	 * process the query
+	 * use case: ProducerImpl getEdges() computed by construct-where 
+	 */
+	Mappings process(Node start, String pname, int index){
+		for (Query q : getQueries()){
+			
+			if (q.isConstruct()){
+				Exp cons = q.getConstruct();
+				for (Exp ee : cons.getExpList()){
+
+					if (ee.isEdge()){
+						Edge edge = ee.getEdge();
+						if (edge.getLabel().equals(pname)){
+
+							Mapping bind = null;
+							if (start != null) {
+								bind = Mapping.create(edge.getNode(index), start);
+							}
+
+							Mappings map = process(q, bind);
+							return map;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
 	
 	
 	

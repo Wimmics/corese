ee632f65df5b9b88e7f37265d08aa54dcf733b8e
Olivier Corby <olivier.corby@inria.fr>
2011-12-19 14:03:05 +0000

    kg:status
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@599 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/PragmaImpl.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/PragmaImpl.java
@@ -1,11 +1,16 @@
 package fr.inria.edelweiss.kgraph.query;
 
 import fr.inria.acacia.corese.api.IDatatype;
+import fr.inria.acacia.corese.exceptions.EngineException;
 import fr.inria.acacia.corese.triple.parser.Atom;
 import fr.inria.acacia.corese.triple.parser.Triple;
 import fr.inria.edelweiss.kgenv.parser.Pragma;
+import fr.inria.edelweiss.kgram.api.query.Producer;
+import fr.inria.edelweiss.kgram.core.Mappings;
 import fr.inria.edelweiss.kgram.core.Query;
+import fr.inria.edelweiss.kgram.tool.MetaProducer;
 import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.api.Log;
 import fr.inria.edelweiss.kgraph.logic.Distance;
 import fr.inria.edelweiss.kgraph.logic.Entailment;
 
@@ -22,7 +27,6 @@ public class PragmaImpl extends Pragma {
 	static final String SIMILARITY 	= KG + "similarity";
 	static final String PSTEP 		= KG + "pstep";
 	static final String CSTEP 		= KG + "cstep";
-
 	
 	QueryProcess exec;
 	Graph graph;
@@ -74,6 +78,9 @@ public class PragmaImpl extends Pragma {
 				// we force entailment
 				if (b) graph.setUpdate(true);	
 			}
+			else if (property.equals(STATUS)){
+				new Describe(exec, query).describe(value(object));
+			}
 		}
 		else if (subject.equals(ENTAIL)){
 			// kg:entailment rdfs:subClassOf true
@@ -97,7 +104,15 @@ public class PragmaImpl extends Pragma {
 				query.addInfo(help(), null);
 			}
 		}
+		
 				
 	}
+	
+	
+	
+	
+	
+	
+	
 
 }

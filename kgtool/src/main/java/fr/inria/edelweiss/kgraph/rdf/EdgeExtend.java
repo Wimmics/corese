249da74634a845d65b32edfaf238e86ded2b1bab
Olivier Corby <olivier.corby@inria.fr>
2012-01-19 09:06:34 +0000

    tuple()
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@658 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/rdf/EdgeExtend.java
@@ -0,0 +1,39 @@
+package fr.inria.edelweiss.kgraph.rdf;
+
+import java.util.ArrayList;
+
+import fr.inria.edelweiss.kgram.api.core.Node;
+import fr.inria.edelweiss.kgraph.core.EdgeCore;
+
+/**
+ * Edge class where the  property node is in the class
+ * e.g. rdfs:label
+ * 
+ * @author Olivier Corby, Edelweiss INRIA 2010
+ *
+ */
+public class EdgeExtend extends EdgeCore {
+
+	ArrayList<Node> nodes;
+	
+	public EdgeExtend(){
+		nodes = new ArrayList<Node>();
+	}
+	
+	public void setNode(int i , Node node){
+		super.setNode(i, node);
+		nodes.add(i, node);	
+	}
+	
+	public Node getNode(int n) {
+		switch (n) {
+		case -1: return getGraph();
+		default: return nodes.get(n);
+		}
+	}
+	
+	public int nbNode() {
+		return nodes.size();
+	}
+	
+}

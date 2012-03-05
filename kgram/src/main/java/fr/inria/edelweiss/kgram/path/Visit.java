7b1daa857fc1cdfcd0562bedbf0650fc95799404
Olivier Corby <olivier.corby@inria.fr>
2012-03-05 09:38:27 +0000

    leverage (exp*)* and (exp+)+

--- kgram/src/main/java/fr/inria/edelweiss/kgram/path/Visit.java
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/path/Visit.java
@@ -23,6 +23,7 @@ public class Visit {
 	Hashtable<State, List<Node>> table;
 	Hashtable<Regex, List<Node>> etable;
 	Hashtable<Regex, Table> eetable;
+	ETable ttable;
 	Hashtable<Regex, Integer> ctable;
 	DTable tdistinct;
 	ArrayList<Regex> list;
@@ -33,18 +34,11 @@ public class Visit {
 		table   = new Hashtable<State, List<Node>>();
 		etable  = new Hashtable<Regex, List<Node>>();
 		eetable = new Hashtable<Regex, Table>();
+		ttable  = new ETable(b);
 		ctable  = new Hashtable<Regex, Integer> ();
 		list    = new ArrayList<Regex>();
 		tdistinct = new DTable();
 		isReverse = b;
-	}
-	
-	interface TTable {
-		
-		Node put(Node n1, Node n2);
-		Node remove(Node n1);
-		boolean containsKey(Node node);
-		
 	}
 	
 	class Table1 extends Hashtable<Node, Node>  {}
@@ -56,6 +50,116 @@ public class Visit {
 			super(new Compare());
 		}
 	}
+	
+	class ETable extends Hashtable<Regex, TTable> {
+		boolean isReverse = false;
+		
+		ETable(boolean b){
+			isReverse = b;
+		}
+		
+		void add(Regex exp, Node n){
+			TTable t = get(exp);
+			if (t == null){
+				t = new TTable(isReverse);
+				put(exp, t);
+			}
+			t.add(n);
+		}
+		
+		void remove(Regex exp, Node n){
+			TTable t = get(exp);
+			t.remove(n);
+		}
+		
+		
+		boolean loop(Regex exp, Node start){
+			TTable table = get(exp);
+			if (table == null) {
+				return false;
+			}
+			return table.loop(exp, start);
+		}
+		
+		
+		
+	}
+	
+	
+	class TTable    {	
+		Table table;
+		List<Node> list;
+		boolean isReverse = false;
+		
+		TTable(boolean b){
+			isReverse = b;
+			if (isReverse){
+				list = new ArrayList<Node>();
+			}
+			else {
+				table = new Table();
+			}
+		}
+		
+		void add(Node n){
+			if (isReverse){
+				list.add(n);
+			}
+			else {
+				table.put(n, n);
+			}
+		}
+		
+		void remove(Node n){
+			if (isReverse){
+				if (! list.get(list.size()-1).equals(n)){
+					System.out.println("** ERROR Visit: " + n + " " + list);
+				}
+				list.remove(list.size()-1);			}
+			else {
+				table.remove(n);
+			}
+		}
+		
+		boolean loop(Regex exp, Node n){
+			if (isReverse){
+				if (list == null || list.size()<2) return false;
+				int last = list.size()-1;
+
+				// use case: ?s {2,} ex:uri
+				// Walk through the list backward (from <uri> to ?s), skip 2 first nodes
+				// cf W3C pp test case
+				Node node = null;
+				int count = 0;
+				int min = exp.getMin();
+				if (min == -1){
+					min = 0;
+				}
+
+				for (int i = last; i>=0; i--){
+					if (count<min) {
+						// continue
+					}
+					else if (count == min){ 
+						node = list.get(i);
+					}
+					else {
+						if (list.get(i).equals(node)){
+							return true;
+						}
+					}
+					count++;
+				}
+							
+				return false;
+			}
+			else {
+				return table.containsKey(n);
+			}
+		}
+		
+	}
+	
 	
 	class DTable extends TreeMap<Node, Table>  {	
 		
@@ -82,101 +186,7 @@ public class Visit {
 	}
 	
 	
-	/*********************
-	 *
-	 * States are deprecated
-	 */
-	List<Node> get(State state){
-		return table.get(state);
-	}
-	
-	void put(State state, List<Node> list){
-		table.put(state, list);
-	}
 
-	public void add(State state, Node node) {
-		List<Node> list = get(state);
-		if (list == null){
-			list = new ArrayList<Node>();
-			put(state, list);
-		}
-		list.add(node);
-	}
-
-	public void remove(State state, Node node) {
-		List<Node> list = get(state);
-		if (! list.get(list.size()-1).equals(node)){
-			System.out.println("** ERROR Visit: " + node + " " + list);
-		}
-		list.remove(list.size()-1);
-	}
-
-	
-	/**
-	 * Check whether the last visited node has already been 
-	 * visited as an intermediate node 
-	 * use case: 
-	 * ?x p{2,} ?y   :  skip the first two nodes (no loop with two first)
-	 * ?x p{2,} ex:uri : walk backward through the list 
-	 * 
-	 */
-	public boolean loop(State state) {
-		if (! state.isCheckLoop()) return false;
-		
-		List<Node> list = get(state);
-		if (list == null || list.size()<2) return false;
-		int last = list.size()-1;
-		int min = 0;
-		if (state.isBound() && ! state.isPlus()) min = state.getMin();
-
-		if (isReverse){
-			// use case: ?s {2,} ex:uri
-			// Walk through the list backward (from <uri> to ?s), skip 2 first nodes
-			// cf W3C pp test case
-			Node node = null;
-			int count = 0;
-			for (int i = last; i>=0; i--){
-				if (count<min) {count++; continue;}
-				else if (count == min){ 
-					node = list.get(i);
-				}
-				else {
-					if (list.get(i).equals(node)){
-						return true;
-					}
-				}
-				count++;
-			}
-		}
-		else 
-		{
-			Node node = list.get(last);		
-			for (int i = 0; i<last; i++){
-				if (i<min) continue;
-				if (list.get(i).equals(node)){
-					return true;
-				}
-			}
-		}
-		
-		return false;
-	} 
-
-	public boolean loop2(State state) {
-		List<Node> list = get(state);
-		if (list == null || list.size()<2) return false;
-		int last = list.size()-1;
-		Node node = list.get(last);
-		for (int i = 0; i<last; i++){
-			if (list.get(i).equals(node)){
-				return true;
-			}
-		}
-		return false;
-	} 
-	
-	
-	
 	
 	
 	/**************************************
@@ -188,6 +198,143 @@ public class Visit {
 	/**
 	 * Test whether path loops 
 	 */
+	boolean nloop(Regex exp, Node start) {
+		if (isReverse){
+			ninsert(exp, start);
+			if (ttable.loop(exp, start)){
+				ttable.remove(exp, start);
+				return true;
+			}
+		}
+		else if (ttable.loop(exp, start)){
+			return true;
+		}
+		else {
+			ninsert(exp, start);
+		}
+	
+		return false;
+	}
+	
+	void ninsert(Regex exp, Node start){
+		if (start == null){
+			// subscribe exp to start() above because start node is not bound yet
+			list.add(exp);
+		}
+		else {
+			ttable.add(exp, start);
+		}
+	}
+	
+
+	
+	boolean knows(Regex exp){
+		return list.contains(exp);
+	}
+	
+	void nset(Regex exp, TTable t){
+		if (t!=null){
+			ttable.put(exp, t); 
+		}
+	}
+	
+	TTable nunset(Regex exp){
+		TTable t = ttable.get(exp);
+		ttable.remove(exp);
+		return t;
+	}
+	
+	
+	
+	
+	/**
+	 * use case:
+	 * ?x p* ?y
+	 * ?x is not bound when path starts
+	 * first occurrence of p binds ?x
+	 * node is ?x
+	 * exp is p*
+	 * add node to the visited list of exp to prevent loops
+	 */
+	void nstart(Node node){
+		for (Regex exp : list){
+			ttable.add(exp, node);
+		}
+	}
+	
+
+	
+	/**
+	 * same use case as start() above
+	 * leave the binding
+	 */
+	void nleave(Node node){
+		for (Regex exp : list){
+			ttable.remove(exp, node);
+		}
+	}
+
+
+	
+	/**
+	 * Remove node from visited list
+	 */
+	void nremove (Regex exp, Node node){
+		if (node == null){
+			list.remove(exp);
+			return;
+		}
+		
+		ttable.remove(exp, node);
+	}
+	
+	
+	 void set(Regex exp, int n){
+		 ctable.put(exp, n);
+	 }
+
+	 void count(Regex exp, int n){
+		 Integer i = ctable.get(exp);
+		 if (i == null) i = 0;
+		 ctable.put(exp, i + n);
+	 }
+
+
+	 int count(Regex exp){
+		 Integer i = ctable.get(exp);
+		 if (i == null) return 0;
+		 return i;
+	 }
+	 
+	 /****************************************/
+	 
+	 /**
+	  * ?x distinct(exp+) ?y
+	  */
+	 boolean isDistinct(Node start, Node node){
+		 Table t = tdistinct.get(start);
+		 if (t == null) return true;
+		 return ! t.containsKey(node);
+	 }
+	 
+	 void addDistinct(Node start, Node node){
+		 Table t = tdistinct.get(start);
+		 if (t == null){
+			 t = new Table();
+			 tdistinct.put(start, t);
+		 }
+		 t.put(node, node);
+	 }
+	 
+	 
+	
+	
+	
+	/*************************************
+	 * Old version
+	 */
+	
+	
 	boolean loop(Regex exp, Node start) {
 		if (isReverse){
 			insert(exp, start);
@@ -206,6 +353,29 @@ public class Visit {
 		return false;
 	}
 	 
+	void start(Node node){
+		for (Regex exp : list){
+			add(exp, node);
+		}
+	}
+	void leave(Node node){
+		for (Regex exp : list){
+			remove(exp, node);
+		}
+	}
+	
+	void set(Regex exp, Table t){
+		if (t!=null){
+			eetable.put(exp, t); 
+		}
+	}
+	
+	Table unset(Regex exp){
+		Table t = eetable.get(exp);
+		eetable.remove(exp);
+		return t;
+	}
+	
 	void insert(Regex exp, Node start){
 		if (start == null){
 			// subscribe exp to start() above because start node is not bound yet
@@ -249,55 +419,6 @@ public class Visit {
 		table.put(node, node);
 	}
 
-	
-	boolean knows(Regex exp){
-		return list.contains(exp);
-	}
-	
-
-	
-	void set(Regex exp, Table t){
-		if (t!=null){
-			eetable.put(exp, t); 
-		}
-	}
-	
-	Table unset(Regex exp){
-		Table t = eetable.get(exp);
-		eetable.remove(exp);
-		return t;
-	}
-	
-	/**
-	 * use case:
-	 * ?x p* ?y
-	 * ?x is not bound when path starts
-	 * first occurrence of p binds ?x
-	 * node is ?x
-	 * exp is p*
-	 * add node to the visited list of exp to prevent loops
-	 */
-	void start(Node node){
-		for (Regex exp : list){
-			add(exp, node);
-		}
-	}
-	
-	/**
-	 * same use case as start() above
-	 * leave the binding
-	 */
-	void leave(Node node){
-		for (Regex exp : list){
-			remove(exp, node);
-		}
-	}
-	
-
-	
-	/**
-	 * Remove node from visited list
-	 */
 	void remove (Regex exp, Node node){
 		if (node == null){
 			list.remove(exp);
@@ -377,42 +498,107 @@ public class Visit {
 	 } 
 	 
 	 
-	 void set(Regex exp, int n){
-		 ctable.put(exp, n);
-	 }
+	
+	 
+	 
+		/*********************
+	 *
+	 * States are deprecated
+	 */
+	List<Node> get(State state){
+		return table.get(state);
+	}
+	
+	void put(State state, List<Node> list){
+		table.put(state, list);
+	}
 
-	 void count(Regex exp, int n){
-		 Integer i = ctable.get(exp);
-		 if (i == null) i = 0;
-		 ctable.put(exp, i + n);
-	 }
+	public void add(State state, Node node) {
+		List<Node> list = get(state);
+		if (list == null){
+			list = new ArrayList<Node>();
+			put(state, list);
+		}
+		list.add(node);
+	}
 
+	public void remove(State state, Node node) {
+		List<Node> list = get(state);
+		if (! list.get(list.size()-1).equals(node)){
+			System.out.println("** ERROR Visit: " + node + " " + list);
+		}
+		list.remove(list.size()-1);
+	}
 
-	 int count(Regex exp){
-		 Integer i = ctable.get(exp);
-		 if (i == null) return 0;
-		 return i;
-	 }
-	 
-	 /****************************************/
-	 
-	 /**
-	  * ?x distinct(exp+) ?y
-	  */
-	 boolean isDistinct(Node start, Node node){
-		 Table t = tdistinct.get(start);
-		 if (t == null) return true;
-		 return ! t.containsKey(node);
-	 }
-	 
-	 void addDistinct(Node start, Node node){
-		 Table t = tdistinct.get(start);
-		 if (t == null){
-			 t = new Table();
-			 tdistinct.put(start, t);
-		 }
-		 t.put(node, node);
-	 }
-	 
+	
+	/**
+	 * Check whether the last visited node has already been 
+	 * visited as an intermediate node 
+	 * use case: 
+	 * ?x p{2,} ?y   :  skip the first two nodes (no loop with two first)
+	 * ?x p{2,} ex:uri : walk backward through the list 
+	 * @deprecated
+	 * 
+	 */
+	public boolean loop(State state) {
+		if (! state.isCheckLoop()) return false;
+		
+		List<Node> list = get(state);
+		if (list == null || list.size()<2) return false;
+		int last = list.size()-1;
+		int min = 0;
+		if (state.isBound() && ! state.isPlus()) min = state.getMin();
+
+		if (isReverse){
+			// use case: ?s {2,} ex:uri
+			// Walk through the list backward (from <uri> to ?s), skip 2 first nodes
+			// cf W3C pp test case
+			Node node = null;
+			int count = 0;
+			for (int i = last; i>=0; i--){
+				if (count<min) {count++; continue;}
+				else if (count == min){ 
+					node = list.get(i);
+				}
+				else {
+					if (list.get(i).equals(node)){
+						return true;
+					}
+				}
+				count++;
+			}
+		}
+		else 
+		{
+			Node node = list.get(last);		
+			for (int i = 0; i<last; i++){
+				if (i<min) continue;
+				if (list.get(i).equals(node)){
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	} 
+
+	/**
+	 *  @deprecated
+	 */
+	public boolean loop2(State state) {
+		List<Node> list = get(state);
+		if (list == null || list.size()<2) return false;
+		int last = list.size()-1;
+		Node node = list.get(last);
+		for (int i = 0; i<last; i++){
+			if (list.get(i).equals(node)){
+				return true;
+			}
+		}
+		return false;
+	} 
+	
+	
+	
 
 }

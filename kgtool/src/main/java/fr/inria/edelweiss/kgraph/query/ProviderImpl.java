457eda5ac9ad1ca99d9c1bea57293007569893d8
Olivier Corby <olivier.corby@inria.fr>
2012-04-13 07:41:26 +0000

    service on sparql endpoint

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/ProviderImpl.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/ProviderImpl.java
@@ -1,11 +1,33 @@
 package fr.inria.edelweiss.kgraph.query;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import fr.inria.acacia.corese.api.IDatatype;
 import fr.inria.acacia.corese.triple.parser.ASTQuery;
+import fr.inria.acacia.corese.triple.parser.Constant;
+import fr.inria.acacia.corese.triple.parser.Variable;
+import fr.inria.edelweiss.kgenv.result.XMLResult;
 import fr.inria.edelweiss.kgram.api.core.Node;
+import fr.inria.edelweiss.kgram.api.query.Environment;
 import fr.inria.edelweiss.kgram.api.query.Provider;
 import fr.inria.edelweiss.kgram.core.Exp;
+import fr.inria.edelweiss.kgram.core.Mapping;
 import fr.inria.edelweiss.kgram.core.Mappings;
 import fr.inria.edelweiss.kgram.core.Query;
 import fr.inria.edelweiss.kgraph.core.Graph;
@@ -15,14 +37,23 @@ import fr.inria.edelweiss.kgraph.core.Graph;
 
 
 /**
- * Implements service expression using local QueryExec
+ * Implements service expression 
+ * There may be local QueryProcess for some URI (use case: W3C test case)
+ * Send query to sparql endpoint using HTTP POST query
+ * There may be a default QueryProcess
+ * 
+ * TODO:
+ * check use same ProducerImpl to generate Nodes ?
  * 
  * @author Olivier Corby, Edelweiss INRIA 2011
  *
  */
 public class ProviderImpl implements Provider {
 	
+	private static Logger logger = Logger.getLogger(ProviderImpl.class);	
+
 	HashMap<String, QueryProcess> table;
+	QueryProcess defaut;
 	
 	ProviderImpl(){
 		table = new HashMap<String, QueryProcess>();
@@ -32,25 +63,187 @@ public class ProviderImpl implements Provider {
 		return new ProviderImpl();
 	}
 	
+	/**
+	 * Define a QueryProcess for this URI
+	 */
 	public void add(String uri, Graph g){
 		QueryProcess exec = QueryProcess.create(g);
 		exec.set(this);
 		table.put(uri, exec);
 	}
+	
+	/**
+	 * Define a default QueryProcess
+	 */
+	public void add(Graph g){
+		QueryProcess exec = QueryProcess.create(g);
+		exec.set(this);
+		defaut = exec;
+	}
 
-	public Mappings service(Node serv, Exp exp) {
-		QueryProcess exec = table.get(serv.getLabel());
+	/**
+	 * If there is a QueryProcess for this URI, use it
+	 * Otherwise send query to spaql endpoint
+	 * If endpoint fails, use default QueryProcess if it exists
+	 */
+	public Mappings service(Node serv, Exp exp, Environment env) {
 		Query q = exp.getQuery();
-		Mappings map;
+		
+		QueryProcess exec = table.get(serv.getLabel());
+
 		if (exec == null){
-			map = Mappings.create(q);
+
+			Mappings map = send(serv, q, env);
+			if (map != null){
+				return map;
+			}
+
+			if (defaut == null){
+				map =  Mappings.create(q);
+				if (q.isSilent()){
+					map.add(Mapping.create());
+				}
+				return map;
+			}
+			else {
+				exec = defaut;
+			}
 		}
-		else {
-			ASTQuery ast = exec.getAST(q);
-			map = exec.query(ast);
-		}
+		
+		ASTQuery ast = exec.getAST(q);
+		Mappings map = exec.query(ast);
+		
 		return map;
 	}
+	
+	/**
+	 * Send query to sparql endpoint using a POST HTTP query
+	 */
+	Mappings send(Node serv, Query q, Environment env){
+		try {
+			bindings(q, env);
+			ASTQuery ast = (ASTQuery) q.getAST();
+			String query = ast.toString();
+			//System.out.println("** PI: " + query);
+			StringBuffer sb = doPost(serv.getLabel(), query);
+			Mappings map = parse(sb);
+			return map;
+		} catch (IOException e) {
+			logger.error(e);
+		} catch (ParserConfigurationException e) {
+			logger.error(e);
+		} catch (SAXException e) {
+			logger.error(e);
+		}
+		return null;
+	}
+	
+	/**
+	 * Search select variable of query that is bound in env
+	 * Generate binding for such variable
+	 * Set bindings in ASTQuery
+	 */
+	void bindings(Query q, Environment env){
+		ASTQuery ast = (ASTQuery) q.getAST();
+		ast.clearBindings();
+		ArrayList<Variable> lvar = new ArrayList<Variable>();
+		ArrayList<Constant> lval = new ArrayList<Constant>();
+
+		for (Node qv : q.getSelect()){
+			String var = qv.getLabel();
+			Node val   = env.getNode(var);
+			
+			if (val != null){
+				lvar.add(Variable.create(var));
+				IDatatype dt = (IDatatype) val.getValue();
+				Constant cst = Constant.create(dt);
+				lval.add(cst);
+			}
+		}
+		
+		if (lvar.size()>0){
+			ast.setVariableBindings(lvar);
+			ast.setValueBindings(lval);
+		}
+	}
+	
+	
+	/**********************************************************************
+	 * 
+	 * SPARQL Protocol client
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws ParserConfigurationException 
+	 * 
+	 */
+	
+	
+	Mappings parse(StringBuffer sb) throws ParserConfigurationException, SAXException, IOException{
+		ProducerImpl p  = ProducerImpl.create(Graph.create());
+		XMLResult r 	= XMLResult.create(p);
+		Mappings map 	= r.parseString(sb.toString());
+		return map;
+	}
+	
+	
+	public StringBuffer doPost(String server, String query) throws IOException{
+		URLConnection cc = post(server, query);
+		return getBuffer(cc.getInputStream());
+	}
+	
+	URLConnection post(String server, String query) throws IOException{
+		String qstr = "query=" + URLEncoder.encode(query, "UTF-8");
+
+		URL queryURL = new URL(server);
+        HttpURLConnection urlConn = (HttpURLConnection) queryURL.openConnection();
+        urlConn.setRequestMethod("POST"); 
+        urlConn.setDoOutput(true);
+        urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        urlConn.setRequestProperty("Accept", "application/rdf+xml, text/xml");
+        urlConn.setRequestProperty("Content-Length", String.valueOf(qstr.length()));
+        
+        OutputStreamWriter out = new OutputStreamWriter(urlConn.getOutputStream());
+        out.write(qstr);
+        out.flush();
+        
+        return urlConn;
+
+	}
+	
+	
+	
+	StringBuffer getBuffer(InputStream stream) throws IOException{
+		InputStreamReader r = new InputStreamReader(stream);
+		BufferedReader br = new BufferedReader(r);
+		StringBuffer sb = new StringBuffer();
+
+		String str = null;
+		while ((str = br.readLine()) != null){
+			sb.append(str);
+			sb.append("\n");
+		}
+		
+		return sb;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 //	public String callSoapEndPoint() {
 //		SparqlSoapClient client = new SparqlSoapClient();

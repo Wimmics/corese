fc6ce29033694b6b2f07b20445c4917f14de4d53
Olivier Corby <olivier.corby@inria.fr>
2012-04-23 16:13:35 +0000

    expand is done in a Visitor
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@825 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/CompileService.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/CompileService.java
@@ -5,12 +5,8 @@ import java.util.Hashtable;
 
 import fr.inria.acacia.corese.api.IDatatype;
 import fr.inria.acacia.corese.triple.parser.ASTQuery;
-import fr.inria.acacia.corese.triple.parser.Atom;
 import fr.inria.acacia.corese.triple.parser.BasicGraphPattern;
 import fr.inria.acacia.corese.triple.parser.Constant;
-import fr.inria.acacia.corese.triple.parser.Exp;
-import fr.inria.acacia.corese.triple.parser.Expression;
-import fr.inria.acacia.corese.triple.parser.Or;
 import fr.inria.acacia.corese.triple.parser.Term;
 import fr.inria.acacia.corese.triple.parser.Triple;
 import fr.inria.acacia.corese.triple.parser.Variable;
@@ -20,8 +16,7 @@ import fr.inria.edelweiss.kgram.core.Query;
 
 public class CompileService {
 	
-	static final String ROOT = "?_VAR_";
-	static final int MAX = 20;
+
 	
 	Hashtable<String, Double> table;
 	
@@ -30,13 +25,12 @@ public class CompileService {
 	}
 
 	void compile(Node serv, Query q, Environment env){
+		Query g 	 = q.getOuterQuery();
 		ASTQuery ast = (ASTQuery) q.getAST();
-		Query g = q.getOuterQuery();
 		ASTQuery ag  = (ASTQuery) g.getAST();
 		ast.setPrefixExp(ag.getPrefixExp());
 		
 		if (isSparql0(serv)){
-			cast(ast);
 			filter(q, env);
 		}
 		else {
@@ -120,71 +114,6 @@ public class CompileService {
 	}
 	
 	
-	/**
-	 * cast path to sparql 1.0
-	 */
-	void cast(ASTQuery ast){
-		Exp body = ast.getBody();
-		for (int i=0; i<body.size(); i++){
-			body.set(i, cast(body.get(i)));
-		}
-	}
-	
-	/**
-	 * Transform exp to SPARQL 1.0
-	 * foaf:knows+ -> union foaf:knows[foaf:knows ?y]
-	 */
-	Exp cast(Exp exp){
-		if (exp.isTriple()){
-			Triple t = exp.getTriple();
-			if (t.isPath()){
-				return cast(t);
-			}
-		}
-		return exp;
-	}
-
-	
-	Exp cast(Triple t){
-		Expression regex = t.getRegex();
-		if (regex.isPlus() && regex.getArg(0).isConstant()){
-			Constant cst = regex.getArg(0).getConstant();
-			Exp exp = loop(t, cst);
-			return exp;
-		}
-		
-		return t;
-	}
-	
-	Exp loop(Triple t, Constant p){
-		Exp res = Triple.create(t.getArg(0), p, t.getArg(1));
-		
-		for (int i = 1; i<MAX; i++){
-			Exp exp = loop(t, p, i);
-			res = Or.create(res, exp);
-		}
-		
-		return res;
-		
-	}
-
-	Exp loop(Triple t, Constant p, int n){
-		Atom at = t.getArg(0);
-		BasicGraphPattern bgp = BasicGraphPattern.create();
-		
-		for (int i = 0; i<n; i++){
-			Variable var = Variable.create(ROOT + i);
-			var.setBlankNode(true);
-			Triple nt = Triple.create(at, p, var);
-			bgp.add(nt);
-			at = var;
-		}
-		
-		Triple nt = Triple.create(at, p, t.getArg(1));
-		bgp.add(nt);
-
-		return bgp;
-	}
 
 
 }

ea7ed260128de151fa245b50687fec4bd0bf17d7
Olivier Corby <olivier.corby@inria.fr>
2012-07-17 12:03:52 +0000

    Draft Visitor for QueryGraph
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@952 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/QueryGraphVisitor.java
@@ -0,0 +1,23 @@
+package fr.inria.edelweiss.kgraph.api;
+
+import fr.inria.acacia.corese.triple.parser.ASTQuery;
+import fr.inria.edelweiss.kgram.api.core.Entity;
+import fr.inria.edelweiss.kgram.core.Query;
+import fr.inria.edelweiss.kgraph.core.Graph;
+
+/**
+ * 
+ * Visitor Design Pattern to rewrite an RDF Graph into a SPARQL BGP Query Graph 
+ * 
+ */
+public interface QueryGraphVisitor {
+	
+	Graph visit(Graph g);
+	
+	ASTQuery visit(ASTQuery ast);
+	
+	Entity visit(Entity ent);
+
+	Query visit(Query q);
+
+}

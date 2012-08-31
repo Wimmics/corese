b347d408ab07045527de91f88fc27c459ce3c309
Olivier Corby <olivier.corby@inria.fr>
2012-08-31 12:35:12 +0000

    addSource to declare a graph that will log insert/delete
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1000 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/GraphListener.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/GraphListener.java
@@ -1,12 +1,15 @@
 package fr.inria.edelweiss.kgraph.api;
 
 import fr.inria.edelweiss.kgram.api.core.Entity;
+import fr.inria.edelweiss.kgraph.core.Graph;
 
 public interface GraphListener {
 	
-	void insert(Entity ent);
+	void addSource(Graph g);
+	
+	void insert(Graph g, Entity ent);
 
-	void delete(Entity ent);
+	void delete(Graph g, Entity ent);
 	
 	
 }

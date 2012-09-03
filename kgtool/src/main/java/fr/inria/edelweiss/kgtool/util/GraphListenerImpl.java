faf361d853b36118c5beb1f6a5303c4623ad7b51
Olivier Corby <olivier.corby@inria.fr>
2012-09-03 08:21:47 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1009 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgtool/util/GraphListenerImpl.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgtool/util/GraphListenerImpl.java
@@ -2,6 +2,7 @@ package fr.inria.edelweiss.kgtool.util;
 
 import fr.inria.edelweiss.kgram.api.core.Entity;
 import fr.inria.edelweiss.kgraph.api.GraphListener;
+import fr.inria.edelweiss.kgraph.core.Graph;
 
 public class GraphListenerImpl implements GraphListener {
 	
@@ -9,12 +10,16 @@ public class GraphListenerImpl implements GraphListener {
 		return new GraphListenerImpl();
 	}
 
-	public void insert(Entity ent) {
+	public void insert(Graph g, Entity ent) {
 		System.out.println("insert: " + ent);
 	}
 
-	public void delete(Entity ent) {
+	public void delete(Graph g, Entity ent) {
 		System.out.println("delete: " + ent);
+	}
+
+	public void addSource(Graph g) {
+		
 	}
 	
 	

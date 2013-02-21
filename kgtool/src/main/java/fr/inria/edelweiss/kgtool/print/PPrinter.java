3c05084c75e77d52f2a324509b8a686a98807d0c
Olivier Corby <olivier.corby@inria.fr>
2013-02-21 08:44:07 +0000

    pprintall named template
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1235 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgtool/print/PPrinter.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgtool/print/PPrinter.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import fr.inria.acacia.corese.api.IDatatype;
 import fr.inria.acacia.corese.cg.datatype.DatatypeMap;
@@ -14,22 +15,33 @@ import fr.inria.edelweiss.kgenv.parser.NodeImpl;
 import fr.inria.edelweiss.kgenv.parser.Pragma;
 import fr.inria.edelweiss.kgram.api.core.Expr;
 import fr.inria.edelweiss.kgram.api.core.Node;
+import fr.inria.edelweiss.kgram.api.query.Producer;
+import fr.inria.edelweiss.kgram.core.Exp;
 import fr.inria.edelweiss.kgram.core.Mapping;
 import fr.inria.edelweiss.kgram.core.Mappings;
 import fr.inria.edelweiss.kgram.core.Query;
 import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.query.ProducerImpl;
 import fr.inria.edelweiss.kgraph.query.QueryEngine;
 import fr.inria.edelweiss.kgraph.query.QueryProcess;
 import fr.inria.edelweiss.kgraph.rule.Rule;
 import fr.inria.edelweiss.kgraph.rule.RuleEngine;
 import fr.inria.edelweiss.kgtool.load.Load;
+import fr.inria.edelweiss.kgtool.load.LoadException;
 
 /**
  * SPARQL-based RDF AST Pretty Printer
- * Use case: pprint SPIN RDF graph in SPARQL concrete syntax
+ * Use case: 
+ * pprint SPIN RDF in SPARQL concrete syntax
+ * pprint OWL 2 RDF in functional syntax
+ * Use a list of templates : 
+ *   template { presentation } where { pattern }
+ * Templates are loaded from a directory or from a file in .rul format (same as rules)
+ * Templates can also be defined using defTemplate()
+ * If called with kg:pprint(?x) :    execute one template on ?x
+ * If called with kg:pprintAll(?x) : execute all templates on ?x
  * 
- * 
- * Olivier Corby, Wimmics INRIA 2012
+ * Olivier Corby, Wimmics INRIA I3S - 2012
  */
 public class PPrinter {
 	
@@ -37,7 +49,8 @@ public class PPrinter {
 	public  static final String PPRINTER = "/home/corby/workspace/kgengine/src/test/resources/data/pprint/asttemplate";
 	private static final String OUT = ASTQuery.OUT;
 	private static final String IN  = ASTQuery.IN;
-	
+	private static String NL = System.getProperty("line.separator");
+
 	Graph graph, fake;
 	QueryEngine qe;
 	Query query;
@@ -47,16 +60,21 @@ public class PPrinter {
 	Stack stack;
 	
 	String pp = PPRINTER;
-	
-	boolean isDebug = ! true;
+	String separator = NL;
+	boolean isDebug = false;
 	private IDatatype EMPTY;
 	boolean isTurtle = false;
+	int nbt = 0, max = 0;
+	private String START = Exp.KGRAM + "start";
+	HashMap<Query,Integer> tcount;
+	private boolean isHide = false;
+	public boolean stat = !true;
 
 	/**
 	 * 
 	 * Keep track of nodes already printed to prevent loop
-	 * Variant: may check the pair (dt, query) 
-	 *   do not to loop on the same query but may use several queries on same node dt
+	 * Variant: check the pair (dt, template) 
+	 *   do not to loop on the same template ; may use several templates on same node dt
 	 * 
 	 */
 	class Stack {
@@ -109,6 +127,11 @@ public class PPrinter {
 		}
 		
 		boolean contains(IDatatype dt, Query q){
+			ArrayList<Query> qlist = map.get(dt);
+			return qlist != null && qlist.contains(q);
+		}
+		
+		boolean contains2(IDatatype dt, Query q){
 			boolean b = list.contains(dt);
 			if (b && multi){
 				ArrayList<Query> qlist = map.get(dt);
@@ -116,6 +139,26 @@ public class PPrinter {
 			}
 			return b;
 		}
+		
+		
+		public String toString(){
+			StringBuilder sb = new StringBuilder();
+			int i = 0;
+			for (IDatatype dt : list){
+				sb.append(i++);
+				sb.append(" ");
+				sb.append(dt);
+				sb.append(": ");
+				for (Query q : map.get(dt)){
+					sb.append(name(q));
+					sb.append(" ");
+				}
+				sb.append("\n");
+			}
+			return sb.toString();
+		}
+		
+		
 	}
 	
 	
@@ -124,14 +167,31 @@ public class PPrinter {
 		graph = g;
 		fake = Graph.create();
 		pp = p;
-		init(graph, p);
+		exec = QueryProcess.create(g, true);		
+		tune(exec);				
+		init();
 		nsm = NSManager.create();
 		stack = new Stack(true);
-		exec = QueryProcess.create(g, true);
 		EMPTY = DatatypeMap.createLiteral(NULL);
+		tcount = new HashMap<Query,Integer> ();
 	}
 	
 	
+	private void tune(QueryProcess exec) {
+		// do not use Thread in Property Path
+		// compute all path nodes and put them in a list
+		// it is faster
+		exec.setListPath(true);
+		Producer prod = exec.getProducer();
+		if (prod instanceof ProducerImpl){
+			// return value as is for kg:pprint()
+			// no need to create a graph node in Producer
+			ProducerImpl pi = (ProducerImpl) prod;
+			pi.setSelfValue(true);
+		}		
+	}
+
+
 	public static PPrinter create(Graph g){
 		return new PPrinter(g, null);
 	}
@@ -152,9 +212,22 @@ public class PPrinter {
 		isTurtle = b;
 	}
 	
+	public void setSeparator(String s){
+		separator = s;
+	}
+	
+	public int nbTemplates(){
+		return nbt;
+	}
+	
 	public String toString(){
 		IDatatype dt = pprint();
 		return dt.getLabel();
+	}
+	
+	public StringBuilder toStringBuilder(){
+		IDatatype dt = pprint();
+		return dt.getStringBuilder();
 	}
 	
 	public void write(String name) throws IOException {				
@@ -181,20 +254,45 @@ public class PPrinter {
 	 * the subpart that matches the first query
 	 */
 	public IDatatype pprint(){
+		return pprint(false);
+	}
+	
+	
+	public IDatatype pprint(boolean all){
 		query = null;
-		for (Query qq : qe.getQueries()){
+		ArrayList<IDatatype> result = new ArrayList<IDatatype>();
+		
+		List<Query> list = getTemplate(START);
+		if (list.size() == 0){
+			list = qe.getTemplates();
+		}		
+		
+		for (Query qq : list){
 			
 			qq.setPPrinter(this);
 			// remember start with qq for function pprint below
 			query = qq;
 			Mappings map = exec.query(qq);
+			
 			query = null;
 			Node res = map.getNode(OUT);
 			if (res != null){
-				return  (IDatatype) res.getValue();
+				if (all){
+					result.add((IDatatype) res.getValue());
+				}
+				else {
+					return  (IDatatype) res.getValue();
+				}
 			}
 		}
+		
 		query = null;
+		
+		if (all){
+			IDatatype res = result(result);
+			return res;
+		}
+		
 		return EMPTY;
 	}
 	
@@ -202,32 +300,47 @@ public class PPrinter {
 		return stack.size();
 	}
 	
+	public int maxLevel(){
+		return max;
+	}
 
+	public IDatatype pprint(Expr exp, IDatatype dt){	
+		return pprint(exp, dt, null, false);
+	}
+	
+	
 	/**
-	 * Object o is the node where to apply the query (with a Mapping)
+	 * IDatatype dt is the focus node to be printed
 	 * use case: 
-	 * select (kg:pprint(?w) as ?pw) where {?q ast:where ?w}
-	 * Search a query that matches ?w
-	 * By convention, ?w is bound to ?in, all queries have ?in as input node
+	 * template { ... ?w ... } where {?in ast:where ?w}
+	 * Search a template that matches ?w
+	 * By convention, ?w is bound to ?in, all templates use variable ?in as focus node
 	 * and ?out as output node
-	 * Compute the first query that matches ?w
-	 * Queries are sorted  more "specific" first: construct/select/ask/describe
+	 * Execute the first template that matches ?w (all templates if all = true)
+	 * Templates are sorted  more "specific" first
 	 * They are sorted using a pragma { kg:query kg:priority n }
-	 * One rule is applied only once on one node, 
-	 * hence we store in a stack : node -> rule
+	 * A template is applied only once on one node, 
+	 * hence we store in a stack : node -> template
 	 */
-	public IDatatype pprint(Expr exp, IDatatype dt){		
+	public IDatatype pprint(Expr exp, IDatatype dt, String temp, boolean all){	
+		
 		if (dt == null){
 			return EMPTY;
 		}
 		
+		ArrayList<IDatatype> result = null;
+		if (all) {
+			result = new ArrayList<IDatatype>();
+		}
 		boolean start = false;
+		
 		if (query != null && stack.size() == 0){
 			// just started with query in pprint() above
-			if (exp != null  && exp.getLabel().equals(IN)){
+			// without focus node at that time
+			if (exp != null){ //  && exp.getLabel().equals(IN)){
 				// current variable is ?in, 
 				// push dt -> query in the stack
-				// query is the first query that started pprint (see function above)
+				// query is the first template that started pprint (see function above)
 				// and at that time ?in was not bound
 				// use case:
 				// template {"subClassOf(" ?in " " ?y ")"} where {?in rdfs:subClassOf ?y}
@@ -239,10 +352,7 @@ public class PPrinter {
 		if (isDebug){
 			System.out.println("pprint: " + dt);
 		}
-		
-		// to prevent infinite loop in the case where the graph is cyclic
-		// should not happen with RDF AST
-		
+				
 		Graph g = graph;									
 		QueryProcess exec = this.exec;
 
@@ -254,40 +364,157 @@ public class PPrinter {
 		}
 		Mapping m = Mapping.create(qn, n);
 
-		for (Query qq : qe.getQueries()){
-			
-			// Tricky: All queries of this PPrinter share the same query base (see PluginImpl)
+		int count = 0;
+		for (Query qq : getTemplates(temp)){
+						
+			// Tricky: All templates of this PPrinter share the same PPrinter (see PluginImpl)
 			qq.setPPrinter(this);
 			
-			if (! stack.contains(dt, qq)){
+			if (! qq.isFail() && ! stack.contains(dt, qq)){
+				
+				nbt++;
 
-				stack.push(dt, qq);
-				Mappings map = exec.query(qq, m);
-				stack.pop();
-				Node res = map.getNode(OUT);
-
-				if (res != null){
-					if (start){
-						stack.pop();
-					}
-					return  (IDatatype) res.getValue();
+				if (all) {
+					count ++;
 				}
+				stack.push(dt, qq);
+				if (stack.size() > max){
+					max = stack.size();
+				}
+				
+				if (stat ){
+					incr(qq);
+				}
+				
+				Mappings map = exec.query(qq, m);				
+				
+//				if (! qq.isAggregate()){
+//					System.out.println(map.size());
+//				}
+				
+				if (! all){
+					// if execute all templates, keep them in the stack 
+					// to prevent loop same template on same focus node
+					stack.pop();
+				}
+				
+				Node res = map.getNode(OUT);
+								
+				if (res != null){
+
+					if (stat){
+						succ(qq);
+					}
+					
+					if (isDebug){
+						trace(qq, res);
+					}
+					
+					if (all){
+						result.add((IDatatype) res.getValue());
+					}
+					else {
+						if (start){
+							stack.pop();
+						}
+						return  (IDatatype) res.getValue();
+					}
+				}
+			}
+		}
+		
+		if (all){
+			// gather results of several templates
+			
+			for (int i=0; i<count; i++){
+				// pop the templates that have been executed
+				stack.pop();
+			}
+			
+			if (result.size() > 0){
+				
+				if (start){
+					stack.pop();
+				}
+				
+				IDatatype res = result(result);
+				return res;
 			}
 		}
 		
 		if (start){
 			stack.pop();
 		}	
-		// no query match dt; it may be a constant		
+		
+		// no template match dt
+		
+		if (temp != null){
+			// named template fail
+			return EMPTY;
+		}
+		
+		// default: display dt as is
 		return display(dt); 
 	}
+	
+	
+
+	private List<Query> getTemplates(String temp) {
+		if (temp == null){
+			return qe.getTemplates();
+		}
+		return getTemplate(temp);
+	}
+
+
+	private List<Query> getTemplate(String temp) {
+		Query q =  qe.getTemplate(temp);
+		ArrayList<Query> l = new ArrayList<Query>(1);
+		if (q != null){
+			l.add(q);
+		}
+		return l;	
+	}
+
+
+	/**
+	 * Concat results of several templates executed on same focus node
+	 * kg:pprintAll(?x)
+	 */
+	IDatatype result(List<IDatatype> result){
+		StringBuilder sb = new StringBuilder();
+
+		for (IDatatype d : result){
+			StringBuilder b = d.getStringBuilder();
+			
+			if (b!=null){
+				if (b.length()>0){
+					sb.append(b);
+					sb.append(separator) ;
+				}
+			}
+			else if (d.getLabel().length() > 0){
+				sb.append(d.getLabel());
+				sb.append(separator) ;
+			}
+		}
+		
+		IDatatype res = DatatypeMap.newStringBuilder(sb);
+		return res;
+	}
+	
+	
 	
 	
 	IDatatype display(IDatatype dt){
 		if (isTurtle){
 			return turtle(dt);
 		}
+		else if (isHide){
+			return EMPTY;
+		}
 		else {
+			// the label of dt as in SPARQL concat()
 			return dt;
 		}
 	}
@@ -302,11 +529,11 @@ public class PPrinter {
 			String qname = nsm.toPrefix(dt.getLabel(), true);
 			if (dt.getLabel().equals(qname)){
 				// no namespace, return <uri>
-				dt = DatatypeMap.newInstance(dt.toString());
+				dt = DatatypeMap.newStringBuilder(dt.toString());
 			}
 			else {
 				// return qname
-				dt = DatatypeMap.newInstance(qname);
+				dt = DatatypeMap.newStringBuilder(qname);
 			}
 		}
 		else if (dt.isLiteral()){
@@ -315,49 +542,139 @@ public class PPrinter {
 			}
 			else {
 				// add quotes around string, add lang tag if any
-				dt = DatatypeMap.newInstance(dt.toString());
+				dt = DatatypeMap.newStringBuilder(dt.toString());
 			}
 		}
 		
 		return dt;	
 	}
 	
-	
-	void init(Graph g, String str){
-		if (str == null){
-			qe = QueryEngine.create(g);
+	/**
+	 * Load templates from directory (.rq) or from a file (.rul)
+	 */
+	void init(){
+		if (pp == null){
+			qe = QueryEngine.create(graph);
 		}
 		else {
-			Load ld = Load.create(g);
-			ld.load(str);
+			Load ld = Load.create(graph);
+			try {
+				ld.loadWE(pp);
+			} catch (LoadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			qe = ld.getQueryEngine();
 
 			if (qe == null && ld.getRuleEngine() != null){
+
 				RuleEngine re = ld.getRuleEngine();
-				qe = QueryEngine.create(g);
+				qe = QueryEngine.create(graph);
 				for (Rule r : re.getRules()){
 					Query q = r.getQuery();
 					qe.defQuery(q);
 				}
 			}
-
-			if (qe == null){
-				qe = QueryEngine.create(g);
+			if (qe == null)  {
+				qe = QueryEngine.create(graph);
 			}
 			
 			qe.sort();	
 		}
+		//trace();
+		
+//		if (isCheck()){
+//			check();
+//		}
 	}
 	
+	/***************************************************************
+	 * 
+	 * Check templates that would never succeed
+	 * 
+	 ***************************************************************/
+	
+	
+	/**
+	 * Check if a template edges not exist in graph
+	 * remove those templates from the list to speed up
+	 * PRAGMA: does not take RDFS entailments into account
+	 */
+	public void check(){
+		for (Query q : qe.getQueries()){
+			boolean b = graph.check(q);
+			if (! b){
+				q.setFail(true);
+			}
+			if (name(q).equals("list.rq")){
+				q.setFail(true);
+			}
+		}
+		qe.clean();
+		if (stat) {
+			trace();
+		}
+	}
+	
+	
+
 	
 	public void trace(){
 		for (Query q : qe.getQueries()){
 			if (q.hasPragma(Pragma.FILE)) {
-				System.out.println(q.getPragma(Pragma.FILE));
+				System.out.println(name(q));
 			}
 			ASTQuery ast = (ASTQuery) q.getAST();
 			System.out.println(ast);
 		}
 	}
 	
+	String name(Query qq){
+		String f =  qq.getStringPragma(Pragma.FILE);
+		if (f != null){
+			int index = f.lastIndexOf("/");
+			if (index != -1){
+				f = f.substring(index + 1);
+			}
+		}
+		return f;
+	}
+	
+	void trace(Query qq, Node res){
+		System.out.println();
+		System.out.println("query:  " + name(qq));
+		System.out.println("result: " + res);
+	}
+	
+	public void nbcall(){
+		for (Query q : qe.getQueries()){
+			System.out.println(q.getNumber() + " " + name(q) + " " + tcount.get(q));
+		}
+	}
+	
+	private void succ(Query q) {
+		Integer c = tcount.get(q);
+		if (c == null){
+			tcount.put(q, 1);
+		}
+		else {
+			tcount.put(q, c+1);
+		}
+	}
+
+
+	private void incr(Query qq) {
+		 qq.setNumber(qq.getNumber()+1);
+	}
+
+
+	public boolean isHide() {
+		return isHide;
+	}
+
+
+	public void setHide(boolean isHide) {
+		this.isHide = isHide;
+	}
+
 }

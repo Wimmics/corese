8e09dcac890f3b41c1740667622f7adf035cf35b
Alban Gaignar <alban.gaignard@i3s.unice.fr>
2013-02-21 14:47:30 +0000

    test for distributed SPARQL 1.1 path expression

--- kg-dqp/src/test/java/test/distribution/FoafSimpleTest.java
+++ kg-dqp/src/test/java/test/distribution/FoafSimpleTest.java
@@ -11,14 +11,7 @@ import fr.inria.acacia.corese.api.EngineFactory;
 import fr.inria.acacia.corese.api.IResultValue;
 import fr.inria.acacia.corese.api.IResults;
 import fr.inria.acacia.corese.exceptions.EngineException;
-import fr.inria.edelweiss.kgengine.GraphEngine;
 import fr.inria.edelweiss.kgdqp.core.QueryExecDQP;
-import fr.inria.edelweiss.kgram.core.Mappings;
-import fr.inria.edelweiss.kgraph.core.Graph;
-import fr.inria.edelweiss.kgraph.query.QueryProcess;
-import fr.inria.edelweiss.kgtool.load.Load;
-import fr.inria.edelweiss.kgtool.print.RDFFormat;
-import fr.inria.edelweiss.kgtool.print.XMLFormat;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -27,17 +20,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Map;
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
-import javax.xml.ws.BindingProvider;
 import org.apache.commons.lang.time.StopWatch;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import wsimport.KgramWS.RemoteProducer;
 import wsimport.KgramWS.RemoteProducerServiceClient;
 
@@ -61,8 +52,8 @@ public class FoafSimpleTest {
     @Before
     public void setUp() throws EngineException, MalformedURLException, IOException {
 
-        RemoteProducer kg1 = RemoteProducerServiceClient.getPort("http://localhost:8091/kgserver-1.0.6-kgram-webservice/RemoteProducerService.RemoteProducerServicePort");
-        RemoteProducer kg2 = RemoteProducerServiceClient.getPort("http://localhost:8092/kgserver-1.0.6-kgram-webservice/RemoteProducerService.RemoteProducerServicePort");
+        RemoteProducer kg1 = RemoteProducerServiceClient.getPort("http://neurolog.unice.fr:8091/kgserver-1.0.7-kgram-webservice/RemoteProducerService.RemoteProducerServicePort");
+        RemoteProducer kg2 = RemoteProducerServiceClient.getPort("http://neurolog.unice.fr:8092/kgserver-1.0.7-kgram-webservice/RemoteProducerService.RemoteProducerServicePort");
 
         kg1.initEngine();
         kg2.initEngine();
@@ -86,14 +77,6 @@ public class FoafSimpleTest {
         is.close();
         fw.close();
 
-//        Map<String, Object> reqCtxt1 = ((BindingProvider) kg1).getRequestContext();
-//        reqCtxt1.put(JAXWSProperties.MTOM_THRESHOLOD_VALUE, 1024);
-//        reqCtxt1.put(JAXWSProperties.HTTP_CLIENT_STREAMING_CHUNK_SIZE, 8192);
-//
-//        Map<String, Object> reqCtxt2 = ((BindingProvider) kg2).getRequestContext();
-//        reqCtxt2.put(JAXWSProperties.MTOM_THRESHOLOD_VALUE, 1024);
-//        reqCtxt2.put(JAXWSProperties.HTTP_CLIENT_STREAMING_CHUNK_SIZE, 8192);
-
         DataHandler data1 = new DataHandler(new FileDataSource(rep1));
         kg1.uploadRDF(data1);
         DataHandler data2 = new DataHandler(new FileDataSource(rep2));
@@ -101,7 +84,6 @@ public class FoafSimpleTest {
 
         rep1.delete();
         rep2.delete();
-
     }
 
     @After
@@ -112,29 +94,28 @@ public class FoafSimpleTest {
     //
 
     @Test
-    @Ignore
     public void remoteFoafQuery() throws EngineException, MalformedURLException, IOException {
 
         String sparqlSeqQuery = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
                 + "SELECT distinct ?y WHERE"
                 + "{"
-                //                + " <http://i3s/Alban> (foaf:knows/foaf:knows) ?y ."
-                //                + " <http://i3s/Simon> (foaf:knows+) ?y ."
-                //                + " <http://i3s/Alban> (foaf:knows+)/foaf:name ?y ."
-                //                                + " <http://i3s/Mireille> ^foaf:knows ?y "
-                //                + " <http://i3s/Alban> (foaf:knows+) ?y ."
-                //                + " <http://i3s/Alban> (foaf:knows*) ?y ."
+//                                + " <http://i3s/Alban> (foaf:knows/foaf:knows) ?y ."
+                                + " <http://i3s/Simon> (foaf:knows+) ?y ."
+//                                + " <http://i3s/Alban> (foaf:knows+)/foaf:name ?y ."
+//                                                + " <http://i3s/Mireille> ^foaf:knows ?y "
+//                                + " <http://i3s/Alban> (foaf:knows+) ?y ."
+//                                + " <http://i3s/Alban> (foaf:knows*) ?y ."
                 //                + " <http://i3s/Alban> (foaf:knows?) ?y ."
                 //                + " <http://i3s/Alban> foaf:knows{2,3} ?y ."
                 //                + " <http://i3s/Alban> foaf:knows{2,3}/foaf:name ?y ."
                 //                + " <http://i3s/Alban> (foaf:knows{3}) ?y ."
                 //                + " <http://i3s/Alban> (foaf:knows{3,}) ?y ."
-                //                + " <http://i3s/Alban> (foaf:knows{,2}) ?y ."
+//                                + " <http://i3s/Alban> (foaf:knows{,2}) ?y ."
                 //                                + " <http://i3s/Alban> (foaf:knows | foaf:knows/foaf:name){2,3} ?y ."
                 //                                + " <http://i3s/Tram> (^foaf:knows)+ ?y ."
                 //                                + " <http://i3s/Alban> ! (foaf:knows) ?y ." //OK
                 //                + " ?x foaf:givenname 'Alban'^^xsd:string ." //OK
-                + " <http://i3s/Alban> ! (foaf:knows | foaf:name | foaf:mbox) ?y ."
+//                + " <http://i3s/Alban> ! (foaf:knows | foaf:name | foaf:mbox) ?y ."
                 //                                + " <http://i3s/Alban> ! (foaf:knows | foaf:name / foaf:mbox ) ?y ." 
                 //                                + "FILTER( ?y ~ 'a')"
                 + "}";
@@ -152,18 +133,19 @@ public class FoafSimpleTest {
 
 //        QueryExec exec2 = QueryExec.create(engine);
         QueryExecDQP exec = QueryExecDQP.create(engine);
-        exec.addRemote(new URL("http://localhost:8091/kgserver-1.0.6-kgram-webservice/RemoteProducerService.RemoteProducerServicePort"));
-        exec.addRemote(new URL("http://localhost:8092/kgserver-1.0.6-kgram-webservice/RemoteProducerService.RemoteProducerServicePort"));
+        exec.addRemote(new URL("http://neurolog.unice.fr:8091/kgserver-1.0.7-kgram-webservice/RemoteProducerService.RemoteProducerServicePort"));
+        exec.addRemote(new URL("http://neurolog.unice.fr:8092/kgserver-1.0.7-kgram-webservice/RemoteProducerService.RemoteProducerServicePort"));
 
         StopWatch sw = new StopWatch();
         sw.start();
-        IResults res = exec.SPARQLQuery(sparqlSampleQuery);
+//        IResults res = exec.SPARQLQuery(sparqlSampleQuery);
+        IResults res = exec.SPARQLQuery(sparqlSeqQuery);
 
         System.out.println("--------");
         System.out.println("Results in " + sw.getTime() + "ms");
-        GraphEngine gEng = (GraphEngine) engine;
-        System.out.println("Graph size " + gEng.getGraph().size());
         String[] variables = res.getVariables();
+        
+        assertEquals(2, res.size());
 
         for (Enumeration<IResult> en = res.getResults(); en.hasMoreElements();) {
             IResult r = en.nextElement();
@@ -180,41 +162,5 @@ public class FoafSimpleTest {
                 }
             }
         }
-    }
-
-    @Test
-    @Ignore
-    public void testAsk() throws IOException, EngineException {
-        Graph graph = Graph.create();
-        QueryProcess exec = QueryProcess.create(graph);
-        
-        File rep1 = File.createTempFile("rep1", ".rdf");
-        FileWriter fw = new FileWriter(rep1);
-        InputStream is = FoafSimpleTest.class.getClassLoader().getResourceAsStream("kgram#1-persons.rdf");
-        int c;
-        while ((c = is.read()) != -1) {
-            fw.write(c);
-        }
-        is.close();
-        fw.close();
-        Load loader = Load.create(graph);
-        loader.load(rep1.getAbsolutePath());
-        
-        String query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
-                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
-                + "PREFIX xml: <http://www.w3.org/XML/1998/namespace>"
-                + "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
-                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-                + "PREFIX cos: <http://www.inria.fr/acacia/corese#>"
-                + "ask  { ?x foaf:givenname ?u } " ;
-        
-        Mappings map = exec.query(query);
-        XMLFormat res = XMLFormat.create(map);
-        RDFFormat resZ = RDFFormat.create(map);
-        System.out.println("");
-        System.out.println(res);
-        System.out.println("");
-        System.out.println(resZ);
     }
 }

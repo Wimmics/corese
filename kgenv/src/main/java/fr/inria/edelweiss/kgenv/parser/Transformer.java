b8b9e5414f8420910aea904d9a39baa3d8d72bd6
Olivier Corby <olivier.corby@inria.fr>
2013-03-11 09:54:00 +0000

    template separator

--- kgenv/src/main/java/fr/inria/edelweiss/kgenv/parser/Transformer.java
+++ kgenv/src/main/java/fr/inria/edelweiss/kgenv/parser/Transformer.java
@@ -165,10 +165,16 @@ public class Transformer implements ExpType {
 		
 		Query q = compile(ast);
 		q.setRule(ast.isRule());
+		
 		q.setTemplate(ast.isTemplate());
+		q.setAllResult(ast.isAllResult());
+		if (ast.getSeparator()!=null){
+			q.setPragma(Pragma.SEPARATOR, ast.getSeparator());
+		}
 		if (ast.getName() != null) {
 			q.setPragma(Pragma.NAME, ast.getName());
 		}
+		
 		q = transform(q, ast);
 		return q;
 	}

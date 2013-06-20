a69c69bad8936e914c18132ca0f1c7a9169254dd
Alban Gaignar <alban.gaignard@i3s.unice.fr>
2013-06-20 09:53:17 +0000

    small fixes

--- kgserver/src/main/java/fr/inria/edelweiss/kgramserver/webservice/EmbeddedJettyServer.java
+++ kgserver/src/main/java/fr/inria/edelweiss/kgramserver/webservice/EmbeddedJettyServer.java
@@ -4,11 +4,7 @@
  */
 package fr.inria.edelweiss.kgramserver.webservice;
 
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
-import java.net.URI;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
@@ -20,13 +16,15 @@ import org.apache.log4j.Logger;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
-
+import org.mortbay.jetty.webapp.WebAppContext;
 
 /**
  * Embedded HTTP server for Corese. Using Jetty implementation.
+ *
  * @author alban.gaignard@cnrs.fr
  */
 public class EmbeddedJettyServer {
+
     private static Logger logger = Logger.getLogger(EmbeddedJettyServer.class);
 
     public static void main(String args[]) throws Exception {
@@ -49,7 +47,7 @@ public class EmbeddedJettyServer {
                 System.exit(0);
             } else if (cmd.hasOption("p")) {
                 port = Integer.parseInt(cmd.getOptionValue("p"));
-            } else if (cmd.hasOption("v")) { 
+            } else if (cmd.hasOption("v")) {
                 logger.info("version 1.0.7");
                 System.exit(0);
             }
@@ -64,15 +62,27 @@ public class EmbeddedJettyServer {
         Server server = new Server(port);
         Context root = new Context(server, "/", Context.SESSIONS);
         root.addServlet(jerseyServletHolder, "/kgram/*");
-        logger.info("Starting Corese/KGRAM on http://localhost:"+port+"/kgram");
-        
+        logger.info("Starting Corese/KGRAM on http://localhost:" + port + "/kgram");
+
+//        WebAppContext webAppContext = new WebAppContext();
+//        webAppContext = new WebAppContext();
+//        String webAppDir = "src/main/webapp/";
+////        webAppContext.setDescriptor(webAppContext + "/WEB-INF/web.xml");
+//        webAppContext.setResourceBase(webAppDir);
+//        System.out.println("");
+//        System.out.println(webAppContext.getDescriptor());
+//        System.out.println(webAppContext.getResourceBase());
+//        System.out.println("");
+//        webAppContext.setContextPath("/runJetty");
+//        server.addHandler(webAppContext);
+
         server.start();
         server.join();
 
-        ClientConfig config = new DefaultClientConfig();
-        com.sun.jersey.api.client.Client client = com.sun.jersey.api.client.Client.create(config);
-        WebResource service = client.resource(new URI("http://localhost:8081/kgram"));
-//        // Fluent interfaces
-        System.out.println(service.path("sparql").path("reset").post(String.class).toString());
+//        ClientConfig config = new DefaultClientConfig();
+//        com.sun.jersey.api.client.Client client = com.sun.jersey.api.client.Client.create(config);
+//        WebResource service = client.resource(new URI("http://localhost:8081/kgram"));
+////        // Fluent interfaces
+//        System.out.println(service.path("sparql").path("reset").post(String.class).toString());
     }
 }

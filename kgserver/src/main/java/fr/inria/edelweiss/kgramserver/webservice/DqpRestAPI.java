c1a45accceac5a6aa38d1d106e9c072e27c9b234
Alban Gaignar <alban.gaignard@i3s.unice.fr>
2013-09-19 13:45:31 +0000

    added SERVICE provider and "xml results"
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1521 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgserver/src/main/java/fr/inria/edelweiss/kgramserver/webservice/DqpRestAPI.java
+++ kgserver/src/main/java/fr/inria/edelweiss/kgramserver/webservice/DqpRestAPI.java
@@ -4,11 +4,15 @@
  */
 package fr.inria.edelweiss.kgramserver.webservice;
 
+import fr.inria.edelweiss.kgdqp.core.ProviderImplV2;
 import fr.inria.edelweiss.kgdqp.core.QueryProcessDQP;
+import fr.inria.edelweiss.kgdqp.core.Util;
 import fr.inria.edelweiss.kgdqp.core.WSImplem;
+import fr.inria.edelweiss.kgram.api.query.Provider;
 import fr.inria.edelweiss.kgram.core.Mappings;
 import fr.inria.edelweiss.kgraph.core.Graph;
 import fr.inria.edelweiss.kgtool.print.JSONFormat;
+import fr.inria.edelweiss.kgtool.print.ResultFormat;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.ws.rs.FormParam;
@@ -31,14 +35,17 @@ public class DqpRestAPI {
 
     private Logger logger = Logger.getLogger(DqpRestAPI.class);
     private static Graph graph = Graph.create(false);
-    private static QueryProcessDQP execDQP = QueryProcessDQP.create(graph);
+    private static Provider sProv = ProviderImplV2.create();
+    private static QueryProcessDQP execDQP = QueryProcessDQP.create(graph, sProv);
 
     @POST
     @Path("/reset")
     public Response resetDQP(@FormParam("endpointUrl") String endpointURLs) {
         //reset the query process DQP
         try {
-            DqpRestAPI.execDQP = QueryProcessDQP.create(graph);
+            DqpRestAPI.graph = Graph.create(false);
+            DqpRestAPI.sProv = ProviderImplV2.create();
+            DqpRestAPI.execDQP = QueryProcessDQP.create(graph, sProv);
             return Response.status(200).header("Access-Control-Allow-Origin", "*").entity("Reinitialized KGRAM-DQP federation engine").build();
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -54,7 +61,7 @@ public class DqpRestAPI {
 //    @Consumes(MediaType.APPLICATION_JSON)
 //    public Response addDataSource(@FormParam("endpointUrl") List<String> endpointURLs) {
     public Response addDataSource(@FormParam("endpointUrl") String endpointURLs) {
-        
+
         if ((endpointURLs == null) || (endpointURLs.isEmpty())) {
             return Response.status(200).header("Access-Control-Allow-Origin", "*").entity("Empty list of data sources !").build();
         }
@@ -63,7 +70,7 @@ public class DqpRestAPI {
         try {
             execDQP.addRemote(new URL(endpointURLs), WSImplem.REST);
             output += endpointURLs;
-            output += " added to the federation engine";    
+            output += " added to the federation engine";
             return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(output).build();
         } catch (MalformedURLException ex) {
             ex.printStackTrace();
@@ -75,6 +82,11 @@ public class DqpRestAPI {
     @Produces("application/sparql-results+json")
     @Path("/sparql")
     public Response getTriplesJSONForGet(@QueryParam("query") String query) {
+        
+        QueryProcessDQP.queryCounter.clear();
+        QueryProcessDQP.queryVolumeCounter.clear();
+        QueryProcessDQP.sourceCounter.clear();
+                
         logger.info("Federated querying: " + query);
 
 
@@ -82,10 +94,49 @@ public class DqpRestAPI {
             StopWatch sw = new StopWatch();
             sw.start();
             Mappings map = execDQP.query(query);
-            logger.info(map.size()+" results in " + sw.getTime() + "ms");
-            
+            logger.info(map.size() + " results in " + sw.getTime() + "ms");
+            System.out.println("");
+            System.out.println("***********************************************************");
+            System.out.println("***********************************************************");
+            System.out.println("Remote queries");
+            System.out.println(Util.prettyPrintCounter(QueryProcessDQP.queryCounter));
+            System.out.println("Transferred results per query");
+            System.out.println(Util.prettyPrintCounter(QueryProcessDQP.queryVolumeCounter));
+            System.out.println("Remote queries per data source");
+            System.out.println(Util.prettyPrintCounter(QueryProcessDQP.sourceCounter));
+
 
             return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(JSONFormat.create(map).toString()).build();
+        } catch (Exception ex) {
+            logger.error("Error while querying the remote KGRAM-DQP engine");
+            ex.printStackTrace();
+            return Response.status(500).header("Access-Control-Allow-Origin", "*").entity("Error while querying the remote KGRAM engine").build();
+        }
+    }
+
+    @GET
+    @Produces("application/sparql-results+xml")
+    @Path("/sparql")
+    public Response getTriplesXMLForGet(@QueryParam("query") String query) {
+        logger.info("Federated querying: " + query);
+
+
+        try {
+            StopWatch sw = new StopWatch();
+            sw.start();
+            Mappings map = execDQP.query(query);
+            logger.info(map.size() + " results in " + sw.getTime() + "ms");
+            System.out.println("");
+            System.out.println("***********************************************************");
+            System.out.println("***********************************************************");
+            System.out.println("Remote queries");
+            System.out.println(Util.prettyPrintCounter(QueryProcessDQP.queryCounter));
+            System.out.println("Transferred results per query");
+            System.out.println(Util.prettyPrintCounter(QueryProcessDQP.queryVolumeCounter));
+            System.out.println("Remote queries per data source");
+            System.out.println(Util.prettyPrintCounter(QueryProcessDQP.sourceCounter));
+
+            return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(ResultFormat.create(map).toString()).build();
         } catch (Exception ex) {
             logger.error("Error while querying the remote KGRAM-DQP engine");
             ex.printStackTrace();

d6b3af6e500f31046b3bceca3bb62aafb8a483fd
Olivier Corby <olivier.corby@inria.fr>
2013-10-16 08:48:45 +0000

    pprinter may be optimized
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1607 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgtool/print/PPrinterTable.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgtool/print/PPrinterTable.java
@@ -2,27 +2,47 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package fr.inria.edelweiss.kgtool.print;
 
 import fr.inria.acacia.corese.triple.parser.NSManager;
-import static fr.inria.edelweiss.kgtool.print.PPrinter.OWL;
-import static fr.inria.edelweiss.kgtool.print.PPrinter.SPIN;
-import static fr.inria.edelweiss.kgtool.print.PPrinter.SQL;
 import java.util.HashMap;
 
 /**
+ * Map pprinter name to pprinter resource path
  *
  * @author Olivier Corby, Wimmics Inria I3S, 2013
  *
  */
 class PPrinterTable extends HashMap<String, String> {
-                                   
-            PPrinterTable(){
-                put(NSManager.SPIN, SPIN);
-                put(NSManager.SQL, SQL);
-                put(NSManager.OWL, OWL);
-            }
-            
-            
+
+    HashMap<String, Boolean> table;
+
+    PPrinterTable() {
+        init();
+    }
+
+    void init() {
+        put(NSManager.SPIN, PPrinter.SPIN);
+        put(NSManager.SQL,  PPrinter.SQL);
+        put(NSManager.OWL,  PPrinter.OWL);
+
+        table = new HashMap<String, Boolean>();
+        table.put(PPrinter.OWL, false);
+        table.put(PPrinter.SPIN, true);
+        table.put(PPrinter.SQL, true);
+
+    }
+    
+    boolean isOptimize(String name){
+        Boolean b = table.get(name);
+        if (b != null){
+            return b;
+        }
+        return false;
+    }
+    
+    void setOptimize(String name, boolean b){
+        table.put(name, b);
+    }
+    
 }

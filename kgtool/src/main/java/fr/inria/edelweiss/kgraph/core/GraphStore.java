bceb0e9a9292df81c49cac5a403aae1a90544ab4
Olivier Corby <olivier.corby@inria.fr>
2013-10-30 08:29:19 +0000

    create(boolean b)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1647 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/core/GraphStore.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/core/GraphStore.java
@@ -1,47 +1,66 @@
-
 package fr.inria.edelweiss.kgraph.core;
 
+import fr.inria.edelweiss.kgram.api.core.ExpType;
 import java.util.Collection;
 import java.util.HashMap;
 
 /**
- *
+ * Draft Graph Store where named graphs are hidden for system use 
+ * 
  * @author Olivier Corby, Wimmics Inria I3S, 2013
  *
  */
 public class GraphStore extends Graph {
-    
-    HashMap <String, Graph> store;
-    
-    GraphStore(){
-        store = new HashMap <String, Graph>();
+
+   
+
+    HashMap<String, Graph> store;
+
+
+    GraphStore() {
+        store = new HashMap<String, Graph>();
     }
-    
-    public static GraphStore create(){
+
+    public static GraphStore create() {
         return new GraphStore();
     }
-    
-    public Graph getNamedGraph(String name){
+
+    public static GraphStore create(boolean b) {
+        GraphStore gs = new GraphStore();
+        if (b) {
+            gs.setEntailment();
+        }
+        return gs;
+    }
+
+
+    public Graph getNamedGraph(String name) {
         return store.get(name);
     }
-    
-    public Graph createNamedGraph(String name){
+
+    public Graph createNamedGraph(String name) {
         Graph g = Graph.create();
         store.put(name, g);
         return g;
     }
-    
-    public void setNamedGraph(String name, Graph g){
+
+    public void setNamedGraph(String name, Graph g) {
         store.put(name, g);
     }
-    
-    public Collection<Graph> getNamedGraphs(){
+
+    public Collection<Graph> getNamedGraphs() {
         return store.values();
     }
-    
-    public Graph getDefaultGraph(){
+
+    public Graph getDefaultGraph() {
         return this;
     }
     
-
+   
+    
+   
+    
+    
+    
+    
 }

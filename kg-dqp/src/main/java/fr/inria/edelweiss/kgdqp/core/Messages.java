ff09c8ff138a3b28ddfd0ae463da7c1b154a2253
Alban Gaignar <alban.gaignard@i3s.unice.fr>
2013-10-31 08:37:40 +0000

    Debug messages
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1661 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kg-dqp/src/main/java/fr/inria/edelweiss/kgdqp/core/Messages.java
+++ kg-dqp/src/main/java/fr/inria/edelweiss/kgdqp/core/Messages.java
@@ -9,7 +9,7 @@ package fr.inria.edelweiss.kgdqp.core;
  * @author Alban Gaignard <alban.gaignard@cnrs.fr>
  */
 public class Messages {
-    
+
     public static String countQueries = "Remote queries";
     public static String countTransferredResults = "Transferred results per query";
     public static String countDS = "Remote queries per data source";

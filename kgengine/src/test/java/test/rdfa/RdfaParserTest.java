466426f5a0c29149709344540143235a02e53ce3
Fuqi Song <Fuqi.Song@inria.fr>
2014-02-28 16:14:34 +0000

    RDFa parser testing is merged to W3C test cases in package kgtool, this test class is deprecated.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1889 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgengine/src/test/java/test/rdfa/RdfaParserTest.java
+++ kgengine/src/test/java/test/rdfa/RdfaParserTest.java
@@ -10,8 +10,13 @@ import org.junit.runner.RunWith;
 /**
  * Test cases for validating RDFa parsing and sparql query against the parsed
  * data
- *
+ * 
+ * @deprecated This test is merged to W3C test cases
+ * please refer to kgtool:test.w3c.rdfa.W3CRDFa10Test.java
+ * 
  * @author Fuqi Song Wimmics inria i3s
+ * @date Jan 2014 new
+ * @date 28 Feb. 2014 deprecated
  */
 @RunWith(DataProviderRunner.class)
 public class RdfaParserTest {
@@ -50,37 +55,37 @@ public class RdfaParserTest {
     }
 
     //-------------run test----------
-    @Test
+    //@Test
     @UseDataProvider("getTestSuiteForHtml4")
     public void runTestsHtml4(String sourceFile, String sparql) throws IOException {
         helper.process(sourceFile, sparql);
     }
 
-    @Test
+    //@Test
     @UseDataProvider("getTestSuiteForHtml5")
     public void runTestsHtml5(String sourceFile, String sparql) throws IOException {
         helper.process(sourceFile, sparql);
     }
 
-    @Test
+    //@Test
     @UseDataProvider("getTestSuiteForXhtml1")
     public void runTestsXhtml1(String sourceFile, String sparql) throws IOException {
         helper.process(sourceFile, sparql);
     }
 
-    @Test
+    //@Test
     @UseDataProvider("getTestSuiteForXhtml5")
     public void runTestsXhtml5(String sourceFile, String sparql) throws IOException {
         helper.process(sourceFile, sparql);
     }
 
-    @Test
+    //@Test
     @UseDataProvider("getTestSuiteForXml")
     public void runTestsXml(String sourceFile, String sparql) throws IOException {
         helper.process(sourceFile, sparql);
     }
 
-    @Test
+    //@Test
     @UseDataProvider("getTestSuiteForSvg")
     public void runTestsSvg(String sourceFile, String sparql) throws IOException {
         helper.process(sourceFile, sparql);

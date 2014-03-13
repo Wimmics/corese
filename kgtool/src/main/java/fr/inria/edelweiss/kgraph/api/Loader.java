6bf77a915b03ef5df7411039a464602049c24030
Olivier Corby <olivier.corby@inria.fr>
2014-03-13 11:11:58 +0000

    Cst for formats
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@1911 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/Loader.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/api/Loader.java
@@ -5,7 +5,15 @@ import fr.inria.edelweiss.kgtool.load.LoadException;
 import java.io.InputStream;
 
 public interface Loader {
-	
+    static final int RDFXML_FORMAT = 0;
+    static final int RDFA_FORMAT   = 1;
+    static final int TURTLE_FORMAT = 2;
+    static final int NT_FORMAT     = 3;
+    static final int JSONLD_FORMAT = 4;
+    static final int RULE_FORMAT   = 5;
+    static final int QUERY_FORMAT  = 6;
+    static final int UNDEF_FORMAT  = 7;
+
 	void init(Object o);
 	
 	boolean isRule(String path);
@@ -14,6 +22,8 @@ public interface Loader {
 	
 	void load(String path, String source);
         
+        void load(String path, String base, String source, int format) throws LoadException;
+
         void load(InputStream stream, String str) throws LoadException;
 	
 	void loadWE(String path) throws LoadException;
@@ -21,6 +31,8 @@ public interface Loader {
 	void loadWE(String path, String source) throws LoadException;
 	
 	RuleEngine getRuleEngine();
+        
+        int getFormat(String path);
 
 
 }

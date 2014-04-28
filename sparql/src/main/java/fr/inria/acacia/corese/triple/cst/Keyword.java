9bd224e3055d4432a0b90631ce43f20f10d2b5a6
Olivier Corby <olivier.corby@inria.fr>
2014-04-28 06:50:42 +0000

    move aggregate to Processor
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2080 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- sparql/src/main/java/fr/inria/acacia/corese/triple/cst/Keyword.java
+++ sparql/src/main/java/fr/inria/acacia/corese/triple/cst/Keyword.java
@@ -32,26 +32,6 @@ public interface Keyword {
 	final static public String MATCH = "match";
 	final static public String STNOT = "not";
 	
-	
-	// WARNING: shared by kgram and corese
-	public final static String GENURI = "genURI";
-	public final static String SCOUNT = "count";
-	public final static String COUNTITEM = "countItem";
-	public final static String AVG = "avg";
-	public final static String SUM = "sum";
-	public final static String TIME = "time";
-	public final static String GTIME = "gtime";
-	public final static String NBRESULTS = "nbResults"; 
-	public final static String ARRAY = "row"; 
-	public final static String MIN 	= "min";
-	public final static String MAX 	= "max";
-	static final String CONCAT = "group_concat";
-	static final String SAMPLE = "sample";
-	static final String AGGAND = "agg_and";
-
-	public static final String[] aggregate = 
-	{GENURI, SCOUNT, AVG, SUM, COUNTITEM, TIME, GTIME, ARRAY, NBRESULTS, MIN, MAX, CONCAT, SAMPLE, AGGAND};
-	
 	final static public String SFAIL = "fail";
 
 

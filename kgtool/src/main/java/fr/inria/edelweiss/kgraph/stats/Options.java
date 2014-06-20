aeee9f3f41bd3137dc1b9adbcac85c9176fff703
Fuqi Song <Fuqi.Song@inria.fr>
2014-06-20 17:05:14 +0000

    added some paramters and comments
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2163 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/stats/Options.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/stats/Options.java
@@ -10,17 +10,11 @@ import fr.inria.edelweiss.kgram.sorter.core.IStatistics;
  */
 public class Options {
 
-    //hash bucket 
+    //*** hash bucket *** 
     private static final int BUCKET_NUMBER_MAX = 10000;//maximum size of bucket
     private static final int BUCKET_SIZE_MIN = 10;//minimum number of objects in one buckets
 
-    //For class Options
-    public final static int HT_HASH = 10;
-    public final static int HT_FULL = 20;
-    public final static int HT_CUTOFF = 30;
-    public final static int HT_AVERAGE = 40;
-
-    //reduced map
+    //*** reduced map ***
     //default limit of resources percentage of all resources
     private static final double RES_LIMIT_TOP = 0.01;
     private static final double RES_LIMIT_BOTTOM = 0.01;
@@ -29,16 +23,34 @@ public class Options {
     private static final double TRI_LIMIT_TOP = 0.50;
     private static final double TRI_LIMIT_BOTTOM = 0.10;
 
-    //default parameters
+    //*** hash triple table ***
+    private static final double HTT_MAX_SIZE = 100;
+    //private static final double HTT_PRE = 10;
+    //private static final double HTT_OBJ = 100;
+
+    //*** default parameters ***
     public final static double[] DEF_PARA_CUTOFF = new double[]{RES_LIMIT_TOP, TRI_LIMIT_TOP, RES_LIMIT_BOTTOM, TRI_LIMIT_BOTTOM};
     public final static double[] DEF_PARA_HASH = new double[]{BUCKET_SIZE_MIN, BUCKET_NUMBER_MAX};
-            
-    //default options
-    public final static Options DEF_OPT_SUB = new Options(HT_CUTOFF, IStatistics.SUBJECT, DEF_PARA_CUTOFF);
-    public final static Options DEF_OPT_PRE = new Options(HT_FULL, IStatistics.PREDICATE, null);
-    public final static Options DEF_OPT_OBJ = new Options(HT_HASH, IStatistics.OBJECT, DEF_PARA_HASH);
+    public final static double[] DEF_PARA_HTT = new double[]{HTT_MAX_SIZE, HTT_MAX_SIZE};
 
-    public final static Options[] DEFAULT_OPTIONS = new Options[]{DEF_OPT_SUB, DEF_OPT_PRE, DEF_OPT_OBJ};
+    //For class Options
+    public final static int HT_HASH = 10;
+    public final static int HT_FULL = 20;
+    public final static int HT_CUTOFF = 30;
+    public final static int HT_AVERAGE = 40;
+    public final static int HT_TRIPLE_HASH = 50;
+
+    //default options
+    //subject
+    public final static Options DEF_OPT_SUB = new Options(HT_CUTOFF, IStatistics.SUBJECT, DEF_PARA_CUTOFF);
+    //predicate
+    public final static Options DEF_OPT_PRE = new Options(HT_FULL, IStatistics.PREDICATE, null);
+    //object
+    public final static Options DEF_OPT_OBJ = new Options(HT_HASH, IStatistics.OBJECT, DEF_PARA_HASH);
+    //other
+    public final static Options DEF_OPT_PAIR = new Options(HT_TRIPLE_HASH, IStatistics.NA, DEF_PARA_HTT);
+
+    public final static Options[] DEFAULT_OPTIONS = new Options[]{DEF_OPT_SUB, DEF_OPT_PRE, DEF_OPT_OBJ, DEF_OPT_PAIR};
 
     private final int heuristic;
     private final int nodeType;

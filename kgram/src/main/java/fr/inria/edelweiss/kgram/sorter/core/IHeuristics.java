3d53b2b86f08e3ab6f0e3b703a852567577b7830
Fuqi Song <Fuqi.Song@inria.fr>
2014-06-24 16:37:42 +0000

    new interface of calling producer for estimating selectivity based on heuristics without stats data
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2168 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/sorter/core/IHeuristics.java
@@ -0,0 +1,13 @@
+package fr.inria.edelweiss.kgram.sorter.core;
+
+import fr.inria.edelweiss.kgram.api.core.Node;
+
+/**
+ *
+ * @author Fuqi Song, WImmics Inria I3S
+ * @date 24 juin 2014
+ */
+public interface IHeuristics {
+
+    boolean isURI(Node n);
+}

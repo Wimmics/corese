f94af58968e4552d597d4a7fa695cac26c5376aa
Fuqi Song <Fuqi.Song@inria.fr>
2014-06-26 09:46:23 +0000

    interface that needs to be implemented by Producer from kgraph
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2173 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgram/src/main/java/fr/inria/edelweiss/kgram/sorter/core/IProducer.java
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/sorter/core/IProducer.java
@@ -1,14 +1,15 @@
 package fr.inria.edelweiss.kgram.sorter.core;
 
 import fr.inria.edelweiss.kgram.api.core.Edge;
+import fr.inria.edelweiss.kgram.api.core.Node;
 
 /**
- * Interface for getting statistics data from a graph
+ * Interface for calling Producer from kgraph, such as get stats data, etc..
  *
  * @author Fuqi Song, WImmics Inria I3S
  * @date 4 juin 2014
  */
-public interface IStatistics {
+public interface IProducer {
 
     public static final int SUBJECT = 1;
     public static final int PREDICATE = 2;
@@ -24,8 +25,10 @@ public interface IStatistics {
     int getObjectNumber();
 
     int getCountByValue(String val, int type);
-    
+
     int getCountByTriple(Edge e);
-    
+
     boolean enabled();
+
+    boolean isURI(Node n);
 }

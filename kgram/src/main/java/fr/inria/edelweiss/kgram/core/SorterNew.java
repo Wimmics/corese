8db74300bb19df663a82bc444d44683c220cb85c
Fuqi Song <Fuqi.Song@inria.fr>
2014-08-14 15:12:07 +0000

    add method to check is an expression can be sorted add case to adapt different query plan mode
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2292 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgram/src/main/java/fr/inria/edelweiss/kgram/core/SorterNew.java
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/core/SorterNew.java
@@ -1,10 +1,11 @@
 package fr.inria.edelweiss.kgram.core;
 
+import static fr.inria.edelweiss.kgram.api.core.ExpType.EDGE;
 import fr.inria.edelweiss.kgram.api.query.Producer;
 import fr.inria.edelweiss.kgram.sorter.core.BPGraph;
 import fr.inria.edelweiss.kgram.sorter.core.IEstimate;
 import fr.inria.edelweiss.kgram.sorter.core.ISort;
-import fr.inria.edelweiss.kgram.sorter.impl.HeuristicsBasedEstimation;
+import fr.inria.edelweiss.kgram.sorter.impl.RuleBasedEstimation;
 import fr.inria.edelweiss.kgram.sorter.impl.SortBySelectivity;
 import fr.inria.edelweiss.kgram.sorter.impl.StatsBasedEstimation;
 import java.util.List;
@@ -17,20 +18,36 @@ import java.util.List;
  */
 public class SorterNew extends Sorter {
 
-    boolean print = false;
+    boolean print = true;
 
-    public void sort(Exp exp, Producer prod, Object utility, boolean stats) {
-        if(exp.size()<2) return;
-        
+    public void sort(Exp exp, List<Exp> bindings, Producer prod, int planType) {
+        //0 sortable?
+        if (!sortable(exp)) {
+            return;
+        }
+
+        if (print) {
+            System.out.println("Before sorting:" + exp);
+        }
         long start = System.currentTimeMillis();
         //1 create graph (list of nodes and their relations)
-        BPGraph bpg = new BPGraph(exp);
+        BPGraph bpg = new BPGraph(exp, bindings);
 
         // 2 assign selectivity
-        IEstimate ies = new HeuristicsBasedEstimation();
-        if(stats) ies = new StatsBasedEstimation();
-        
-        ies.estimate(bpg, prod, utility);
+        IEstimate ies ;
+        switch (planType) {
+            case Query.PLAN_COMBINED:
+            case Query.PLAN_RULE_BASED:
+                ies = new RuleBasedEstimation();
+                break;
+            case Query.PLAN_STATS_BASED:
+                ies = new StatsBasedEstimation();
+                break;
+            default:
+                ies = new RuleBasedEstimation();
+        }
+
+        ies.estimate(bpg, prod, bindings);
 
         //3 find order
         ISort is = new SortBySelectivity();
@@ -38,6 +55,35 @@ public class SorterNew extends Sorter {
 
         //4 rewrite
         is.rewrite(exp, l);
-        if(print) System.out.println("== Query sorting time:" + (System.currentTimeMillis() - start) + "ms ==");
+        if (print) {
+            System.out.println("After sorting:" + exp);
+        }
+        if (print) {
+            System.out.println("== Query sorting time:" + (System.currentTimeMillis() - start) + "ms ==");
+        }
+    }
+
+    /**
+     * Check weather an expression can be sorted, now only support sorting AND
+     * with sub-expression: FILTER, EDGE, VALUES and length >= 2
+     *
+     * @param e expression to be sorted
+     * @return
+     */
+    private boolean sortable(Exp e) {
+        if (e.type() != Exp.AND) {
+            return false;
+        }
+        if (e.size() < 2) {
+            return false;
+        }
+
+        for (Exp ee : e) {
+            if (!(ee.type() == Exp.FILTER || ee.type() == EDGE || ee.type() == Exp.VALUES)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

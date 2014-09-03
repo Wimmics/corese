0f193bc28c0a38e3264022abd327c498aa4b41f0
Olivier Corby <olivier.corby@inria.fr>
2014-09-03 13:11:45 +0000

    Draft pseudo transitivity with two predicates not used
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2314 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/logic/Closure.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/logic/Closure.java
@@ -3,7 +3,9 @@ package fr.inria.edelweiss.kgraph.logic;
 import fr.inria.edelweiss.kgram.api.core.Entity;
 import fr.inria.edelweiss.kgram.api.core.Node;
 import fr.inria.edelweiss.kgram.core.Distinct;
+import fr.inria.edelweiss.kgram.core.Query;
 import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.rule.Rule;
 import java.util.ArrayList;
 
 /**
@@ -17,7 +19,8 @@ static int count = 0;
 
     Graph graph;
     Distinct dist;
-    Node pred, src;
+    Query query;
+    Node pred1, pred2, src;
     boolean[][] connect;
     boolean isDistinct = true;
     private boolean isConnect = false;
@@ -39,26 +42,36 @@ static int count = 0;
      * Transitive edge will not be created in kg:rule 
      * for nodes that are already connected 
      */
-    public void init(Node p) {
+    
+        public void init(Node p) {
+            init(p, p);
+        }
+        
+    public void init(Node p1, Node p2) {
         // named graph where entailments are stored
         src = graph.addGraph(Entailment.RULE);
         // predicate of transitive property
-        pred = graph.getPropertyNode(p);       
+        pred1 = graph.getPropertyNode(p1); 
+        pred2 = graph.getPropertyNode(p2); 
         if (isConnect()) {
             int i = graph.getNodeIndex();            
             try {
                 connect = new boolean[i][];
-                for (Entity ent : graph.getEdges(pred)) {
+                for (Entity ent : graph.getEdges(pred1)) {
                     connect(ent.getNode(0), ent.getNode(1));
                 }
             } catch (OutOfMemoryError E) {
                 setConnect(false);
-                System.out.println("Skip Cache Out Of Memory:  " + pred);
+                System.out.println("Skip Cache Out Of Memory:  " + pred1);
             }
         }
     }
+    
+    public void setQuery(Query q){
+        query = q;
+    }
 
-    public boolean isConnected(Node n1, Node n2) {
+    public boolean isConnected(Node p, Node n1, Node n2) {
         if (isConnect()){
             int i1 = n1.getIndex();       
             if (i1 >= 0 && n2.getIndex() >= 0) {
@@ -68,15 +81,15 @@ static int count = 0;
                     } catch (OutOfMemoryError E) {  
                         if (isMessage){
                             isMessage = false;
-                            System.out.println("Skip Cache Out Of Memory:  "  + pred);
-                            return graph.exist(pred, n1, n2);
+                            System.out.println("Skip Cache Out Of Memory:  "  + p);
+                            return graph.exist(p, n1, n2);
                         }
                     }                   
                 }
                 return connect[i1][n2.getIndex()];
             }
         }
-        return graph.exist(pred, n1, n2);
+        return graph.exist(p, n1, n2);
     }
 
     public void connect(Node n1, Node n2) {
@@ -89,7 +102,7 @@ static int count = 0;
                     } catch (OutOfMemoryError E) {
                         if (isMessage) {
                             isMessage = false;
-                            System.out.println("Skip Cache Out Of Memory:  " + pred);
+                            System.out.println("Skip Cache Out Of Memory:  " + pred1);
                         }        
                         return;
                     }
@@ -109,37 +122,40 @@ static int count = 0;
      * current loop index. TODO: Producer vs Graph
      */
     public void closure(int loop, int loopIndex, int prevIndex) {
-        if (pred == null){
+        if (pred1 == null || pred2 == null){
             return;
         }
+        boolean same = pred1 == pred2;
         boolean go = true, isFirst = loop == 0;
         int n = 0;
 
         ArrayList<Entity> lnew = new ArrayList<Entity>(),
                 ltmp = new ArrayList<Entity>();
         if (isTrace){
-            System.out.println("Cl: 0 "  + graph.size(pred));
+            System.out.println("Cl: 0 "  + graph.size(pred1));
         }
         while (go) {
 
             Iterable<Entity> it1 = lnew;
             if (n == 0) {
-                it1 = graph.getEdges(pred);
+                it1 = graph.getEdges(pred1);
             }
             else if (isTrace){
                 System.out.println("Cl: "  +n + " " + + lnew.size());               
             }
             n++;
+
             for (Entity e1 : it1) {
                 // after step 0, consider only edges created at previous step
                 Node node = e1.getNode(1);
-                Node n1 = e1.getNode(0);
-                if (n1 == node) {
+                Node n1   = e1.getNode(0);
+
+                if (same && n1 == node) {
                     continue;
                 }
                 boolean ok1 = isFirst || e1.getEdge().getIndex() >= prevIndex;
 
-                Iterable<Entity> it2 = graph.getEdges(pred, node, 0);
+               Iterable<Entity> it2 = graph.getEdges(pred2, node, 0);
 
                 if (it2 != null) {
 
@@ -158,8 +174,8 @@ static int count = 0;
                                 continue;
                             }
 
-                            if (!isConnected(n1, n2) && isDistinct(n1, n2)) {
-                                Entity ent = create(n1, n2);
+                            if (!isConnected(pred1, n1, n2) && isDistinct(n1, n2)) {
+                                Entity ent = create(pred1, n1, n2);
                                 ent.getEdge().setIndex(loopIndex);
                                 ltmp.add(ent);
                                 connect(n1, n2);
@@ -168,7 +184,7 @@ static int count = 0;
                     }
                 }
             }
-            Node p = pred;
+            Node p = pred1;
             if (p.getLabel().equals(Graph.TOPREL)){
                 p = null;
             }
@@ -189,8 +205,10 @@ static int count = 0;
         return true;
     }
 
-    Entity create(Node n1, Node n2) {
-        return graph.create(src, n1, pred, n2);
+    Entity create(Node p, Node n1, Node n2) {
+        Entity ent = graph.create(src, n1, p, n2);
+        ent.setProvenance(query);
+        return ent;
     }
 
     /**

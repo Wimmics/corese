9b5c7e49cfc93df3ab7fc2ec347dd6786fce52c7
Olivier Corby <olivier.corby@inria.fr>
2014-09-11 14:51:21 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2330 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/main/java/fr/inria/edelweiss/kgtool/util/GraphUtil.java
@@ -0,0 +1,40 @@
+package fr.inria.edelweiss.kgtool.util;
+
+import fr.inria.edelweiss.kgram.api.core.ExpType;
+import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgtool.load.Load;
+import fr.inria.edelweiss.kgtool.load.LoadException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Olivier Corby, Wimmics INRIA I3S, 2014
+ *
+ */
+public class GraphUtil {
+    
+    Graph gs;
+    
+    public GraphUtil(Graph g){
+        gs = g;
+    }
+    
+    public Graph shoot(){
+        Graph g = Graph.create();
+        try {
+            Load ld = Load.create(g);
+            ld.loadString(gs.toString(), Load.TURTLE_FORMAT);
+        } catch (LoadException ex) {
+            Logger.getLogger(GraphUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return g;
+    }   
+    
+    public Graph shoot(String name){
+        Graph g = shoot();
+        gs.setNamedGraph(name, g);
+        return g;
+    }
+
+}

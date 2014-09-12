447432933243789c9a437bad7c1db770f61db4e6
Olivier Corby <olivier.corby@inria.fr>
2014-09-12 06:47:36 +0000

    implements Graphable for named graph pattern
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2350 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/RDFizer.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.inria.edelweiss.kgraph.query;
+
+import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.acacia.corese.triple.parser.ASTQuery;
+import fr.inria.edelweiss.kgram.api.query.Graphable;
+import fr.inria.edelweiss.kgram.core.Query;
+import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgtool.load.Load;
+import fr.inria.edelweiss.kgtool.load.LoadException;
+import fr.inria.edelweiss.kgtool.util.SPINProcess;
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Olivier Corby, Wimmics INRIA I3S, 2014
+ *
+ */
+public class RDFizer {
+    
+    
+     public boolean isGraphAble(Object obj) {
+        if (obj instanceof Graphable) {
+                return true;
+        }  
+        return false;
+    }
+
+    public Graph getGraph(Object obj) {
+        if (obj instanceof Graphable) {
+            return getGraph((Graphable) obj);
+        }
+        return null;
+    }
+    
+    Graph getGraph(Graphable gg) {
+        Graph g = (Graph) gg.getGraph();
+        if (g != null){
+            return g;
+        }
+        String rdf = gg.toGraph();
+        try {
+            g = getGraph(rdf);
+            gg.setGraph(g);
+        } catch (EngineException ex) {
+           
+        }
+        return g;
+    }
+    
+ 
+    
+    
+      Graph getGraph(String rdf) throws EngineException {
+        Graph g = Graph.create();
+        Load ld = Load.create(g);
+        try {
+            ld.load(new ByteArrayInputStream(rdf.getBytes("UTF-8")), Load.TURTLE_FORMAT);
+        } catch (LoadException ex) {
+            Logger.getLogger(SPINProcess.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(SPINProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return g;
+     }
+     
+     
+     
+       Graph getGraph(Query q) {
+        try {
+            Graph g = (Graph) q.getGraph();
+            if (g == null){
+                ASTQuery ast = (ASTQuery) q.getAST();
+                SPINProcess sp = SPINProcess.create();
+                g = sp.toSpinGraph(ast);
+                q.setGraph(g);
+            }           
+            return g;
+        } catch (EngineException ex) {
+        }
+        return null;
+    }
+
+}

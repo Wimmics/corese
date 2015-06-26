69eb9ce975b7d623803330b8bf964c8ba1d1e876
Fuqi Song <Fuqi.Song@inria.fr>
2015-06-26 14:02:22 +0000

    Add method supporting @POST @Consumes("multipart/form-data")
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2851 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgserver/src/main/java/fr/inria/edelweiss/kgramserver/webservice/SPIN.java
+++ kgserver/src/main/java/fr/inria/edelweiss/kgramserver/webservice/SPIN.java
@@ -1,5 +1,6 @@
 package fr.inria.edelweiss.kgramserver.webservice;
 
+import com.sun.jersey.multipart.FormDataParam;
 import fr.inria.acacia.corese.exceptions.EngineException;
 import fr.inria.acacia.corese.triple.parser.Context;
 import fr.inria.edelweiss.kgraph.core.Graph;
@@ -9,12 +10,14 @@ import fr.inria.edelweiss.kgtool.print.HTMLFormat;
 import fr.inria.edelweiss.kgtool.transform.Transformer;
 import fr.inria.edelweiss.kgtool.util.SPINProcess;
 import java.util.logging.Level;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -26,13 +29,22 @@ import javax.ws.rs.core.Response;
 public class SPIN {
 
     private static final String headerAccept = "Access-Control-Allow-Origin";
-    public static final String TOSPIN_SERVICE    = "/spin/tospin";
-    public static final String TOSPARQL_SERVICE  = "/spin/tosparql";
-        
+    public static final String TOSPIN_SERVICE = "/spin/tospin";
+    public static final String TOSPARQL_SERVICE = "/spin/tosparql";
+
     @POST
     @Produces("text/html")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Path("tospin")
     public Response toSPINPOST(@FormParam("query") String query) {
+        return toSPIN(query);
+    }
+
+    @POST
+    @Produces("text/html")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Path("tospin")
+    public Response toSPINPOST_MD(@FormDataParam("query") String query) {
         return toSPIN(query);
     }
 
@@ -61,9 +73,9 @@ public class SPIN {
             return Response.status(500).header(headerAccept, "*").entity("Error while querying the remote KGRAM engine").build();
         }
     }
-    
-     Context complete(Context c){
-        if (SPARQLRestAPI.isAjax){
+
+    Context complete(Context c) {
+        if (SPARQLRestAPI.isAjax) {
             c.setProtocol(Context.STL_AJAX);
         }
         return c;
@@ -71,8 +83,17 @@ public class SPIN {
 
     @POST
     @Produces("text/html")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Path("tosparql")
     public Response toSPARQLPOST(@FormParam("query") String query) {
+        return toSPARQL(query);
+    }
+
+    @POST
+    @Produces("text/html")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Path("tosparql")
+    public Response toSPARQLPOST_MD(@FormDataParam("query") String query) {
         return toSPARQL(query);
     }
 
@@ -83,11 +104,19 @@ public class SPIN {
         Graph g = Graph.create();
         try {
             Load ld = Load.create(g);
-            if (query != null) {
-                ld.loadString(query, Load.TURTLE_FORMAT);
+            if (query == null) {
+                query = "@prefix sp: <http://spinrdf.org/sp#> .\n"
+                        + "\n"
+                        + "[a sp:Select ;\n"
+                        + "  sp:star true ;\n"
+                        + "  sp:where ([sp:object [sp:varName \"y\"] ;\n"
+                        + "    sp:predicate [sp:varName \"p\"] ;\n"
+                        + "    sp:subject [sp:varName \"x\"]])] .";
             }
+            ld.loadString(query, Load.TURTLE_FORMAT);
 
             Context c = new Context().setTransform(Transformer.TOSPIN).setQuery(query).setService(TOSPARQL_SERVICE);
+            complete(c);
             HTMLFormat ft = HTMLFormat.create(g, c);
 
             return Response.status(200).header(headerAccept, "*").entity(ft.toString()).build();

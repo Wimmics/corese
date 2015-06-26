002dfd9d7cb3da75a82dd3b26d99b582cad185ca
Fuqi Song <Fuqi.Song@inria.fr>
2015-06-26 13:54:42 +0000

    web resources for new server look
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2849 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgserver/src/main/resources/webapp/js/server.js
@@ -0,0 +1,116 @@
+var content = "#contentOfSite";
+var changeURL = true;
+var srv = "/srv";
+
+//Get template by sending ajax request 'GET or 'POST' according to parameter type
+function trans(obj) {
+    if (typeof obj === 'string') {
+        transGET(obj);
+    } else if (typeof obj === 'object') {
+        transPOST(obj);
+    } else {
+        return false;
+    }
+}
+
+//get generated html from server by sending ajax 'GET' request
+function transGET(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        crossDomain: true,
+        success: function (response) {
+            success(response, url);
+        },
+        error: function (response) {
+            $(content).html(response);
+        }
+    });
+}
+
+//get generated html from server by sending ajax 'POST' request
+function transPOST(form) {
+    var fd = new FormData(form);
+    if (form.id === '') {
+        alert('Please give a unique name to the form.');
+        return false;
+    }
+    var url = $('#' + form.id).attr('action');//get relative path
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: fd,
+        enctype: "multipart/form-data",
+        processData: false,
+        contentType: false,
+        crossDomain: true,
+        success: function (response) {
+            success(response, url);
+        },
+        error: function (response) {
+            $(content).html(response);
+        }
+    });
+}
+
+//when ajax returns '200 ok', display the response text on the page
+function success(response, url) {
+    //1 load the content 
+    $(content).html('<div class="container">' + response + '</div>');
+
+    //2 change the url displayed in broswer url bar
+    if (changeURL && url.trim() !== '') {
+        url = location.protocol + "//" + location.host + "/srv" + url;
+        window.history.pushState('', '', url);
+    }
+    changeURL = true;
+}
+
+//initialize the page content
+window.onload = function () {
+    loadContent();
+};
+
+//go back/forward, reload page
+window.onpopstate = function (event) {
+    changeURL = false;
+    loadContent();
+};
+
+//loading content of page whe refreshing or reloading page, load all contents
+function loadContent() {
+    //0. load header and footer of page
+    $('#navgator').load("/html/navigator.html");
+    $('#footerOfSite').load("/html/footer.html");
+    
+    //1 home page
+    if (location.pathname === '/demo_new.html' || location.pathname.trim() === '/') {
+        $(content).load("/html/content.html");
+        return;
+    }
+
+    //2 load template from server
+    var url = location.pathname + location.search;
+    var index = url.indexOf(srv);
+    if (index === -1) {
+        alert("The url does not contain '/srv' service.");
+        return;
+    }
+    url = url.substr(index + srv.length);
+    trans(url);
+}
+
+//call function 'callback' when ajax request is completed
+//function 'callback' is a javascript function that is defined in template,
+//which need to be executed after ajax request completes
+$(document).ajaxComplete(function (event, request, settings) {
+    //if the funciton 'callback' is defined, otherwise do nothing
+    if (typeof callback === 'function') {
+        callback();
+    }
+});
+
+
+function showMap(){
+    
+}

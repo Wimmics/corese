fc3d4ec2a5326b2e033497ea7b55e59fa901e79e
Olivier Corby <olivier.corby@inria.fr>
2015-08-18 07:40:32 +0000

    trace getMappings
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2944 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/ProducerImpl.java
+++ kgtool/src/main/java/fr/inria/edelweiss/kgraph/query/ProducerImpl.java
@@ -316,11 +316,15 @@ public class ProducerImpl implements Producer, IProducerQP {
     public Mappings getMappings(Node gNode, List<Node> from, Exp exp, Environment env) {
         if (env instanceof Memory){
             Memory mem = (Memory) env;
-            System.out.println("P:");
-            System.out.println(env.getQuery().getAST());
+            if (env.getQuery().isDebug()){
+                System.out.println("BGP:\n" + env.getQuery().getAST());
+            }
             Eval eval = mem.getEval();          
             exp.setType(Exp.AND);
             Mappings map = eval.subEval(this, gNode, gNode, exp, null);
+            if (env.getQuery().isDebug()){
+                System.out.println("BGP:\n" + map);
+            }
             exp.setType(Exp.BGP);
             return map;
         }

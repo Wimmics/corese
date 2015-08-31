adf88071d08366123c493336fe8b5319330d4998
Olivier Corby <olivier.corby@inria.fr>
2015-08-31 08:46:10 +0000

    Exp
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2963 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- sparql/src/main/java/fr/inria/acacia/corese/triple/api/ExpressionVisitor.java
+++ sparql/src/main/java/fr/inria/acacia/corese/triple/api/ExpressionVisitor.java
@@ -1,10 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.inria.acacia.corese.triple.api;
 
 import fr.inria.acacia.corese.triple.parser.Constant;
+import fr.inria.acacia.corese.triple.parser.Exp;
 import fr.inria.acacia.corese.triple.parser.Term;
 import fr.inria.acacia.corese.triple.parser.Variable;
 
@@ -14,10 +11,12 @@ import fr.inria.acacia.corese.triple.parser.Variable;
  */
 public interface ExpressionVisitor {
     
+    void visit(Exp exp);
+    
     void visit(Term t);
     
     void visit(Variable v);
     
     void visit(Constant c);
-    
+        
 }

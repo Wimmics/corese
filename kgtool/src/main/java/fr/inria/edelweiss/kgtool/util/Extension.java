89d27596efe874b6d985bf7b732326cd7c4fbc11
Olivier Corby <olivier.corby@inria.fr>
2015-08-31 08:59:30 +0000

    Init with extension function queries (calendar)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@2982 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/main/java/fr/inria/edelweiss/kgtool/util/Extension.java
@@ -0,0 +1,47 @@
+package fr.inria.edelweiss.kgtool.util;
+
+import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.query.QueryProcess;
+import fr.inria.edelweiss.kgtool.load.QueryLoad;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Olivier Corby, Wimmics INRIA I3S, 2015
+ *
+ */
+public class Extension {
+    
+    private static final String[] NAMES = { "extension.rq", "calendar.rq", "spqr.rq" };
+    
+    public void process(){
+        for (String name : NAMES){
+            process("/query/" + name);
+        }
+    }
+    
+     void process(String name) {
+        InputStream in = Extension.class.getResourceAsStream(name);
+        if (in == null){
+            return;
+        }
+        try {
+            QueryLoad ql = QueryLoad.create();
+            String str = ql.read(in);
+            QueryProcess exec = QueryProcess.create(Graph.create());
+            try {
+                exec.compile(str);
+            } catch (EngineException ex) {
+                Logger.getLogger(QueryProcess.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(QueryProcess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}

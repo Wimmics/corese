dbfa8be8bdf74ba964ac3683160be3325c5cd1f3
Abdoul Macina <macina@i3s.unice.fr>
2015-11-09 12:27:48 +0000

    BGP strategy + GetMappings in DQP
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@3187 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kg-dqp/src/main/java/fr/inria/edelweiss/kgdqp/core/ParallelMetaProducerLessBlocking.java
+++ kg-dqp/src/main/java/fr/inria/edelweiss/kgdqp/core/ParallelMetaProducerLessBlocking.java
@@ -1,5 +1,6 @@
 package fr.inria.edelweiss.kgdqp.core;
 
+import fr.inria.edelweiss.kgenv.parser.EdgeImpl;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +9,11 @@ import fr.inria.edelweiss.kgram.api.core.Entity;
 import fr.inria.edelweiss.kgram.api.core.Node;
 import fr.inria.edelweiss.kgram.api.query.Environment;
 import fr.inria.edelweiss.kgram.api.query.Producer;
+import fr.inria.edelweiss.kgram.core.Exp;
+import fr.inria.edelweiss.kgram.core.Mappings;
 import fr.inria.edelweiss.kgram.tool.MetaIterator;
 import fr.inria.edelweiss.kgram.tool.MetaProducer;
+import java.util.Iterator;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
@@ -20,11 +24,12 @@ import org.apache.commons.lang.time.StopWatch;
 import org.apache.log4j.Logger;
 
 /**
- * Meta Producer that handles concurrent accesses to several Producers.
- * Uses a generic MetaIterator that iterates over Producer iterators
- * 
- * This version uses a CompletionService that allows to exploit the first available results.
- * 
+ * Meta Producer that handles concurrent accesses to several Producers. Uses a
+ * generic MetaIterator that iterates over Producer iterators
+ *
+ * This version uses a CompletionService that allows to exploit the first
+ * available results.
+ *
  * @author Alban Gaignard, alban.gaignard@i3s.unice.fr
  *
  */
@@ -44,17 +49,20 @@ public class ParallelMetaProducerLessBlocking extends MetaProducer {
     public Iterable<Entity> getEdges(Node gNode, List<Node> from, Edge edge, Environment env) {
         ExecutorService executorS = Executors.newCachedThreadPool();
         CompletionService<Iterable<Entity>> completionS = new ExecutorCompletionService<Iterable<Entity>>(executorS);
-        
+
         MetaIterator<Entity> meta = null;
         List<Future<Iterable<Entity>>> futures = new ArrayList<Future<Iterable<Entity>>>();
 
-//        logger.info("Searching for edge : " + edge.toString());
+        logger.info("Searching for edge : " + edge.toString());
         // iteration over 
         for (Producer p : this.getProducers()) {
-            //TODO Check index
-            CallableGetEdges getEdges = new CallableGetEdges(p, gNode, from, edge, env);
-//            results.add(executorS.submit(getEdges));
-            futures.add(completionS.submit(getEdges));
+            if (p instanceof RemoteProducerWSImpl) {
+                RemoteProducerWSImpl rp = (RemoteProducerWSImpl) p;
+                if (rp.checkEdge(edge)) {
+                    CallableGetEdges getEdges = new CallableGetEdges(p, gNode, from, edge, env);
+                    futures.add(completionS.submit(getEdges));
+                }
+            }
         }
 
         StopWatch sw = new StopWatch();
@@ -63,7 +71,32 @@ public class ParallelMetaProducerLessBlocking extends MetaProducer {
         //retrieving results
         for (Future<Iterable<Entity>> f : futures) {
             try {
-                meta = add(meta, completionS.take().get());
+                Iterable<Entity> resultFromProducer = completionS.take().get();
+                //detele duplicates
+                Iterator<Entity> it = resultFromProducer.iterator();
+                boolean doubles = false;
+                while (it.hasNext()) {
+                    Entity ent = it.next();
+                    if (ent != null) {
+                        Edge ed = ent.getEdge();
+                        if (meta != null) {
+                            Iterator<Entity> itt = meta.iterator();
+                            while (itt.hasNext()) {
+                                Entity entt = itt.next();
+                                Edge edd = entt.getEdge();
+                                if (ed.getNode().equals(edd.getNode())) {
+//                                    logger.info(" DUPLICATES " + ed.getNode().toString());
+                                    doubles = true;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                if (!doubles) {
+                    meta = add(meta, resultFromProducer);
+                }
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             } catch (ExecutionException ex) {
@@ -72,7 +105,7 @@ public class ParallelMetaProducerLessBlocking extends MetaProducer {
         }
         executorS.shutdown();
         sw.stop();
-//        logger.info("Global results retrieved in " + sw.getTime() + "ms.");
+        logger.info("Global results retrieved in getEDGES " + sw.getTime() + "ms.");
 
         return meta;
     }
@@ -85,5 +118,50 @@ public class ParallelMetaProducerLessBlocking extends MetaProducer {
             meta.next(m);
         }
         return meta;
+    }
+
+    @Override
+    public Mappings getMappings(Node gNode, List<Node> from, Exp bgp, Environment env) {
+        ExecutorService executors = Executors.newCachedThreadPool();
+        CompletionService<Mappings> completions = new ExecutorCompletionService<Mappings>(executors);
+        List<Future<Mappings>> futures = new ArrayList<Future<Mappings>>();
+
+        Mappings results = new Mappings();
+
+        logger.info("Searching for BGP : " + bgp.toString());
+        // sending BGP to relevant producers 
+        for (Producer p : this.getProducers()) {
+            //Checking indexes
+            if (p instanceof RemoteProducerWSImpl) {
+                RemoteProducerWSImpl rp = (RemoteProducerWSImpl) p;
+                if (rp.checkBGP(bgp)) {
+                    CallableGetBasicGraphPattern getBGP = new CallableGetBasicGraphPattern(p, gNode, from, bgp, env);
+                    futures.add(completions.submit(getBGP));
+                }
+            }
+        }
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+
+        //retrieving results
+        for (Future<Mappings> f : futures) {
+            try {
+                //delete duplicates
+                Mappings resFromProd = completions.take().get();
+                resFromProd = resFromProd.minus(results);
+                results.add(resFromProd);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            } catch (ExecutionException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        executors.shutdown();
+        sw.stop();
+        logger.info("Global results retrieved in getMappings " + sw.getTime() + "ms.");
+
+        return results;
     }
 }

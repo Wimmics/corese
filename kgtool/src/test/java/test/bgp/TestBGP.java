d4456da61f9395a14720963159f8b1f380fdfd43
Abdoul Macina <macina@i3s.unice.fr>
2015-11-09 12:56:56 +0000

    BGP strategy + GetMappings in DQP
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@3208 da0ba97e-2115-4e2a-9ddf-745f233b2552

---
+++ kgtool/src/test/java/test/bgp/TestBGP.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.bgp;
+
+
+import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.edelweiss.kgram.core.Mappings;
+import fr.inria.edelweiss.kgram.core.Query;
+import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.query.QueryProcess;
+import fr.inria.edelweiss.kgtool.load.Load;
+import fr.inria.edelweiss.kgtool.print.XMLFormat;
+/**
+ *
+ * @author macina
+ */
+public class TestBGP {
+     private static final String query = ""+
+             "PREFIX idemo:<http://rdf.insee.fr/def/demo#> \n" +
+             "PREFIX igeo:<http://rdf.insee.fr/def/geo#> \n" +
+             "SELECT * WHERE { \n" +
+             "   { ?region igeo:codeRegion \"24\" .\n" +
+             "    ?region igeo:subdivisionDirecte ?departement .}\n" +
+             "   { ?departement igeo:nom ?nom .\n"  +
+             "    ?departement idemo:population ?popLeg .\n" +
+             "    ?popLeg idemo:populationTotale ?popTotale .\n }" +
+             "}"; // ORDER BY ?popTotale";
+    
+
+     
+    public static void main(String[]  args) throws EngineException {
+        //Test default KGRAM
+        Graph g = Graph.create();
+        Load ld = Load.create(g);
+        ld.load("/home/macina/CodeKGRAM/kgram/Dev/trunk/kgtool/src/main/resources/demographie/cog-2012.ttl");
+        ld.load("/home/macina/CodeKGRAM/kgram/Dev/trunk/kgtool/src/main/resources/demographie/popleg-2010.ttl");
+        QueryProcess exec = QueryProcess.create(g);
+        long start = System.currentTimeMillis();
+        Mappings m = exec.query(query);
+        System.out.println("resutls: "+m.size());
+//        XMLFormat xmlF = XMLFormat.create(m);
+//        System.out.println(xmlF);
+        System.out.println("== Querying time:" + (System.currentTimeMillis() - start) + "ms ==\n\n");
+    }
+    
+}

d3c57e28f688b484790c1ad093bc9e27e713ec41
Olivier Corby <olivier.corby@inria.fr>
2015-11-09 08:00:51 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@3183 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kggui/src/main/java/fr/inria/acacia/corese/gui/core/MainFrame.java
+++ kggui/src/main/java/fr/inria/acacia/corese/gui/core/MainFrame.java
@@ -72,7 +72,7 @@ public class MainFrame extends JFrame implements ActionListener {
      */
     private static final long serialVersionUID = 1L;
     private static final int LOAD = 1;
-    private static final String TITLE = "Corese/KGRAM 3.1 - Wimmics INRIA I3S - 2015-10-16";
+    private static final String TITLE = "Corese/KGRAM 3.1 - Wimmics INRIA I3S - 2015-11-11";
     // On déclare notre conteneur d'onglets
     protected static JTabbedPane conteneurOnglets;
     // Compteur pour le nombre d'onglets query créés 

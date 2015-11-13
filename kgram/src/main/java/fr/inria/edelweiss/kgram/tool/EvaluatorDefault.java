65a9b6f8c9f81969b8d989ba2b517aecddc730eb
Olivier Corby <olivier.corby@inria.fr>
2015-11-13 15:07:01 +0000

    getDefine(name)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/kgram/Dev/trunk@3260 da0ba97e-2115-4e2a-9ddf-745f233b2552

--- kgram/src/main/java/fr/inria/edelweiss/kgram/tool/EvaluatorDefault.java
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/tool/EvaluatorDefault.java
@@ -125,4 +125,9 @@ public class EvaluatorDefault implements Evaluator {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public Expr getDefine(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }

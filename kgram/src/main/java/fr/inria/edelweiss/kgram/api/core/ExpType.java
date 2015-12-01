180438b96ee3b903db6f66503ff48e6a963bfcb4
corby <corby@courmayeur.unice.fr>
2015-12-01 11:07:02 +0100

    SPARQL namespace for std SPARQL functions

--- kgram/src/main/java/fr/inria/edelweiss/kgram/api/core/ExpType.java
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/api/core/ExpType.java
@@ -11,6 +11,7 @@ public interface ExpType {
 	public static final String KGRAM  = "http://ns.inria.fr/edelweiss/2010/kgram/";
         public static final String SKOLEM_MARKER = "/.well-known/genid/";
         public static final String SKOLEM = "http://ns.inria.fr" + SKOLEM_MARKER ;
+	public static final String SPARQL = "http://ns.inria.fr/sparql-function/";
 	public static final String STL    = "http://ns.inria.fr/sparql-template/";
 	public static final String EXT    = "http://ns.inria.fr/sparql-extension/";
 	public static final String BNODE  = EXT + "bnode";

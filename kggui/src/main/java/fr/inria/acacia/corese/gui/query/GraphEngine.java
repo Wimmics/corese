1c57399bbe2ef609db29e2a2aa3e8a6628cd9c08
corby <corby@courmayeur.unice.fr>
2016-01-04 14:57:22 +0100

    loadWE

--- kggui/src/main/java/fr/inria/acacia/corese/gui/query/GraphEngine.java
+++ kggui/src/main/java/fr/inria/acacia/corese/gui/query/GraphEngine.java
@@ -110,14 +110,14 @@ public class GraphEngine  {
 		build = b;
 	}
 	
-	public void load(String path) throws EngineException {
+	public void load(String path) throws EngineException, LoadException {
 		if (path.endsWith(BRUL)){
 			bengine.load(path);
 		}
 		else 
 		{
                     Load ld = loader();
-                    ld.load(path);
+                    ld.loadWE(path);
                     // in case of load rule
                     if (ld.getRuleEngine() != null){
                         rengine = ld.getRuleEngine();
@@ -126,7 +126,7 @@ public class GraphEngine  {
                 
 	}
 	
-	public void loadDir(String path) throws EngineException {
+	public void loadDir(String path) throws EngineException, LoadException {
 		load(path);
 	}
 	

64e3ab78a7082f47e2141088a83959a69394c2b9
Erwan Demairy <erwan.demairy@inria.fr>
2016-03-09 14:34:50 +0100

    Addition of some documentation.

--- kgram/src/main/java/fr/inria/edelweiss/kgram/api/query/Provider.java
+++ kgram/src/main/java/fr/inria/edelweiss/kgram/api/query/Provider.java
@@ -12,8 +12,20 @@ import fr.inria.edelweiss.kgram.core.Mappings;
  */
 public interface Provider {
 	
+	/** Execute the request in exp on the serv node, with the env environment,
+	 *  and returns the mapping obtained.
+	 *  @param serv Server on which to execute the query.
+	 *  @param exp  SPARQL request.
+	 *  @param env  Environment to use when applying the request.
+	 *  @return The mapping of the variables to their values.
+	 */
 	Mappings service(Node serv, Exp exp, Environment env);
 	
+	/**
+	 * @param map Pre-existing mappings.
+	 * @return 
+	 * @see #service(fr.inria.edelweiss.kgram.api.core.Node, fr.inria.edelweiss.kgram.core.Exp, fr.inria.edelweiss.kgram.api.query.Environment) 
+	 */
 	Mappings service(Node serv, Exp exp, Mappings map, Environment env);
 
 	void set(String uri, double version);

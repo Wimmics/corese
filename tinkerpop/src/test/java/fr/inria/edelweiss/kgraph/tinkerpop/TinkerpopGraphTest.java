d0761c54fee91b00acc6992da6569d89de5fd720
Erwan Demairy <erwan.demairy@inria.fr>
2016-04-27 14:02:44 +0200

    Accept the new mapping described in https://partage.inria.fr/share/page/site/skgram/blog-postview?postId=post-1461685785953&listViewLinkBack=true

--- tinkerpop/src/test/java/fr/inria/edelweiss/kgraph/tinkerpop/TinkerpopGraphTest.java
+++ tinkerpop/src/test/java/fr/inria/edelweiss/kgraph/tinkerpop/TinkerpopGraphTest.java
@@ -8,6 +8,7 @@ package fr.inria.edelweiss.kgraph.tinkerpop;
 import fr.inria.edelweiss.kgram.api.core.Entity;
 import fr.inria.edelweiss.kgraph.tinkerpop.TinkerpopGraph;
 import java.io.File;
+import org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils;
 
 /**
  *
@@ -16,9 +17,15 @@ import java.io.File;
 public class TinkerpopGraphTest {
 
 	public static void main(String[] args) throws Throwable {
-		TinkerpopGraph graph = TinkerpopGraph.create("src/test/resources/tinkerpop/neo4j_233/btc_1000.graphdb");
+		TinkerpopGraph graph = TinkerpopGraph.create("/Users/edemairy/Developpement/Neo4jTinkerpop/CreateRepoSail/src/test/resources/testConvertResult.neo4jdb");
+		int cpt = 0;
+		System.out.println(IteratorUtils.count(graph.getEdges()));
 		for (Entity e : graph.getEdges()) {
 			System.out.println(e);
+			cpt++;
+			if (cpt > 10000) { 
+				break;
+			}
 		}
 		graph.finalize();
 	}

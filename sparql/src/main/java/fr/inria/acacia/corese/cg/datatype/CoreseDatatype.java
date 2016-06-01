fca80bcd7e5e746a69f93a19200aaba6d71404e0
corby <corby@courmayeur.unice.fr>
2016-06-01 12:06:20 +0200

    leverage toString and quote

--- sparql/src/main/java/fr/inria/acacia/corese/cg/datatype/CoreseDatatype.java
+++ sparql/src/main/java/fr/inria/acacia/corese/cg/datatype/CoreseDatatype.java
@@ -116,10 +116,7 @@ public class CoreseDatatype
 	
 	
 	String protect(String label){
-		String str = Constant.addEscapes(label);
-//		if (str.contains("\"") || str.contains("'")){
-//			return "\"\"\"" + str + "\"\"\"";
-//		}
+		String str = Constant.addEscapes(label, false);
 		return "\"" + str + "\"";
 	}
 	

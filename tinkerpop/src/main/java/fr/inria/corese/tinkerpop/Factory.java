30925088c09319d9796fb1490f663153c0a03bea
Erwan Demairy <erwan.demairy@inria.fr>
2016-10-11 10:36:21 +0200

    Addition of the management of the named graph.

--- tinkerpop/src/main/java/fr/inria/corese/tinkerpop/Factory.java
+++ tinkerpop/src/main/java/fr/inria/corese/tinkerpop/Factory.java
@@ -5,11 +5,8 @@
  */
 package fr.inria.corese.tinkerpop;
 
-import com.thinkaurelius.titan.core.TitanGraph;
 import fr.inria.edelweiss.kgraph.core.Graph;
-import fr.inria.edelweiss.kgraph.query.ProducerImpl;
 import java.util.Optional;
-import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
 //import org.apache.tinkerpop.gremlin.orientdb.OrientGraph;
 
 /**
@@ -18,22 +15,17 @@ import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
  */
 public class Factory {
 
+	public static final String DB_PATH_PROPERTY = "fr.inria.corese.tinkerpop.dbinput";
 	public static String DRIVER = "com.thinkaurelius.titan.core.TitanFactory";
 	public static String DB_PATH;
 	public static String[] CONFIG;
-//	public static String DRIVER = "org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph";
-//	public static String DB_PATH = "/Users/edemairy/btc_neo4j_10m";
-//		public static String DRIVER = "org.apache.tinkerpop.gremlin.orientdb.OrientGraph";
-//		public static String DB_PATH = "plocal:/Users/edemairy/btc_orientdb_1m";
-//		public static String[] CONFIG3 = {OrientGraph.CONFIG_URL, DB_PATH};
-
 	private static Optional<TinkerpopGraph> graph = null;
 
 	public static Object create(Graph g) {
-		DB_PATH = System.getProperty("fr.inria.corese.tinkerpop.dbinput");
+		DB_PATH = System.getProperty(DB_PATH_PROPERTY);
 		TinkerpopProducer p = new TinkerpopProducer(g);
 		CONFIG = new String[1];
-		CONFIG[0] = DB_PATH+"/conf.properties";
+		CONFIG[0] = DB_PATH + "/conf.properties";
 		graph = TinkerpopGraph.create(DRIVER, CONFIG);
 		p.setTinkerpopGraph(graph.get());
 		return p;

c2a546ca0878aeec9b343bb2216db4242ee82d08
corby <corby@courmayeur.unice.fr>
2017-05-29 11:42:02 +0200

    match

--- kgenv/src/main/java/fr/inria/edelweiss/kgenv/parser/NodeImpl.java
+++ kgenv/src/main/java/fr/inria/edelweiss/kgenv/parser/NodeImpl.java
@@ -9,156 +9,170 @@ import fr.inria.edelweiss.kgram.api.core.Node;
 import fr.inria.edelweiss.kgram.api.core.TripleStore;
 
 public class NodeImpl implements Node {
-	
-	Atom atom;
-	int index = -1;
-	
-	public NodeImpl(Atom at){
-		atom = at;
-	}
-	
-	public static NodeImpl createNode(Atom at){
-		return new NodeImpl(at);
-	}
-	
-	public static NodeImpl createVariable(String name){
-		return new NodeImpl(Variable.create(name));
-	}
-	
-	public static NodeImpl createResource(String name){
-		return new NodeImpl(Constant.create(name));
-	}
-	
-	public static NodeImpl createConstant(String name){
-		return new NodeImpl(Constant.create(name, RDFS.xsdstring));
-	}
-	
-	public static NodeImpl createConstant(String name, String datatype){
-		return new NodeImpl(Constant.create(name, datatype));
-	}
-	
-	public static NodeImpl createConstant(String name, String datatype, String lang){
-		return new NodeImpl(Constant.create(name, null, lang));
-	}
-	
-	public Atom getAtom(){
-		return atom;
-	}
-	
-        @Override
-	public IDatatype getValue(){
-		return atom.getDatatypeValue();               
-	}
-        
-        @Override
-        public IDatatype getDatatypeValue(){
-		return atom.getDatatypeValue();               
-	}
-	
-	public String toString(){
-		return atom.toSparql();
-	}
 
-	@Override
-	public int compare(Node node) {
-		if (node.getValue() instanceof IDatatype){
-			IDatatype dt1 = getValue();
-			IDatatype dt2 = (IDatatype) node.getValue();
-			return dt1.compareTo(dt2);
-		}
-		return getLabel().compareTo(node.getLabel());
-	}
+    Atom atom;
+    int index = -1;
 
-	@Override
-	public int getIndex() {
-		// TODO Auto-generated method stub
-		return index;
-	}
+    public NodeImpl(Atom at) {
+        atom = at;
+    }
 
-	@Override
-	public String getLabel() {
-		// TODO Auto-generated method stub
-		if (atom.isResource())
-			return atom.getLongName();
-		return atom.getName();
-	}
+    public static NodeImpl createNode(Atom at) {
+        return new NodeImpl(at);
+    }
 
-	@Override
-	public boolean isConstant() {
-		// TODO Auto-generated method stub
-		return atom.isConstant();
-	}
+    public static NodeImpl createVariable(String name) {
+        return new NodeImpl(Variable.create(name));
+    }
 
-	@Override
-	public boolean isVariable() {
-		// TODO Auto-generated method stub
-		return atom.isVariable() ; //&& ! atom.getVariable().isBlankNode();
-	}
-	
-	public boolean isBlank() {
-		// TODO Auto-generated method stub
-		return atom.isBlank() || (isVariable() && atom.getVariable().isBlankNode());
-	}
-        
-        public boolean isFuture(){
-            return isConstant() && atom.getConstant().getDatatypeValue().isFuture();
+    public static NodeImpl createResource(String name) {
+        return new NodeImpl(Constant.create(name));
+    }
+
+    public static NodeImpl createConstant(String name) {
+        return new NodeImpl(Constant.create(name, RDFS.xsdstring));
+    }
+
+    public static NodeImpl createConstant(String name, String datatype) {
+        return new NodeImpl(Constant.create(name, datatype));
+    }
+
+    public static NodeImpl createConstant(String name, String datatype, String lang) {
+        return new NodeImpl(Constant.create(name, null, lang));
+    }
+
+    public Atom getAtom() {
+        return atom;
+    }
+
+    @Override
+    public IDatatype getValue() {
+        return atom.getDatatypeValue();
+    }
+    
+    public IDatatype getValue(Node n) {
+        return (IDatatype) n.getValue();    
+    }
+
+    @Override
+    public IDatatype getDatatypeValue() {
+        return atom.getDatatypeValue();
+    }
+
+    @Override
+    public String toString() {
+        return atom.toSparql();
+    }
+
+    @Override
+    public int compare(Node node) {
+        if (node.getValue() instanceof IDatatype) {
+            return getValue().compareTo(getValue(node));
         }
+        return getLabel().compareTo(node.getLabel());
+    }
 
+    @Override
+    public int getIndex() {
+        // TODO Auto-generated method stub
+        return index;
+    }
 
-	@Override
-	public boolean same(Node n) {
-		if (isVariable()){
-			if (n.isVariable()){
-				return getLabel().equals(n.getLabel());
-			}
-			else {
-				return false;
-			}
-		}
-		else if (n.isVariable()){
-			return false;
-		}
-		
-		return compare(n) == 0;
-	}
-	
-	public boolean equals(Object o) {
-		if (o instanceof Node){
-			return same((Node) o);
-		}
-		return false;
-	}
-	
+    @Override
+    public String getLabel() {
+        // TODO Auto-generated method stub
+        if (atom.isResource()) {
+            return atom.getLongName();
+        }
+        return atom.getName();
+    }
 
-	@Override
-	public void setIndex(int n) {
-		// TODO Auto-generated method stub
-		index = n;
-	}
+    @Override
+    public boolean isConstant() {
+        // TODO Auto-generated method stub
+        return atom.isConstant();
+    }
 
-	@Override
-	public Object getObject() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean isVariable() {
+        // TODO Auto-generated method stub
+        return atom.isVariable(); //&& ! atom.getVariable().isBlankNode();
+    }
 
-	@Override
-	public void setObject(Object o) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public boolean isBlank() {
+        // TODO Auto-generated method stub
+        return atom.isBlank() || (isVariable() && atom.getVariable().isBlankNode());
+    }
 
-	@Override
-	public Object getProperty(int p) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean isFuture() {
+        return isConstant() && atom.getConstant().getDatatypeValue().isFuture();
+    }
 
-	@Override
-	public void setProperty(int p, Object o) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public boolean same(Node n) {
+        if (isVariable() || n.isVariable()) {
+            return sameVariable(n);
+        }       
+        return getValue().sameTerm(getValue(n));
+        //return compare(n) == 0;
+    }
+    
+    boolean sameVariable(Node n){      
+        return isVariable() && n.isVariable() && getLabel().equals(n.getLabel());
+    }
+
+    @Override
+    public boolean match(Node n) {
+       if (isVariable() || n.isVariable()) {
+            return sameVariable(n);
+        }
+        return getValue().match(getValue(n));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Node) {
+            return equals((Node) o); // was same
+        }
+        return false;
+    }
+    
+     public boolean equals(Node n) {
+        if (isVariable() || n.isVariable()) {
+            return sameVariable(n);
+        }
+        return getValue().equals(getValue(n));
+     }
+
+    @Override
+    public void setIndex(int n) {
+        // TODO Auto-generated method stub
+        index = n;
+    }
+
+    @Override
+    public Object getObject() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setObject(Object o) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Object getProperty(int p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setProperty(int p, Object o) {
+        // TODO Auto-generated method stub
+    }
 
     @Override
     public String getKey() {
@@ -173,5 +187,4 @@ public class NodeImpl implements Node {
     public TripleStore getTripleStore() {
         return null;
     }
-
 }

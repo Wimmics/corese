ddc5f3071e5c6a3449dd7a25ab3a67fed61f0962
corby <corby@esteron.inria.fr>
2017-06-12 09:21:53 +0200

    Proposal for buffer edge creation (not used in this version)

--- tinkerpop/src/main/java/fr/inria/corese/tinkerpop/mapper/TinkerpopToCorese.java
+++ tinkerpop/src/main/java/fr/inria/corese/tinkerpop/mapper/TinkerpopToCorese.java
@@ -17,6 +17,8 @@ import static fr.inria.wimmics.rdf_to_bd_map.RdfToBdMap.*;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import fr.inria.acacia.corese.api.IDatatype;
+import fr.inria.edelweiss.kgraph.core.edge.EdgeGeneric;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,62 +27,85 @@ import java.util.concurrent.TimeUnit;
  */
 public class TinkerpopToCorese {
 
-	LoadingCache<Edge, Entity> cache;
+    LoadingCache<Edge, Entity> cache;
+    EdgeGeneric edge;
+    Node name;
 
-	public TinkerpopToCorese(Graph g) {
-		this.cache = CacheBuilder.newBuilder().
-			maximumSize(1000000).
-			expireAfterAccess(100, TimeUnit.DAYS).
-			build(new CacheLoader<Edge, Entity>() {
-				@Override
-				public Entity load(Edge e) throws Exception {
-					String graph = e.value(EDGE_G);
-					Entity result = EdgeQuad.create(DatatypeMap.createResource(graph),
-						unmapNode(e.outVertex()),
-						DatatypeMap.createResource((String) e.value(EDGE_P)),
-						unmapNode(e.inVertex())
-					);
-					return result;
-				}
-			});
-	}
+    public TinkerpopToCorese(Graph g) {
+        edge = new EdgeGeneric();
+        this.cache = CacheBuilder.newBuilder().
+                maximumSize(1000000).
+                expireAfterAccess(100, TimeUnit.DAYS).
+                build(new CacheLoader<Edge, Entity>() {
+                    IDatatype name, predicate;
 
-	/**
-	 * Returns a Corese Entity from a Tinkerpop edge.
-	 *
-	 * @param e
-	 */
-	public Entity buildEntity(Edge e) {
-		return cache.getUnchecked(e);
-	}
+                    @Override
+                    public Entity load(Edge e) throws Exception {
+                        //String graph = e.value(EDGE_G);
+                        if (name == null) {
+                            name = DatatypeMap.createResource((String)e.value(EDGE_G));
+                        }
+                        String property = (String) e.value(EDGE_P);
+                        if (predicate == null || !predicate.getLabel().equals(property)) {
+                            predicate = DatatypeMap.createResource(property);
+                        }
+                        Entity result = EdgeQuad.create(name,
+                                unmapNode(e.outVertex()),
+                                predicate,
+                                unmapNode(e.inVertex())
+                        );
+                        return result;
+                    }
+                });
+    }
 
-	public Node unmapNode(Vertex node) {
-		String id = (String) node.value(VERTEX_VALUE);
-		switch ((String) node.value(KIND)) {
-			case IRI:
-				return DatatypeMap.createResource(id);
-			case BNODE:
-				return DatatypeMap.createBlank(id);
-			case LITERAL:
-				String label = (String) node.value(VERTEX_VALUE);
-				String type = (String) node.value(TYPE);
-				VertexProperty<String> lang = node.property(LANG);
-				if (lang.isPresent()) {
-					return DatatypeMap.createLiteral(label, type, lang.value());
-				} else {
-					return DatatypeMap.createLiteral(label, type);
-				}
-			case LARGE_LITERAL:
-				label = (String) node.value(VERTEX_LARGE_VALUE);
-				type = (String) node.value(TYPE);
-				lang = node.property(LANG);
-				if (lang.isPresent()) {
-					return DatatypeMap.createLiteral(label, type, lang.value());
-				} else {
-					return DatatypeMap.createLiteral(label, type);
-				}
-			default:
-				throw new IllegalArgumentException("node " + node.toString() + " type is unknown.");
-		}
-	}
+    /**
+     * Returns a Corese Entity from a Tinkerpop edge.
+     *
+     * @param e
+     */
+    public Entity buildEntity(Edge e) {
+        return cache.getUnchecked(e);
+    }
+    
+    public Entity buildEntity2(Edge e) {
+        if (name == null){
+            name = DatatypeMap.createResource(e.value(EDGE_G));
+        }
+        edge.setGraph(name);
+        edge.setEdgeNode(DatatypeMap.createResource(e.value(EDGE_P)));
+        edge.setNode(0, unmapNode(e.outVertex()));
+        edge.setNode(1, unmapNode(e.inVertex()));
+        return edge;
+    }
+
+    public Node unmapNode(Vertex node) {
+        String id = (String) node.value(VERTEX_VALUE);
+        switch ((String) node.value(KIND)) {
+            case IRI:
+                return DatatypeMap.createResource(id);
+            case BNODE:
+                return DatatypeMap.createBlank(id);
+            case LITERAL:
+                String label = (String) node.value(VERTEX_VALUE);
+                String type = (String) node.value(TYPE);
+                VertexProperty<String> lang = node.property(LANG);
+                if (lang.isPresent()) {
+                    return DatatypeMap.createLiteral(label, type, lang.value());
+                } else {
+                    return DatatypeMap.createLiteral(label, type);
+                }
+            case LARGE_LITERAL:
+                label = (String) node.value(VERTEX_LARGE_VALUE);
+                type = (String) node.value(TYPE);
+                lang = node.property(LANG);
+                if (lang.isPresent()) {
+                    return DatatypeMap.createLiteral(label, type, lang.value());
+                } else {
+                    return DatatypeMap.createLiteral(label, type);
+                }
+            default:
+                throw new IllegalArgumentException("node " + node.toString() + " type is unknown.");
+        }
+    }
 }

296f5a15d57bc6fd3c904cf34b090bcbf6e257ad
corby <corby@esteron.inria.fr>
2017-06-19 09:10:39 +0200

    Two getEdges @test annotation exploits relevant filters

--- tinkerpop/src/main/java/fr/inria/corese/tinkerpop/TinkerpopProducer.java
+++ tinkerpop/src/main/java/fr/inria/corese/tinkerpop/TinkerpopProducer.java
@@ -5,7 +5,10 @@ package fr.inria.corese.tinkerpop;
 
 import fr.inria.acacia.corese.triple.parser.ASTQuery;
 import fr.inria.acacia.corese.triple.parser.Metadata;
+import fr.inria.acacia.corese.triple.parser.Variable;
 import fr.inria.corese.rdftograph.driver.GdbDriver;
+import fr.inria.edelweiss.kgenv.parser.NodeImpl;
+import fr.inria.edelweiss.kgram.api.core.DatatypeValue;
 import fr.inria.edelweiss.kgram.api.core.Edge;
 import fr.inria.edelweiss.kgram.api.core.Entity;
 import fr.inria.edelweiss.kgram.api.core.Expr;
@@ -42,50 +45,91 @@ public class TinkerpopProducer extends ProducerImpl {
         this.graph = graph;
     }
 
+    
     /**
-     * @return An iterable
-     * @param gNode @TODO Not used for the moment
-     * @param from @TODO Not used for the moment
-     * @param qEdge Requested edge.
-     * @param env Provided values that can set the values for all the Nodes of
-     * the request.
+     * @test annotation exploits relevant filters for query edge
      */
     @Override
     public Iterable<Entity> getEdges(Node gNode, List<Node> from, Edge qEdge, Environment env) {
+        ASTQuery ast = (ASTQuery) env.getQuery().getAST();
+        if (ast.hasMetadata(Metadata.TEST)) {
+            return getEdgesNew(gNode, from, qEdge, env);
+        }
+        else {
+            return getEdgesSave(gNode, from, qEdge, env);            
+        }
+        
+    }
+   
+    /**
+     * Generate Entity iterable
+     * Exploit variable value, constant in edge and filters that are relevant for current edge
+     * filter (?o = "test") generates Tinkerpop predicate P.eq("test") This is done in the Driver
+     * Values are passed as DatatypeValue in order to distinguish numeric, string and boolean values
+     * @param gNode
+     * @param from
+     * @param qEdge
+     * @param env
+     * @return 
+     */
+    public Iterable<Entity> getEdgesNew(Node gNode, List<Node> from, Edge qEdge, Environment env) {
         Exp exp = env.getExp();
         Query q = env.getQuery();
         ASTQuery ast = (ASTQuery) q.getAST();
-        if (!ast.hasMetadata(Metadata.TEST)) {
-            // uncomment to exploit exp filter only when @test annotation
-            //exp = null;
-        }
         boolean isDebug = q.isDebug();
         Node subject = qEdge.getNode(0);
-        Node object = qEdge.getNode(1);
-        Node predicate = qEdge.getEdgeVariable();
-        Node property = (predicate == null) ? qEdge.getEdgeNode() : predicate;
-        boolean propVar = predicate != null || isPredicateFree(qEdge);
+        Node object  = qEdge.getNode(1);
+        Node predicate = getPredicate(qEdge);
 
         Function<GraphTraversalSource, GraphTraversal<? extends org.apache.tinkerpop.gremlin.structure.Element, org.apache.tinkerpop.gremlin.structure.Edge>> filter;
-        StringBuilder key = new StringBuilder();
 
-        String g = (gNode == null) ? "" : gNode.getLabel();
-        key.append((gNode == null) || (gNode.getLabel().compareTo("?g") == 0) ? "?g" : "G");
-
-        String s = updateVariable(exp, Exp.SUBJECT, subject.isVariable(), subject, env, key, isDebug);
-        String p = updateVariable(exp, Exp.PREDICATE, propVar, property, env, key, isDebug);
-        String o = updateVariable(exp, Exp.OBJECT, object.isVariable(), object, env, key, isDebug);
-        //LOGGER.trace("in case " + key.toString());
-        if (!ast.hasMetadata(Metadata.TEST)) {
-            // exploit exp filter when @test annotation
-            exp = null;
+        DatatypeValue s = updateVariable(exp, Exp.SUBJECT,   subject,   env, isDebug);
+        DatatypeValue p = updateVariable(exp, Exp.PREDICATE, predicate, env, isDebug);
+        DatatypeValue o = updateVariable(exp, Exp.OBJECT,    object,    env, isDebug);
+        DatatypeValue g = null;
+        if (gNode != null){
+            g = updateVariable(exp, Exp.GRAPH_NAME, gNode, env, isDebug);
         }
-        filter = databaseDriver.getFilter(exp, key.toString(), s, p, o, g);
+        
+        filter = databaseDriver.getFilter(exp, s, p, o, g);
         if (q.isDebug()) {
-            System.out.println("TK: " + key + " " + s + " " + p + " " + o);
+            System.out.println("TK: " +  " " + s + " " + p + " " + o);
         }
         Iterable<Entity> result = graph.getEdges(filter);
         return result;
+    }
+    
+    
+    private DatatypeValue updateVariable(Exp exp, int rank, Node node, Environment env, boolean isDebug) {
+        DatatypeValue result = null;
+        if (node.isVariable()) {
+            Node value = env.getNode(node.getLabel());
+            if (value != null) {
+                // var bound
+                result = value.getDatatypeValue();
+            }         
+        } else {
+            result = node.getDatatypeValue();
+        }
+        return result;
+    }
+    
+    
+    /**
+     * Generate a fake variable Node when property = TopLevel property
+     * @param e
+     * @return 
+     */
+    Node getPredicate(Edge e){
+        Node predicate = e.getEdgeVariable();
+        if (predicate != null){
+            return predicate;
+        }
+        Node property =  e.getEdgeNode();
+        if (property.getLabel().equals(Graph.TOPREL)){
+            return new NodeImpl(new Variable("?_tmp_var_"));
+        }
+        return property;
     }
 
     /**
@@ -94,28 +138,14 @@ public class TinkerpopProducer extends ProducerImpl {
     @Override
     public Iterable<Entity> getEdges(Node gNode, Node sNode, List<Node> from,
             Node predicate, Node focusNode, Node objectNode, int n) {
-        String key = "?gSP?o";
-        if (n == 1) {
-            key = "?g?sPO";
-        }
-        String s = (n == 0) ? focusNode.getLabel() : "?s";
-        String o = (n == 1) ? focusNode.getLabel() : "?o";
+
+        DatatypeValue s = (n == 0) ? focusNode.getDatatypeValue() : null;
+        DatatypeValue o = (n == 1) ? focusNode.getDatatypeValue() : null;
 
         Function<GraphTraversalSource, GraphTraversal<? extends org.apache.tinkerpop.gremlin.structure.Element, org.apache.tinkerpop.gremlin.structure.Edge>> filter;
-        filter = databaseDriver.getFilter(key, s, predicate.getLabel(), o, "?g");
+        filter = databaseDriver.getFilter(null, s, predicate.getDatatypeValue(), o, null);
         Iterable<Entity> result = graph.getEdges(filter);
         return result;
-    }
-
-    /**
-     *
-     * @param edge
-     * @return
-     */
-    private boolean isPredicateFree(Edge edge) {
-        Node predicate = edge.getEdgeNode();
-        String name = predicate.getLabel();
-        return name.equals(Graph.TOPREL);
     }
 
     @Override
@@ -138,6 +168,42 @@ public class TinkerpopProducer extends ProducerImpl {
         graph.close();
     }
 
+     /**
+     * @return An iterable
+     * @param gNode @TODO Not used for the moment
+     * @param from @TODO Not used for the moment
+     * @param qEdge Requested edge.
+     * @param env Provided values that can set the values for all the Nodes of
+     * the request.
+     */
+    //@Override
+    public Iterable<Entity> getEdgesSave(Node gNode, List<Node> from, Edge qEdge, Environment env) {
+        Exp exp = env.getExp();
+        Query q = env.getQuery();
+        boolean isDebug = q.isDebug();
+        Node subject = qEdge.getNode(0);
+        Node object  = qEdge.getNode(1);
+        Node predicate = getPredicate(qEdge);
+
+        Function<GraphTraversalSource, GraphTraversal<? extends org.apache.tinkerpop.gremlin.structure.Element, org.apache.tinkerpop.gremlin.structure.Edge>> filter;
+        StringBuilder key = new StringBuilder();
+
+        String g = (gNode == null) ? "" : gNode.getLabel();
+        key.append((gNode == null) || (gNode.isVariable()) ? "?g" : "G");
+
+        String s = updateVariableSave(exp, Exp.SUBJECT,   subject, env, key, isDebug);
+        String p = updateVariableSave(exp, Exp.PREDICATE, predicate, env, key, isDebug);
+        String o = updateVariableSave(exp, Exp.OBJECT,    object, env, key, isDebug);
+        //LOGGER.trace("in case " + key.toString());       
+        filter = databaseDriver.getFilter(exp, key.toString(), s, p, o, g);
+        if (q.isDebug()) {
+            System.out.println("TK: " + key + " " + s + " " + p + " " + o);
+        }
+        Iterable<Entity> result = graph.getEdges(filter);
+        return result;
+    }
+    
+    
     String free(int rank) {
         switch (rank) {
             case Exp.SUBJECT:
@@ -161,38 +227,16 @@ public class TinkerpopProducer extends ProducerImpl {
         }
         return "";
     }
-
-    private String updateVariable(Exp exp, int rank, boolean isVariable, Node node, Environment env, StringBuilder key, boolean isDebug) {
+          
+    
+    private String updateVariableSave(Exp exp, int rank, Node node, Environment env, StringBuilder key, boolean isDebug) {
         String result = "";
-        if (isVariable) {
+        if (node.isVariable()) {
             if (env.getNode(node.getLabel()) != null) {
                 // var bound
                 key.append(bound(rank));
                 result = env.getNode(node.getLabel()).getLabel();
-            } else if (exp != null) {
-                // filter list for this edge node rank available in tinkerpop
-                // filter such as: var = cst where var is node rank and cst is constant
-                // boolean connector filter: f && f || ! f
-                List<Filter> list = exp.getFilters(rank, ExprType.TINKERPOP);
-                if (isDebug && !list.isEmpty()) {
-                    System.out.println(list);
-                }
-
-                boolean ok = false;
-                for (Filter f : list) {
-                    Expr e = f.getExp();
-                    if (e.match(ExprType.EQ_SAME)) {
-                        // filter (var = cst) or sameTerm(var, cst)
-                        key.append(bound(rank));
-                        result = e.getExp(1).getDatatypeValue().stringValue();
-                        ok = true;
-                        break;
-                    }
-                }
-                if (!ok) {
-                    key.append(free(rank));
-                }
-            } else {
+            }  else {
                 key.append(free(rank));
             }
         } else {
@@ -201,4 +245,5 @@ public class TinkerpopProducer extends ProducerImpl {
         }
         return result;
     }
+    
 }

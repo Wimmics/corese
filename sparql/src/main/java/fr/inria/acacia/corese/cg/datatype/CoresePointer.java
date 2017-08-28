53c262c4f95ffcf72a148c036329bf5c6836983a
corby <corby@esteron.inria.fr>
2017-08-28 11:29:29 +0200

    leverage pointer datatypes

--- sparql/src/main/java/fr/inria/acacia/corese/cg/datatype/CoresePointer.java
+++ sparql/src/main/java/fr/inria/acacia/corese/cg/datatype/CoresePointer.java
@@ -11,7 +11,14 @@ import fr.inria.edelweiss.kgram.api.core.Pointerable;
  *
  */
 public class CoresePointer extends CoreseUndefLiteral {
-    private static final IDatatype dt = getGenericDatatype(IDatatype.POINTER);
+    private static final IDatatype dt           = getGenericDatatype(IDatatype.POINTER);
+    private static final IDatatype graph_dt     = getGenericDatatype(IDatatype.GRAPH_DATATYPE);
+    private static final IDatatype triple_dt    = getGenericDatatype(IDatatype.TRIPLE_DATATYPE);
+    private static final IDatatype mappings_dt  = getGenericDatatype(IDatatype.MAPPINGS_DATATYPE);
+    private static final IDatatype mapping_dt   = getGenericDatatype(IDatatype.MAPPING_DATATYPE);
+    private static final IDatatype context_dt   = getGenericDatatype(IDatatype.CONTEXT_DATATYPE);
+    private static final IDatatype nsmanager_dt = getGenericDatatype(IDatatype.NSM_DATATYPE);
+    private static final IDatatype annotation_dt= getGenericDatatype(IDatatype.METADATA_DATATYPE);
 
     Pointerable pobject;
     
@@ -26,7 +33,16 @@ public class CoresePointer extends CoreseUndefLiteral {
     
     @Override
     public IDatatype getDatatype() {
-        return dt;
+        switch (pointerType()){
+            case Pointerable.ENTITY_POINTER:    return triple_dt;
+            case Pointerable.GRAPH_POINTER:     return graph_dt;
+            case Pointerable.MAPPINGS_POINTER:  return mappings_dt;
+            case Pointerable.MAPPING_POINTER:   return mapping_dt;
+            case Pointerable.CONTEXT_POINTER:   return context_dt;
+            case Pointerable.NSMANAGER_POINTER: return nsmanager_dt;
+            case Pointerable.METADATA_POINTER:  return annotation_dt;
+            default: return dt;
+        }
     }
     
     @Override

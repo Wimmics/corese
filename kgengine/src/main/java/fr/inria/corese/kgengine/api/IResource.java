a00b6f0ec1fbe7ffeef80b39729858ed2b009bef
Erwan Demairy <erwan.demairy@inria.fr>
2018-03-22 15:19:17 +0100

    Removed the obsoletes packages

--- kgengine/src/main/java/fr/inria/corese/kgengine/api/IResource.java
+++
@@ -1,9 +0,0 @@
-package fr.inria.corese.kgengine.api;
-
-public interface IResource extends IResultValue {
-	
-	public IType getIType();
-
-	public String getType();
-
-}
--- rif/src/main/java/fr/inria/corese/rif/ast/Symbol.java
+++
@@ -1,5 +0,0 @@
-package fr.inria.corese.rif.ast;
-
-public abstract class Symbol {
-
-}

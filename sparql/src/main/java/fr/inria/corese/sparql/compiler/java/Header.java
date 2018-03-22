4bf79b1e714c8ab8c56f7e714d93ecddb52501ac
corby <olivier.corby@inria.fr>
2018-03-22 13:40:13 +0100

    Rename datatype package

--- sparql/src/main/java/fr/inria/corese/sparql/compiler/java/Header.java
+++ sparql/src/main/java/fr/inria/corese/sparql/compiler/java/Header.java
@@ -12,7 +12,7 @@ public class Header {
     
  static final String importList = 
               "import fr.inria.corese.sparql.api.IDatatype;\n"
-            + "import fr.inria.corese.sparql.cg.datatype.DatatypeMap;\n"
+            + "import fr.inria.corese.sparql.datatype.DatatypeMap;\n"
             + "import fr.inria.corese.sparql.triple.function.extension.*;\n"  
             + "import fr.inria.corese.extension.core.Core;\n";
  

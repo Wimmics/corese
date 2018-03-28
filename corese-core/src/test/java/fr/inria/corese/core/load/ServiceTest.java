cb0f4708981ec6068c1a4a281a18ea1a68fb1823
Erwan Demairy <erwan.demairy@inria.fr>
2018-03-28 11:21:56 +0200

    Update from jersey 1.x to jersey 2.25.1 (jersey 2.26 seems bugged).

---
+++ corese-core/src/test/java/fr/inria/corese/core/load/ServiceTest.java
@@ -0,0 +1,26 @@
+package fr.inria.corese.core.load;
+
+import fr.inria.corese.kgram.core.Mappings;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServiceTest {
+    private Logger logger = LoggerFactory.getLogger(ServiceTest.class);
+    private Service service;
+    @Before
+    public void setUp() {
+        service = new Service("https://dbpedia.org/sparql");
+    }
+
+    @Test
+    public void select() throws LoadException {
+        Mappings mappings = service.select("select distinct ?Concept where {[] a ?Concept} LIMIT 100");
+        logger.info("returned mappings = {}", mappings);
+        assertEquals(100, mappings.size());
+    }
+}

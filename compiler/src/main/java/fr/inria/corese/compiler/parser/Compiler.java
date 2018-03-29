11001b30fb74a894ce5cefa6f11b4b913cd09622
Erwan Demairy <erwan.demairy@inria.fr>
2018-03-29 10:33:31 +0200

    [minor] following some checkstyle advices on imports and javadoc comments.

--- compiler/src/main/java/fr/inria/corese/compiler/parser/Compiler.java
+++ compiler/src/main/java/fr/inria/corese/compiler/parser/Compiler.java
@@ -1,66 +1,55 @@
 package fr.inria.corese.compiler.parser;
 
-import java.util.List;
-
-import fr.inria.corese.sparql.triple.parser.ASTQuery;
-import fr.inria.corese.sparql.triple.parser.Atom;
-import fr.inria.corese.sparql.triple.parser.Expression;
-import fr.inria.corese.sparql.triple.parser.Triple;
 import fr.inria.corese.kgram.api.core.Edge;
 import fr.inria.corese.kgram.api.core.Filter;
 import fr.inria.corese.kgram.api.core.Node;
 import fr.inria.corese.kgram.api.core.Regex;
+import fr.inria.corese.sparql.triple.parser.ASTQuery;
+import fr.inria.corese.sparql.triple.parser.Atom;
+import fr.inria.corese.sparql.triple.parser.Expression;
+import fr.inria.corese.sparql.triple.parser.Triple;
+
 import java.util.Collection;
+import java.util.List;
 
 /**
- * Compiler API for Transformer
+ * Compiler API for Transformer.
  * Generate target Edge/Node/Filter
- * 
- * @author Olivier Corby, Edelweiss, INRIA 2010
  *
+ * @author Olivier Corby, Edelweiss, INRIA 2010
  */
 
 public interface Compiler {
-	
-	void setAST(ASTQuery ast);
-	
-	boolean isFail();
-			
-	Node createNode(String name);
-	
-	Node createNode(Atom at);
 
-	Node getNode();
-	
-	Edge getEdge();
-	
-	List<Filter> getFilters();
-        
-        Collection<Node> getVariables();
-	
-	Filter compile(Expression exp);
+  void setAST(ASTQuery ast);
 
-	List<Filter> compileFilter(Triple t);
-	
-	Edge compile(Triple t, boolean insertData);
-	
-	Regex getRegex(Filter f);
-		
-	String getMode(Filter f);
+  boolean isFail();
 
-	int getMin(Filter f);
-	
-	int getMax(Filter f);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+  Node createNode(String name);
+
+  Node createNode(Atom at);
+
+  Node getNode();
+
+  Edge getEdge();
+
+  List<Filter> getFilters();
+
+  Collection<Node> getVariables();
+
+  Filter compile(Expression exp);
+
+  Edge compile(Triple t, boolean insertData);
+
+  List<Filter> compileFilter(Triple t);
+
+  Regex getRegex(Filter f);
+
+  String getMode(Filter f);
+
+  int getMin(Filter f);
+
+  int getMax(Filter f);
 
 
 }

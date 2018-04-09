332d3fe62799f6492ce0287fad7b9dccf02cd1ec
Erwan Demairy <erwan.demairy@inria.fr>
2018-04-09 11:12:57 +0200

    correction of test in corese-server. Addition of a small test to understand DecimalFormat of icu4j.

---
+++ corese-core/src/test/java/fr/inria/corese/core/approximate/algorithm/UtilsTest.java
@@ -0,0 +1,19 @@
+package fr.inria.corese.core.approximate.algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest {
+
+    @Test
+    public void format() {
+        assertEquals(Utils.format(Math.PI, "#"), "3");
+    }
+
+    @Test
+    public void format1() {
+        assertEquals( "3.1416", Utils.format(Math.PI));
+        assertEquals( "314.1593", Utils.format(Math.PI* 100));
+    }
+}

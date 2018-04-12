c51cae21527e6b35d303967fe980dc855279d5fb
Erwan Demairy <erwan.demairy@inria.fr>
2018-04-12 19:00:46 +0200

    Removing icu dependency.

--- corese-core/src/test/java/fr/inria/corese/core/approximate/algorithm/UtilsTest.java
+++ corese-core/src/test/java/fr/inria/corese/core/approximate/algorithm/UtilsTest.java
@@ -7,11 +7,6 @@ import static org.junit.Assert.*;
 public class UtilsTest {
 
     @Test
-    public void format() {
-        assertEquals(Utils.format(Math.PI, "#"), "3");
-    }
-
-    @Test
     public void format1() {
         assertEquals( "3.1416", Utils.format(Math.PI));
         assertEquals( "314.1593", Utils.format(Math.PI* 100));

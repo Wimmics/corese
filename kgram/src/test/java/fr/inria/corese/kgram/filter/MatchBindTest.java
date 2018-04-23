776017f940affda513383155ed955ff3ac9889a4
Erwan Demairy <erwan.demairy@inria.fr>
2018-04-23 20:27:22 +0200

    compilation ok for compiler module with java 9.

--- kgram/src/test/java/fr/inria/corese/kgram/filter/MatchBindTest.java
+++ kgram/src/test/java/fr/inria/corese/kgram/filter/MatchBindTest.java
@@ -1,5 +1,22 @@
+package fr.inria.corese.kgram.filter;
+
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchBindTest {
 
+    @org.junit.jupiter.api.Test
+    void clean()
+    {
+        MatchBind m = new MatchBind();
+        IntStream.range( 0, 10 ).forEach(
+                n -> {
+                    m.setValue( Pattern.variable( "variable" + n ), Pattern.variable( "variable" + n ) );
+                    assertEquals( n + 1, m.size() );
+                }
+        );
+        m.clean(5);
+        assertEquals(5, m.size());
+    }
 }

d9aaac5205818253889bcaa072d88027b0ffa487
Erwan Demairy <erwan.demairy@inria.fr>
2018-05-02 13:23:29 +0200

    corese-core modularization ok.

---
+++ corese-core/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module fr.inria.corese.corese_core {
+	requires transitive fr.inria.corese.compiler;
+	requires org.slf4j;
+	requires java.logging;
+	requires java.xml;
+	requires jsonld.java;
+	requires arp;
+	requires java.ws.rs;
+	requires java.sql;
+	requires sesame.rio.api;
+	requires sesame.model;
+	requires commons.lang;
+	requires semargl.core;
+	requires semargl.rdfa;
+	requires java.management;
+	requires jdk.management;
+}

c86b85782e6b66b49b5b4f2177deb69dbc94f1d0
corby <olivier.corby@inria.fr>
2018-05-03 08:26:50 +0200

    leverage transformer api

--- sparql/src/main/java/fr/inria/corese/sparql/api/ComputerProxy.java
+++ sparql/src/main/java/fr/inria/corese/sparql/api/ComputerProxy.java
@@ -3,6 +3,8 @@ package fr.inria.corese.sparql.api;
 import fr.inria.corese.kgram.api.core.Expr;
 import fr.inria.corese.kgram.api.query.Environment;
 import fr.inria.corese.kgram.api.query.Producer;
+import fr.inria.corese.sparql.triple.parser.Context;
+import fr.inria.corese.sparql.triple.parser.NSManager;
 
 /**
  *
@@ -14,8 +16,19 @@ public interface ComputerProxy {
     ComputerProxy getComputerTransform();
     
     IDatatype eval(Expr exp, Environment env, Producer p, IDatatype[] param);
+    
     IDatatype function(Expr exp, Environment env, Producer p);
     IDatatype function(Expr exp, Environment env, Producer p, IDatatype dt);
     IDatatype function(Expr exp, Environment env, Producer p, IDatatype dt1, IDatatype dt2);
+    
+    IDatatype transform(IDatatype[] args, IDatatype focus, IDatatype trans, IDatatype temp, IDatatype name,
+            Expr exp, Environment env, Producer prod);
+    
+    IDatatype transform(IDatatype trans, IDatatype temp, IDatatype name, Expr exp, Environment env, Producer prod);
+    
+    Context getContext(Environment env, Producer p);
+    
+    NSManager getNSM(Environment env, Producer p);
+
     IDatatype hash(Expr exp, IDatatype dt);
 }

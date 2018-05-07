b22b7fac0c8c5834025e0ea8c7891f396a2e9a12
corby <olivier.corby@inria.fr>
2018-05-07 07:58:09 +0200

    Graph API Edge getGraph()

---
+++ kgram/src/main/java/fr/inria/corese/kgram/api/core/Graph.java
@@ -0,0 +1,17 @@
+package fr.inria.corese.kgram.api.core;
+
+/**
+ *
+ */
+public interface Graph {
+    
+    Edge getEdge(Node predicate, Node node, int n);
+    
+    Node value(Node subject, Node predicate, int n);
+    
+    Node getPropertyNode(Node dt);
+    
+    Node getNode(Node dt);
+
+    
+}

689b64f4550b609696f83e35ed31396d55aaeb07
corby <olivier.corby@inria.fr>
2018-05-17 16:03:39 +0200

    Leverage LDScript API

--- compiler/src/main/java/fr/inria/corese/compiler/parser/EdgeImpl.java
+++ compiler/src/main/java/fr/inria/corese/compiler/parser/EdgeImpl.java
@@ -16,223 +16,233 @@ import fr.inria.corese.kgram.core.PointerObject;
 
 public class EdgeImpl extends PointerObject implements Edge {
 
-	public static String TOP = RDFS.RootPropertyURI;
-	ArrayList<Node> nodes;
-	Node edgeNode, edgeVariable, mySelf;
-	String label;
-	Triple triple;
-	int index = -1;
-    
-	
-	public EdgeImpl(){
-		this(TOP);
-	}
-	
-	public EdgeImpl(String label){
-		this.label = label;
-		nodes = new ArrayList<Node>();
-	}
-	
-	public EdgeImpl(Triple t){
-		label = t.getProperty().getLongName();
-		triple = t;
-		nodes = new ArrayList<Node>();
-	}
-	
-	public static EdgeImpl create(String label, Node sub, Node obj){
-		EdgeImpl edge = new EdgeImpl(label);
-		edge.add(sub);
-		edge.add(obj);
-		return edge;
-	}
-	
-	public static EdgeImpl create(Node prop, Node sub, Node obj){
-		String name = TOP;
-		if (prop.isConstant()){
-			name = prop.getLabel();
-		}
-		EdgeImpl edge = new EdgeImpl(name);
-		edge.add(sub);
-		edge.add(obj);
-		if (prop.isVariable()){
-			//edge.setEdgeNode(prop);
-			edge.setEdgeVariable(prop);
-		}
-		else {
-			edge.setEdgeNode(prop);
-		}
-		return edge;
-	}
-	
-	
-        @Override
-	public String toString(){
-		String str = "";
-		String name = label;
-		if (getEdgeVariable()!=null) name = getEdgeVariable().toString();
-		else if (triple!=null) name = triple.getProperty().getName();
-		str += getNode(0) + " " + name ;
-		for (int i=1; i<nodes.size(); i++){
-			str += " " + getNode(i);
-		}
-		return str;
-	}
-        
-        @Override
-     public Iterable<DatatypeValue> getLoop() {
-       ArrayList<DatatypeValue> list = new ArrayList();
-       for (int i = 0; i<3; i++){
-           list.add(getValue(null, i));
-       }
-       return list;
+    public static String TOP = RDFS.RootPropertyURI;
+    ArrayList<Node> nodes;
+    Node edgeNode, edgeVariable, mySelf;
+    String label;
+    Triple triple;
+    int index = -1;
+
+    public EdgeImpl() {
+        this(TOP);
     }
-        
-     @Override
-     public DatatypeValue getValue(String var, int n){        
-        switch (n){
-            case 0: return getNode(0).getDatatypeValue();
-            case 1: return getPredicate().getDatatypeValue();                  
-            case 2: return getNode(1).getDatatypeValue();
+
+    public EdgeImpl(String label) {
+        this.label = label;
+        nodes = new ArrayList<Node>();
+    }
+
+    public EdgeImpl(Triple t) {
+        label = t.getProperty().getLongName();
+        triple = t;
+        nodes = new ArrayList<Node>();
+    }
+
+    public static EdgeImpl create(String label, Node sub, Node obj) {
+        EdgeImpl edge = new EdgeImpl(label);
+        edge.add(sub);
+        edge.add(obj);
+        return edge;
+    }
+
+    public static EdgeImpl create(Node prop, Node sub, Node obj) {
+        String name = TOP;
+        if (prop.isConstant()) {
+            name = prop.getLabel();
+        }
+        EdgeImpl edge = new EdgeImpl(name);
+        edge.add(sub);
+        edge.add(obj);
+        if (prop.isVariable()) {
+            //edge.setEdgeNode(prop);
+            edge.setEdgeVariable(prop);
+        } else {
+            edge.setEdgeNode(prop);
+        }
+        return edge;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        String name = label;
+        if (getEdgeVariable() != null) {
+            name = getEdgeVariable().toString();
+        } else if (triple != null) {
+            name = triple.getProperty().getName();
+        }
+        str += getNode(0) + " " + name;
+        for (int i = 1; i < nodes.size(); i++) {
+            str += " " + getNode(i);
+        }
+        return str;
+    }
+
+    @Override
+    public Iterable<DatatypeValue> getLoop() {
+        ArrayList<DatatypeValue> list = new ArrayList();
+        for (int i = 0; i <= nodes.size(); i++) {
+            list.add(getValue(null, i));
+        }
+        return list;
+    }
+    
+    @Override
+    public String getDatatypeLabel() {
+        return toString();
+    }
+
+    @Override
+    public DatatypeValue getValue(String var, int n) {
+        switch (n) {
+            case 0:
+                return getNode(0).getDatatypeValue();
+            case 1:
+                return getPredicate().getDatatypeValue();
+            default:
+                if (n <= nodes.size()) {
+                    return getNode(n - 1).getDatatypeValue();
+                }
         }
         return null;
-    }   
-     
-     public Node getPredicateNode(){
+    }
+
+    public Node getPredicateNode() {
         Node var = getEdgeVariable();
         return (var == null) ? getEdgeNode() : var;
-     }
-	
-	public Triple getTriple(){
-		if (triple == null) triple = triple();
-		return triple;
-	}
-	
-	Triple triple(){
-		Atom subject = ((NodeImpl)nodes.get(0)).getAtom();
-		Atom object  = ((NodeImpl)nodes.get(1)).getAtom();
-		Constant property = getName();
-		Variable variable = getVariable();
-		Triple triple = Triple.create(subject, property, variable, object);
-		return triple;
-	}
-	
-	Constant getName(){
-		Atom name;
-		if (edgeNode != null){
-			name = ((NodeImpl)edgeNode).getAtom();
-			return name.getConstant();
-		}
-		return Constant.create(label);
-	}
-	
-	Variable getVariable(){
-		if (edgeVariable != null){
-			return((NodeImpl)edgeVariable).getAtom().getVariable();
-		}
-		return null;
-	}
-	
-	public void add(Node node){
-		nodes.add(node);
-	}
-	
-	/**
-	 * 
-	 * Query edge node is stored only if it is a variable
-	 * otherwise it is useless and may lead to a pb when match subproperty 
-	 */
-	public void setEdgeNode(Node n){
-		edgeNode = n;
-	}
+    }
 
-	@Override
-	public boolean contains(Node n) {
-		// TODO Auto-generated method stub
-		return nodes.contains(n);
-	}
-
-	@Override
-	public Node getEdgeNode() {
-		// TODO Auto-generated method stub
-		return edgeNode;
-	}
-	
-        @Override
-	public Node getEdgeVariable() {
-		// TODO Auto-generated method stub
-		return edgeVariable;
-	}
-        
-        @Override
-        public Node getPredicate(){
-            if (edgeVariable == null){
-                return edgeNode;
-            }
-            return edgeVariable;
+    public Triple getTriple() {
+        if (triple == null) {
+            triple = triple();
         }
-	
-	public void setEdgeVariable(Node n){
-		edgeVariable = n;
-	}
+        return triple;
+    }
 
-	@Override
-	public int getIndex() {		
-		return index;
-	}
+    Triple triple() {
+        Atom subject = ((NodeImpl) nodes.get(0)).getAtom();
+        Atom object = ((NodeImpl) nodes.get(1)).getAtom();
+        Constant property = getName();
+        Variable variable = getVariable();
+        Triple triple = Triple.create(subject, property, variable, object);
+        return triple;
+    }
 
-	@Override
-	public String getLabel() {
-		return label;
-	}
+    Constant getName() {
+        Atom name;
+        if (edgeNode != null) {
+            name = ((NodeImpl) edgeNode).getAtom();
+            return name.getConstant();
+        }
+        return Constant.create(label);
+    }
 
-	@Override
-	public Node getNode(int n) {
-		return nodes.get(n);
-	}
+    Variable getVariable() {
+        if (edgeVariable != null) {
+            return ((NodeImpl) edgeVariable).getAtom().getVariable();
+        }
+        return null;
+    }
 
-	@Override
-	public int nbNode() {
-		return nodes.size();
-	}
-        
-        @Override
-	public int nbGraphNode() {
-		return nodes.size();
-	}
-        
-	@Override
-	public void setIndex(int n) {
-		index = n;
-	}
+    public void add(Node node) {
+        nodes.add(node);
+    }
 
-        @Override
-	public Edge getEdge() {
-		return this;
-	}
+    /**
+     *
+     * Query edge node is stored only if it is a variable otherwise it is
+     * useless and may lead to a pb when match subproperty
+     */
+    public void setEdgeNode(Node n) {
+        edgeNode = n;
+    }
 
-        @Override
-	public Node getGraph() {
-		return null;
-	}
+    @Override
+    public boolean contains(Node n) {
+        // TODO Auto-generated method stub
+        return nodes.contains(n);
+    }
 
-        @Override
-	public Node getNode() {
-            if (mySelf == null){
-                mySelf = DatatypeMap.createObject(this.toString(), this);
-            }
-            return mySelf;
-	}
+    @Override
+    public Node getEdgeNode() {
+        // TODO Auto-generated method stub
+        return edgeNode;
+    }
+
+    @Override
+    public Node getEdgeVariable() {
+        // TODO Auto-generated method stub
+        return edgeVariable;
+    }
+
+    @Override
+    public Node getPredicate() {
+        if (edgeVariable == null) {
+            return edgeNode;
+        }
+        return edgeVariable;
+    }
+
+    public void setEdgeVariable(Node n) {
+        edgeVariable = n;
+    }
+
+    @Override
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public Node getNode(int n) {
+        return nodes.get(n);
+    }
+
+    @Override
+    public int nbNode() {
+        return nodes.size();
+    }
+
+    @Override
+    public int nbGraphNode() {
+        return nodes.size();
+    }
+
+    @Override
+    public void setIndex(int n) {
+        index = n;
+    }
+
+    @Override
+    public Edge getEdge() {
+        return this;
+    }
+
+    @Override
+    public Node getGraph() {
+        return null;
+    }
+
+    @Override
+    public Node getNode() {
+        if (mySelf == null) {
+            mySelf = DatatypeMap.createObject(this.toString(), this);
+        }
+        return mySelf;
+    }
 
     @Override
     public Object getProvenance() {
-        return null;        
-    }
-    
-    public void setProvenance(Object obj){
-        
+        return null;
     }
 
+    public void setProvenance(Object obj) {
 
+    }
 
     @Override
     public int pointerType() {

681cce7b3c76c24af77ab68fc36f60841fe06f1c
Erwan Demairy <erwan.demairy@inria.fr>
2018-07-04 15:43:25 +0200

    Testing Http and Https as integration tests.

---
+++ corese-server/src/test/resources/launch_ssl_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+jacocoArgLine=$1
+buildPath=$2
+resourcePath=$3
+java ${jacocoArgLine} -jar ${buildPath}/corese-server-*-jar-with-dependencies.jar -lh -l ${resourcePath} -ssl -jks corese.inria.fr.jks -pwd coreseatwimmics &
+echo $! > SSL_SERVER_PID 

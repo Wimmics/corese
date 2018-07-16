af52052065f13c9ccee4f47957c0e0c68f3f0507
Erwan Demairy <erwan.demairy@inria.fr>
2018-07-16 13:50:24 +0200

    correction of the launching script so that it launches the server correctrly also on corese-s2 (fedora).

--- corese-server/src/test/resources/launch_ssl_server.sh
+++ corese-server/src/test/resources/launch_ssl_server.sh
@@ -2,5 +2,8 @@
 jacocoArgLine=$1
 buildPath=$2
 resourcePath=$3
-java ${jacocoArgLine} -jar ${buildPath}/corese-server-*-jar-with-dependencies.jar -lh -l ${resourcePath} -ssl -jks corese.inria.fr.jks -pwd coreseatwimmics -p 8080 -pssl 8443 &
-echo $! > SSL_SERVER_PID 
+COMMAND="java ${jacocoArgLine} -Dfile.encoding=UTF-8 -jar ${buildPath}/corese-server-*-jar-with-dependencies.jar -l ${resourcePath} -ssl -jks corese.inria.fr.jks -pwd coreseatwimmics  -p 8080 -ssl -pssl 8443 &"
+echo "Launching ${COMMAND}"
+eval $COMMAND 2>&1 > /tmp/corese.log
+sleep 5
+echo $! > SSL_SERVER_PID

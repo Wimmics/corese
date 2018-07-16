bde1564b3e53471d842ffb876165d34b3f9ac5db
Erwan Demairy <erwan.demairy@inria.fr>
2018-07-16 09:53:46 +0200

    setting http port to 8080.

--- corese-server/src/test/resources/launch_ssl_server.sh
+++ corese-server/src/test/resources/launch_ssl_server.sh
@@ -2,5 +2,5 @@
 jacocoArgLine=$1
 buildPath=$2
 resourcePath=$3
-java ${jacocoArgLine} -jar ${buildPath}/corese-server-*-jar-with-dependencies.jar -lh -l ${resourcePath} -ssl -jks corese.inria.fr.jks -pwd coreseatwimmics -pssl 8443 &
+java ${jacocoArgLine} -jar ${buildPath}/corese-server-*-jar-with-dependencies.jar -lh -l ${resourcePath} -ssl -jks corese.inria.fr.jks -pwd coreseatwimmics -p 8080 -pssl 8443 &
 echo $! > SSL_SERVER_PID 

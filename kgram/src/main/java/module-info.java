c84fc1535c9519da928d1b5d7c567adc2a19d6a2
ErwanDemairy <erwan.demairy@inria.fr>
2019-04-18 10:52:44 +0200

    Set the version number in the root pom.xml instead of setting it in the childs.

--- kgram/src/main/java/module-info.java
+++ kgram/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.inria.corese.kgram {
 	requires java.desktop;
 	requires org.slf4j;
+
 	exports fr.inria.corese.kgram.core;
 	exports fr.inria.corese.kgram.api.core;
 	exports fr.inria.corese.kgram.api.query;
--- sparql/src/main/java/module-info.java
+++ sparql/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module fr.inria.corese.sparql {
 	requires fr.inria.corese.kgram;
 	requires org.slf4j;
 	requires java.sql;
+
 	exports fr.inria.corese.sparql.triple.parser;
 	exports fr.inria.corese.sparql.exceptions;
 	exports fr.inria.corese.sparql.datatype;
@@ -13,10 +14,10 @@ module fr.inria.corese.sparql {
 	exports fr.inria.corese.sparql.triple.function.term;
 	exports fr.inria.corese.sparql.compiler.java;
 	exports fr.inria.corese.sparql.datatype.function;
-    exports fr.inria.corese.sparql.storage.api;
+	exports fr.inria.corese.sparql.storage.api;
 	exports fr.inria.corese.sparql.storage.util;
-    exports fr.inria.corese.sparql.triple.printer;
-    exports fr.inria.corese.sparql.triple.api;
+	exports fr.inria.corese.sparql.triple.printer;
+	exports fr.inria.corese.sparql.triple.api;
 	exports fr.inria.corese.sparql.triple.function.core;
 	exports fr.inria.corese.sparql.storage.fs;
 }

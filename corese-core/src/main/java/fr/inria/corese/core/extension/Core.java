12725eb590856e6389bd1016b158aa0f07448be0
corby <olivier.corby@inria.fr>
2019-10-22 17:27:23 +0200

    Leverage Java Compiler

---
+++ corese-core/src/main/java/fr/inria/corese/core/extension/Core.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.inria.corese.core.extension;
+
+import fr.inria.corese.core.query.PluginImpl;
+
+/**
+ *
+ * @author corby
+ */
+public class Core extends PluginImpl {
+    
+}

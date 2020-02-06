d62765846fee97c65dc2eaa6ebba60ef85c50dd9
corby <olivier.corby@inria.fr>
2020-02-06 13:41:30 +0100

    SHACL

--- corese-gui/src/main/java/fr/inria/corese/gui/core/MainFrame.java
+++ corese-gui/src/main/java/fr/inria/corese/gui/core/MainFrame.java
@@ -81,7 +81,7 @@ public class MainFrame extends JFrame implements ActionListener {
     static MainFrame singleton ;
     private static final long serialVersionUID = 1L;
     private static final int LOAD = 1;
-    private static final String TITLE = "Corese 4.1 - Wimmics INRIA I3S - 2020-01-01";
+    private static final String TITLE = "Corese 4.1 - Wimmics INRIA I3S - 2020-01-29";
     // On déclare notre conteneur d'onglets
     protected static JTabbedPane conteneurOnglets;
     // Compteur pour le nombre d'onglets query créés

e17ec50b7abfc73db361dd8b03ce1d79dafa7308
corby <olivier.corby@inria.fr>
2020-02-13 08:57:59 +0100

    display in GUI (like xt:print)

--- corese-gui/src/main/java/fr/inria/corese/gui/core/Callback.java
+++ corese-gui/src/main/java/fr/inria/corese/gui/core/Callback.java
@@ -9,7 +9,36 @@ import fr.inria.corese.sparql.datatype.DatatypeMap;
  */
 public class Callback {
     
-    public IDatatype display(IDatatype name, IDatatype node, IDatatype suc) {
+    public IDatatype display(IDatatype dt) {
+        return mdisplay(dt);
+    }
+    
+    public IDatatype display(IDatatype dt1, IDatatype dt2) {
+        return mdisplay(dt1, dt2);
+    }
+    
+    public IDatatype display(IDatatype dt1, IDatatype dt2, IDatatype dt3) {
+        return mdisplay(dt1, dt2, dt3);
+    }
+    
+    public IDatatype display(IDatatype dt1, IDatatype dt2, IDatatype dt3, IDatatype dt4) {
+        return mdisplay(dt1, dt2, dt3, dt4);
+    }
+    
+    public IDatatype display(IDatatype dt1, IDatatype dt2, IDatatype dt3, IDatatype dt4, IDatatype dt5) {
+        return mdisplay(dt1, dt2, dt3, dt4, dt5);
+    }
+    
+    public IDatatype mdisplay(IDatatype... param) {
+        StringBuilder sb = new StringBuilder();
+        for (IDatatype dt : param) {
+            sb.append(dt).append(" ");
+        }
+        MainFrame.display(sb.toString());
+        return DatatypeMap.TRUE;
+    }
+    
+    public IDatatype display1(IDatatype name, IDatatype node, IDatatype suc) {
         MainFrame.display(String.format("report: %s %s", name.getLabel(), node.getLabel(), suc));
         return DatatypeMap.TRUE;
     }

0023d7c89c4ad1f22ce13366752843d4f4a9c2d7
corby <olivier.corby@inria.fr>
2020-08-20 16:33:08 +0200

    values unnest dt:xml

--- sparql/src/main/java/fr/inria/corese/sparql/triple/function/extension/XML.java
+++ sparql/src/main/java/fr/inria/corese/sparql/triple/function/extension/XML.java
@@ -26,7 +26,7 @@ import fr.inria.corese.kgram.api.query.Environment;
 import fr.inria.corese.kgram.api.query.Producer;
 import fr.inria.corese.sparql.api.Computer;
 import fr.inria.corese.sparql.api.IDatatype;
-import fr.inria.corese.sparql.datatype.CoreseXML;
+import fr.inria.corese.sparql.datatype.extension.CoreseXML;
 import fr.inria.corese.sparql.triple.function.term.Binding;
 import fr.inria.corese.sparql.triple.function.term.TermEval;
 

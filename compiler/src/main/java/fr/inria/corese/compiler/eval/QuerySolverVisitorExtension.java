75e7f71e5eedeb2a7f9383eebd4ccb4ef3d9a08d
corby <olivier.corby@inria.fr>
2020-09-15 09:52:55 +0200

    set user defined visitor exec.query(q, new UserDefinedVisitor())
    QueruProcess.setVisitorName("fr.inria.corese...UserDefinedVisitor");

---
+++ compiler/src/main/java/fr/inria/corese/compiler/eval/QuerySolverVisitorExtension.java
@@ -0,0 +1,40 @@
+package fr.inria.corese.compiler.eval;
+
+import fr.inria.corese.kgram.api.core.DatatypeValue;
+import fr.inria.corese.kgram.api.core.Edge;
+import fr.inria.corese.kgram.core.Eval;
+import fr.inria.corese.kgram.core.Query;
+import fr.inria.corese.sparql.api.IDatatype;
+import java.util.List;
+
+/**
+ * Draf example of specific Visitor, e.g. implemented in Java instead of LDScript
+ * Use case: 
+ * for one query: exec.query(q, new QuerySolverVisitorExtension())
+ * for one query: exec.query(q, Mapping.create(vis));
+ * 
+ * for all query: QueryProcess.setSolverVisitorName("fr.inria.corese.compiler.eval.QuerySolverVisitorExtension");
+ * 
+ * @author Olivier Corby, Wimmics INRIA I3S, 2020
+ */
+public class QuerySolverVisitorExtension extends QuerySolverVisitor {
+    
+    public QuerySolverVisitorExtension() {}
+  
+    public QuerySolverVisitorExtension(Eval ev) {
+        super(ev);
+    }
+    
+    @Override
+    public IDatatype update(Query q, List<Edge> delete, List<Edge> insert) {
+        System.out.println("My Visitor update");
+        return super.update(q, delete, insert);
+    }
+    
+    @Override
+    public IDatatype insert(DatatypeValue path, Edge triple) {
+        System.out.println("My Visitor insert: " + triple);
+        return (IDatatype) path;
+        //return super.insert(path, triple);
+    }
+}

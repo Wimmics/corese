6ea448a8baefd41a7feda52f47ee3f17705182b0
corby <olivier.corby@inria.fr>
2020-09-28 16:37:52 +0200

    Leverage Exception

--- kgram/src/main/java/fr/inria/corese/kgram/core/SparqlException.java
+++ kgram/src/main/java/fr/inria/corese/kgram/core/SparqlException.java
@@ -5,12 +5,16 @@ package fr.inria.corese.kgram.core;
  * @author corby
  */
 public class SparqlException extends Exception {
-    
+
     public SparqlException() {
     }
-    
+
     public SparqlException(String message) {
-		super(message);
+        super(message);
     }
-    
+
+    public SparqlException(Exception e) {
+        super(e);
+    }
+
 }

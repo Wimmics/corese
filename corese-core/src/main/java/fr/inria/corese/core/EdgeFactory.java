1dff0b4c24435e5dcac348810f1b084270542f45
corby <olivier.corby@inria.fr>
2021-01-04 18:11:18 +0100

    Clean edge classes

--- corese-core/src/main/java/fr/inria/corese/core/EdgeFactory.java
+++ corese-core/src/main/java/fr/inria/corese/core/EdgeFactory.java
@@ -4,7 +4,6 @@ import fr.inria.corese.core.edge.*;
 import fr.inria.corese.sparql.datatype.DatatypeMap;
 import fr.inria.corese.kgram.api.core.Node;
 import fr.inria.corese.core.logic.Entailment;
-import fr.inria.corese.core.producer.DataFilter;
 import fr.inria.corese.core.query.QueryProcess;
 import fr.inria.corese.sparql.api.IDatatype;
 import fr.inria.corese.sparql.exceptions.EngineException;

ae255217dba2fc38ae3e881ebc83002b5280d64e
corby <olivier.corby@inria.fr>
2021-01-18 16:28:02 +0100

    new result format

--- sparql/src/main/java/fr/inria/corese/sparql/api/ResultFormatDef.java
+++ sparql/src/main/java/fr/inria/corese/sparql/api/ResultFormatDef.java
@@ -5,11 +5,20 @@ package fr.inria.corese.sparql.api;
  * @author corby
  */
 public interface ResultFormatDef {    
-    public static final int UNDEF_FORMAT   = -1;    
+    public static final int UNDEF_FORMAT   = -1;
+    
+    
+    public static final int TEXT_FORMAT    = 0;
+
     public static final int RDF_XML_FORMAT = 1;
     public static final int TURTLE_FORMAT  = 2;
-    public static final int JSON_LD_FORMAT = 3;    
+    public static final int TRIG_FORMAT    = 3;
+    public static final int JSON_LD_FORMAT = 4; 
+    
     public static final int XML_FORMAT     = 11;
     public static final int RDF_FORMAT     = 12;
-    public static final int JSON_FORMAT    = 13;     
+    public static final int JSON_FORMAT    = 13; 
+    public static final int CSV_FORMAT     = 14; 
+    public static final int TSV_FORMAT     = 15; 
+    
 }

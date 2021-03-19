488c3d6bc8117997551f28433447a8022a656f8e
corby <olivier.corby@inria.fr>
2021-03-19 17:02:50 +0100

    dt:graph(dt:mappings amap)

--- sparql/src/main/java/fr/inria/corese/sparql/triple/function/extension/Cast.java
+++ sparql/src/main/java/fr/inria/corese/sparql/triple/function/extension/Cast.java
@@ -4,39 +4,53 @@ import fr.inria.corese.kgram.api.query.Environment;
 import fr.inria.corese.sparql.exceptions.EngineException;
 import fr.inria.corese.kgram.api.query.Producer;
 import fr.inria.corese.sparql.api.Computer;
+import fr.inria.corese.sparql.api.GraphProcessor;
 import fr.inria.corese.sparql.api.IDatatype;
 import fr.inria.corese.sparql.datatype.DatatypeMap;
 import fr.inria.corese.sparql.triple.function.term.Binding;
 import fr.inria.corese.sparql.triple.function.term.TermEval;
 
 /**
- *
- * @author corby
+ * dt:list(term) -> cast term to list
+ * dt:graph(dt:mappings amap) -> create the Mappings graph with W3C Results schema.
  */
 public class Cast extends TermEval {
-    kind akind = kind.DEFAULT;
     
-    enum kind { DEFAULT, LIST };
+    static final String MAPPINGS_DATATYPE = IDatatype.MAPPINGS_DATATYPE;
+    
+    String datatype;
 
     public Cast() {}
     
     public Cast(String name, String lname) {
         super(name);
         setArity(1);
-        switch(lname) {
-            case IDatatype.LIST_DATATYPE: 
-                akind =  kind.LIST; break;
-        }
+        datatype = lname;       
     }
 
     @Override
     public IDatatype eval(Computer eval, Binding b, Environment env, Producer p) throws EngineException {
         IDatatype dt = getBasicArg(0).eval(eval, b, env, p);
+        
         if (dt == null) {
             return null;
         }
-        switch (akind) {
-            case LIST: return list(dt);
+        switch (datatype) {
+            case IDatatype.LIST_DATATYPE : return list(dt);
+            case IDatatype.GRAPH_DATATYPE: return graph(eval, dt);       
+        }
+        
+        return null;
+    }
+    
+    IDatatype graph(Computer eval, IDatatype dt) {
+        if (dt.isExtension()) {
+            switch (dt.getDatatypeURI()) {
+                case IDatatype.MAPPINGS_DATATYPE:
+                    // dt:graph(dt:mappings amap)
+                    GraphProcessor proc = eval.getGraphProcessor();
+                    return proc.graph(dt);
+            }
         }
         return null;
     }

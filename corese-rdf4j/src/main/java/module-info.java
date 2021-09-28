509bde1f0056e9554b7fc193f16a64c9520f9686
corby <olivier.corby@inria.fr>
2021-09-28 15:35:40 +0200

    Merge sparql and kgram, Node value is IDatatype

--- corese-rdf4j/src/main/java/module-info.java
+++ corese-rdf4j/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module fr.inria.corese.corese_rdf4j {
     requires fr.inria.corese.corese_core;
     requires fr.inria.corese.sparql;
-    requires fr.inria.corese.kgram;
+    //requires fr.inria.corese.kgram;
 }

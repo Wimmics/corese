57970d1393ff0fded7f3e5f6615a4a842721a233
corby <olivier.corby@inria.fr>
2022-09-06 09:51:50 +0200

    test storage

---
+++ corese-test/src/test/java/fr/inria/corese/test/storage/RuleWithStorage.java
@@ -0,0 +1,76 @@
+
+package fr.inria.corese.test.storage;
+
+import fr.inria.corese.core.Graph;
+import fr.inria.corese.core.api.DataManager;
+import fr.inria.corese.core.load.Load;
+import fr.inria.corese.core.load.LoadException;
+import fr.inria.corese.core.query.QueryProcess;
+import fr.inria.corese.core.rule.RuleEngine;
+import fr.inria.corese.core.transform.Transformer;
+import fr.inria.corese.sparql.exceptions.EngineException;
+import fr.inria.corese.storage.jenatdb1.JenaDataManager;
+
+/**
+ *
+ */
+public class RuleWithStorage {
+    
+    private static final String STORAGE = "/user/corby/home/AADemoNew/storage/humantest";
+    private static final String DATASET = "/user/corby/home/AADemoNew/human/human.rdf";
+    
+    
+    public static void main(String[] args) throws LoadException, EngineException {
+        new RuleWithStorage().test();
+    }
+    
+    
+    void test() throws LoadException, EngineException {
+        DataManager man = init();
+        //ruleengine(man);
+        transformer(man);
+    }
+    
+    void testGraph() throws LoadException, EngineException {
+        Graph g = initGraph();
+        //ruleengine(man);
+        transformer(g);
+    }
+    
+    void transformer(DataManager man) throws LoadException, EngineException {
+        Transformer t = Transformer.createWE(QueryProcess.create(man), Transformer.TURTLE);
+        //man.startReadTransaction();
+        System.out.println(t.transform());
+        //t.transform();
+        //man.endReadTransaction();
+    }
+    
+    DataManager init() throws LoadException {
+        JenaDataManager man = new JenaDataManager(STORAGE);
+        man.setReentrant(true);
+        Graph g = Graph.create();
+        Load ld = Load.create(g, man);
+        //ld.parse("/user/corby/home/AADemoNew/human/human.rdf");
+        return man;
+    }
+    
+    void transformer(Graph g) throws LoadException, EngineException {
+        Transformer t = Transformer.createWE(g, Transformer.TURTLE);
+        System.out.println(t.transform());
+    }
+    
+    Graph initGraph() throws LoadException {
+        Graph g = Graph.create();
+        Load ld = Load.create(g);
+        ld.parse(DATASET);
+        return g;
+    }
+    
+    void ruleengine(DataManager man) throws LoadException, EngineException {                
+        RuleEngine re = RuleEngine.create(Graph.create(), man);
+        re.setProfile(RuleEngine.Profile.OWLRL);
+        re.setTrace(true);
+        re.process();
+    }
+    
+}

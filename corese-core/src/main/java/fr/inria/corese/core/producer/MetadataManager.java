3ffcc722ce32246a74a50ca81e97b4007411d01e
corby <olivier.corby@inria.fr>
2022-09-08 13:57:50 +0200

    MetadataManager Placeholder to manage additional data related to a DataManager

--- corese-core/src/main/java/fr/inria/corese/core/producer/MetadataManager.java
+++ corese-core/src/main/java/fr/inria/corese/core/producer/MetadataManager.java
@@ -1,5 +1,6 @@
 package fr.inria.corese.core.producer;
 
+import fr.inria.corese.core.api.DataManager;
 import fr.inria.corese.core.logic.Distance;
 
 /**
@@ -8,18 +9,61 @@ import fr.inria.corese.core.logic.Distance;
  */
 public class MetadataManager {
 
+    private DataManager dataManager;
     private Distance distance;
+    private boolean debug = false;
     
     
-    public MetadataManager() {
+    public MetadataManager() {    
+    }
     
+    public MetadataManager(DataManager man) {
+        setDataManager(man);
+    }
+    
+    public void startReadTransaction() {
+        traceStart("read");
+    }
+    
+    public void endReadTransaction() {
+        traceEnd("read");
     }
     
     public void startWriteTransaction() {
+        traceStart("write");
     }
     
     public void endWriteTransaction() {
-        setDistance(null);
+        clean();
+        traceEnd("write");
+    }
+    
+    void clean() {
+        setDistance(null);    
+    }
+    
+    void traceStart(String name) {
+        if (isDebug()) {
+            System.out.println(
+                String.format("start %s %s", name,
+                        getDataManager().getStoragePath()));
+        }
+    }
+
+    void traceEnd(String name) {
+        if (isDebug()) {
+            System.out.println(
+                String.format("end %s %s", name,
+                        getDataManager().getStoragePath()));
+        }
+    }
+
+    public Distance getCreateDistance() {
+        if (getDistance() == null) {
+            setDistance(new Distance(getDataManager()));
+            getDistance().start();
+        }
+        return getDistance();
     }
 
     public Distance getDistance() {
@@ -28,6 +72,22 @@ public class MetadataManager {
 
     public void setDistance(Distance distance) {
         this.distance = distance;
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public void setDataManager(DataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
     }
     
     

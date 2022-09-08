f79b294f3cdd973dfb4dae740e1522dbb80eccd9
corby <olivier.corby@inria.fr>
2022-09-08 11:44:47 +0200

    Placeholder to manage additional data related to a DataManager

---
+++ corese-core/src/main/java/fr/inria/corese/core/producer/MetadataManager.java
@@ -0,0 +1,31 @@
+package fr.inria.corese.core.producer;
+
+import fr.inria.corese.core.logic.Distance;
+
+/**
+ * Corese object associated to DataManager
+ * enables corese core to manage additional data such as Distance
+ */
+public class MetadataManager {
+
+    private Distance distance;
+    
+    
+    public MetadataManager() {
+    
+    }
+    
+    public void endWriteTransaction() {
+        setDistance(null);
+    }
+
+    public Distance getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Distance distance) {
+        this.distance = distance;
+    }
+    
+    
+}

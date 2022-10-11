54ec8e8c6d3daa695fcd31c9d913bd8355ec191f
corby <olivier.corby@inria.fr>
2022-10-11 10:05:54 +0200

    Clean exists {} Move code from Interpreter ExistFunction

--- compiler/src/main/java/fr/inria/corese/compiler/eval/Interpreter.java
+++ compiler/src/main/java/fr/inria/corese/compiler/eval/Interpreter.java
@@ -120,120 +120,120 @@ public class Interpreter implements Computer, Evaluator, ExprType {
     }
 
 
-    Eval createEval(Eval currentEval, Expr exp, Environment env, Producer p) {
-        Exp pat = env.getQuery().getPattern(exp);
-        Memory memory = currentEval.createMemory(env, pat);
-        if (memory == null) {
-            return null;
-        }
-        // producer below must be original Producer, it is used for cast purpose
-        Eval eval = currentEval.copy(memory, p, exp.isSystem());
-        eval.setSubEval(true);
-        return eval;
-    }
+//    Eval createEval(Eval currentEval, Expr exp, Environment env, Producer p) {
+//        Exp pat = env.getQuery().getPattern(exp);
+//        Memory memory = currentEval.createMemory(env, pat);
+//        if (memory == null) {
+//            return null;
+//        }
+//        // producer below must be original Producer, it is used for cast purpose
+//        Eval eval = currentEval.copy(memory, p, exp.isSystem());
+//        eval.setSubEval(true);
+//        return eval;
+//    }
 
     /**
      * filter exists { } exists statement is also used to embed LDScript nested
      * query in this case it is tagged as system
      */
-    @Override
-    public IDatatype exist(Expr exp, Environment env, Producer p) throws EngineException {
-        try {
-            if (hasListener) {
-                listener.listen(exp);
-            }
-            if (exp.arity() == 1) {
-                // argument return a graph on which we evaluate the exists
-                //IDatatype res = eval(exp.getExp(0), env, p);
-                IDatatype res = exp.getExp(0).evalWE(this, env.getBind(), env, p);
-                if (res == ERROR_VALUE) {
-                    return ERROR_VALUE;
-                }
-                if (p.isProducer((Node) res)) {
-                    p = p.getProducer((Node) res, env);
-                }
-            }
-            Query q = env.getQuery();
-            Exp pat = q.getPattern(exp);
-            Node gNode = env.getGraphNode();
-            Eval currentEval = getEval(env);
-            Mappings map = null;
-
-            // in case of // evaluation of a pattern
-            synchronized (exp) {
-                if (exp.isSystem()) {
-                    // system generated for LDScript nested query 
-                    // e.g. for (?m in select where) {}
-                    // is compiled with internal system exists 
-                    // for (?m in exists {select where}){}
-                    Exp sub = pat.get(0).get(0);
-
-                    if (sub.isQuery()) {
-                        Query qq = sub.getQuery();
-                        qq.setFun(true);
-                        if (qq.isConstruct() || qq.isUpdate()) {
-                            Mappings m = currentEval.getSPARQLEngine().eval(gNode, qq, getMapping(env, qq), p);
-                            return DatatypeMap.createObject((m.getGraph() == null) ? p.getGraph() : m.getGraph());
-                        }
-                        if (qq.getService() != null) {
-                            // @federate <uri> let (?m = select where)
-                            Mappings m = currentEval.getSPARQLEngine().eval(qq, getMapping(env, qq), p);
-                            return DatatypeMap.createObject(m);
-                        } else {
-                            // let (?m = select where)
-                            if (testNewEval) {
-                                map = currentEval.getSPARQLEngine().eval(gNode, qq, getMapping(env, qq), p);
-                            } else {
-                                Eval eval = createEval(currentEval, exp, env, p);
-                                if (eval == null) {
-                                    return null;
-                                }
-                                map = eval.subEval(qq, gNode, Stack.create(sub), 0);
-                            }
-                        }
-                    } else {
-                        // never happen
-                        return null;
-                    }
-                } else {
-                    // SPARQL exists {}               
-                    Eval eval = createEval(currentEval, exp, env, p);
-                    if (eval == null) {
-                        return null;
-                    }
-                    eval.setLimit(1);
-                    map = eval.subEval(q, gNode, Stack.create(pat), 0);
-                }
-            }
-
-            boolean b = map.size() > 0;
-
-            if (exp.isSystem()) {
-                return DatatypeMap.createObject(map);
-            } else {
-                //report(q, env, map);
-                return DatatypeMap.newInstance(b);
-            }
-        } catch (SparqlException e) {
-            throw EngineException.cast(e);
-        }
-    }
+//    @Override
+//    public IDatatype exist(Expr exp, Environment env, Producer p) throws EngineException {
+//        try {
+//            if (hasListener) {
+//                listener.listen(exp);
+//            }
+//            if (exp.arity() == 1) {
+//                // argument return a graph on which we evaluate the exists
+//                //IDatatype res = eval(exp.getExp(0), env, p);
+//                IDatatype res = exp.getExp(0).evalWE(this, env.getBind(), env, p);
+//                if (res == ERROR_VALUE) {
+//                    return ERROR_VALUE;
+//                }
+//                if (p.isProducer((Node) res)) {
+//                    p = p.getProducer((Node) res, env);
+//                }
+//            }
+//            Query q = env.getQuery();
+//            Exp pat = q.getPattern(exp);
+//            Node gNode = env.getGraphNode();
+//            Eval currentEval = getEval(env);
+//            Mappings map = null;
+//
+//            // in case of // evaluation of a pattern
+//            synchronized (exp) {
+//                if (exp.isSystem()) {
+//                    // system generated for LDScript nested query 
+//                    // e.g. for (?m in select where) {}
+//                    // is compiled with internal system exists 
+//                    // for (?m in exists {select where}){}
+//                    Exp sub = pat.get(0).get(0);
+//
+//                    if (sub.isQuery()) {
+//                        Query qq = sub.getQuery();
+//                        qq.setFun(true);
+//                        if (qq.isConstruct() || qq.isUpdate()) {
+//                            Mappings m = currentEval.getSPARQLEngine().eval(gNode, qq, getMapping(env, qq), p);
+//                            return DatatypeMap.createObject((m.getGraph() == null) ? p.getGraph() : m.getGraph());
+//                        }
+//                        if (qq.getService() != null) {
+//                            // @federate <uri> let (?m = select where)
+//                            Mappings m = currentEval.getSPARQLEngine().eval(qq, getMapping(env, qq), p);
+//                            return DatatypeMap.createObject(m);
+//                        } else {
+//                            // let (?m = select where)
+//                            if (testNewEval) {
+//                                map = currentEval.getSPARQLEngine().eval(gNode, qq, getMapping(env, qq), p);
+//                            } else {
+//                                Eval eval = createEval(currentEval, exp, env, p);
+//                                if (eval == null) {
+//                                    return null;
+//                                }
+//                                map = eval.subEval(qq, gNode, Stack.create(sub), 0);
+//                            }
+//                        }
+//                    } else {
+//                        // never happen
+//                        return null;
+//                    }
+//                } else {
+//                    // SPARQL exists {}               
+//                    Eval eval = createEval(currentEval, exp, env, p);
+//                    if (eval == null) {
+//                        return null;
+//                    }
+//                    eval.setLimit(1);
+//                    map = eval.subEval(q, gNode, Stack.create(pat), 0);
+//                }
+//            }
+//
+//            boolean b = map.size() > 0;
+//
+//            if (exp.isSystem()) {
+//                return DatatypeMap.createObject(map);
+//            } else {
+//                //report(q, env, map);
+//                return DatatypeMap.newInstance(b);
+//            }
+//        } catch (SparqlException e) {
+//            throw EngineException.cast(e);
+//        }
+//    }
 
     /**
      * Create a mapping with var = val coming from Bind stack
      */
-    Mapping getMapping(Environment env, Query q) {
-        if (env.hasBind()) {
-            // share variables
-            Mapping map = Mapping.create(q, env.getBind());
-            // share global variables and ProcessVisitor
-            map.setBind(env.getBind());
-            return map;
-        } else {
-            // share global variables and ProcessVisitor
-            return Mapping.create(env.getBind());
-        }
-    }
+//    Mapping getMapping(Environment env, Query q) {
+//        if (env.hasBind()) {
+//            // share variables
+//            Mapping map = Mapping.create(q, env.getBind());
+//            // share global variables and ProcessVisitor
+//            map.setBind(env.getBind());
+//            return map;
+//        } else {
+//            // share global variables and ProcessVisitor
+//            return Mapping.create(env.getBind());
+//        }
+//    }
 
     @Override
     public void setMode(int m) {

07376b6ce6393a1bc649d749b66b4fb57f3f7b4e
corby <corby@antipolis>
2024-02-14 10:45:52 +0100

    DefaultResultWhenEmptyAccept Overload default behaviour when namespace accept list is empty

--- sparql/src/main/java/fr/inria/corese/sparql/triple/parser/Access.java
+++ sparql/src/main/java/fr/inria/corese/sparql/triple/parser/Access.java
@@ -28,7 +28,8 @@ public class Access {
     // throw exception for coalesce  
     // false -> sparql semantics, coalesce trap error
     public static boolean COALESCE_EXCEPTION = false;
-    
+    // by default, access is denied when accept list is empty
+    private static boolean DefaultResultWhenEmptyAccept = false;    
     // true -> skip access control
     public static boolean SKIP = false;
 
@@ -62,6 +63,7 @@ public class Access {
         public static Level USER    = PUBLIC;
         // deny access to feature
         public static Level DENY    = DENIED;
+
         
         private Level(int n) {
             value = n;
@@ -313,8 +315,12 @@ public class Access {
         return alist;
     }
     
+    // by default, isDefaultResultWhenEmptyAccept() is false
+    // DataManagerJava initGraph() set it true in order to authorize xt:read()
+    // in init query
     static boolean accept(String uri, boolean resultWhenEmptyAccept) {
-        return NSManager.isPredefinedNamespace(uri) || AccessNamespace.access(uri, resultWhenEmptyAccept);
+        return NSManager.isPredefinedNamespace(uri) 
+            || AccessNamespace.access(uri, isDefaultResultWhenEmptyAccept() || resultWhenEmptyAccept);
     }
     
     public static void define(String ns, boolean b) {
@@ -525,7 +531,14 @@ public class Access {
         }
         return ((fr.inria.corese.sparql.triple.function.term.Binding)m.getBind()).getAccessLevel();
     }
-    
+
+    public static boolean isDefaultResultWhenEmptyAccept() {
+        return DefaultResultWhenEmptyAccept;
+    }
+
+    public static void setDefaultResultWhenEmptyAccept(boolean aDefaultResultWhenEmptyAccept) {
+        DefaultResultWhenEmptyAccept = aDefaultResultWhenEmptyAccept;
+    }
     
     
 }
